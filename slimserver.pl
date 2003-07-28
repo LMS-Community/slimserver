af48109a15bc38a483736f410b635c0b6525ba92
Sean Adams <sean@slimdevices.com>
2003-07-28 22:59:30 +0000

    testing checkin notifications. Please confirm this one.
    git-svn-id: file:///home/awy/mirror/slim/trunk/server@25 62299810-d8cb-41fd-93b7-d32162e5a4a4

--- slimserver.pl
+++ slimserver.pl
@@ -1,5 +1,5 @@
 #!/usr/bin/perl -w
-  
+
 # Slim Server Copyright (C) 2001 Sean Adams, Slim Devices Inc.
 # This program is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License,
