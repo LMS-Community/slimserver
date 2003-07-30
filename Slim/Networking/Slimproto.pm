44dbe10e05a855c5a38a6f6bcf3f1c3b5a54ca26
Sean Adams <sean@slimdevices.com>
2003-07-30 23:01:57 +0000

    going to use TYPE.LENGTH.DATA for framing the new protocol. This is better than LENGTH.TYPE.DATA because it avoids confusion as to whether the length includes the 4-byte type code (op code? what are we calling it?)
    Also, parse the type the same way we parse the length, during data reception
    instead of in a regex.
    
    Still can't get non-blocking reads to work in Windows. Sucks cuz I
    need ethereal and I don't have a linux machine with X here...
    
    Robgert, any ideas? All we care about is activestate 5.8.0
    
    
    git-svn-id: file:///home/awy/mirror/slim/trunk/server@35 62299810-d8cb-41fd-93b7-d32162e5a4a4

--- Slim/Networking/Slimproto.pm
+++ Slim/Networking/Slimproto.pm
@@ -1,6 +1,6 @@
 package Slim::Networking::Slimproto;
 
-# $Id: Slimproto.pm,v 1.1 2003/07/30 00:45:35 sadams Exp $
+# $Id: Slimproto.pm,v 1.2 2003/07/30 23:01:57 sadams Exp $
 
 # Slim Server Copyright (c) 2001, 2002, 2003 Sean Adams, Slim Devices Inc.
 # This program is free software; you can redistribute it and/or
@@ -20,6 +20,8 @@ use Fcntl qw(F_GETFL F_SETFL);
 use Slim::Utils::Misc;
 use Slim::Utils::Strings qw(string);
 
+use Errno qw(EWOULDBLOCK EINPROGRESS);
+
 my $SLIMPROTO_ADDR = 0;
 my $SLIMPROTO_PORT = 3483;
 
@@ -29,8 +31,9 @@ my $slimSelWrite = IO::Select->new();
 
 my %peeraddr;     	# peer address for each socket
 my %inputbuffer;  	# inefficiently append data here until we have a full slimproto frame
-my %parser_state; 	# 'LENGTH', 'DATA'
+my %parser_state; 	# 'LENGTH', 'OP', or 'DATA'
 my %parser_framelength; # total number of bytes for data frame
+my %parser_frametype;   # frame type eg "HELO", "IR  ", etc.
 
 sub init {
 	my ($listenerport, $listeneraddr) = ($SLIMPROTO_PORT, $SLIMPROTO_ADDR);
@@ -45,12 +48,7 @@ sub init {
 		Timeout   => 0.001
 	) || die "Can't listen on port $listenerport for Slim protocol: $!";
 
-        if( $^O =~ /Win32/ ) {
-                my $temp = 1;
-                ioctl($slimproto_socket, 0x8004667e, \$temp);
-        } else {
-                defined($slimproto_socket->blocking(0))  || die "Cannot set port nonblocking";
-        }
+        defined($slimproto_socket->blocking(0))  || die "Cannot set port nonblocking";
 
 	$slimSelRead->add($slimproto_socket);
 	$main::selRead->add($slimproto_socket);
@@ -87,6 +85,8 @@ sub slimproto_accept {
 
 	return unless $clientsock;
 
+        defined($clientsock->blocking(0))  || die "Cannot set port nonblocking";
+
 	my $peer = $clientsock->peeraddr;
 
 	if (!($clientsock->connected && $peer)) {
@@ -104,7 +104,7 @@ sub slimproto_accept {
 	}
 	
 	$peeraddr{$clientsock} = $tmpaddr;
-	$parser_state{$clientsock} = 'LENGTH';
+	$parser_state{$clientsock} = 'OP';
 	$parser_framelength{$clientsock} = 0;
 	$inputbuffer{$clientsock}='';
 
@@ -157,6 +157,7 @@ sub client_readable {
 
 	$::d_protocol && msg("Slimproto client readable: ".$peeraddr{$s}."\n");
 
+GETMORE:
 	if (!($s->connected)) {
 		$::d_protocol && msg("Slimproto connection closed by peer.\n");
 		slimproto_close($s);		
@@ -171,52 +172,83 @@ sub client_readable {
 		"inbuflen: ".length($inputbuffer{$s})
 		)."\n");
 
-	if ($parser_state{$s} eq 'LENGTH') {
+	if ($parser_state{$s} eq 'OP') {
 		$bytes_remaining = 4 - length($inputbuffer{$s});
-		assert (($bytes_remaining > 0) && ($bytes_remaining <= 4));
+                assert ($bytes_remaining <= 4);
+	} elsif ($parser_state{$s} eq 'LENGTH') {
+		$bytes_remaining = 4 - length($inputbuffer{$s});
+		assert ($bytes_remaining <= 4);
 	} else {
 		assert ($parser_state{$s} eq 'DATA');
 		$bytes_remaining = $parser_framelength{$s} - length($inputbuffer{$s});
-		assert ($bytes_remaining > 0);
 	}
+	assert ($bytes_remaining > 0);
+
+	$::d_protocol && msg("attempting to read $bytes_remaining bytes\n");
 
 	my $indata;
 	my $bytes_read = $s->read($indata, $bytes_remaining);
 	$inputbuffer{$s}.=$indata;
 
 	if ($bytes_read == 0) {
-		$::d_protocol && msg("Slimproto half-close from client: ".$peeraddr{$s}."\n");
-		slimproto_close($s);
-		return;
-	}
+#		$::d_protocol && msg("Slimproto half-close from client: ".$peeraddr{$s}."\n");
+#		slimproto_close($s);
+#		return;
 
-	$::d_protocol && msg ("Got $bytes_read bytes from client, $bytes_remaining left\n");
+		$::d_protocol && msg("no more to read.\n");
+		return;
+
+	}
 
 	$bytes_remaining -= $bytes_read;
 
-	$::d_protocol && msg ("bytes remaining: $bytes_remaining\n");
+	$::d_protocol && msg ("Got $bytes_read bytes from client, $bytes_remaining remaining\n");
+
 	assert ($bytes_remaining>=0);
 
 	if ($bytes_remaining == 0) {
-		if ($parser_state{$s} eq 'LENGTH') {
+		if ($parser_state{$s} eq 'OP') {
+			assert(length($inputbuffer{$s}) == 4);
+			$parser_frametype{$s} = $inputbuffer{$s};
+			$inputbuffer{$s} = '';
+			$parser_state{$s} = 'LENGTH';
+
+			$d::protocol && msg("got op: ". $parser_frametype{$s}."\n");
+
+		} elsif ($parser_state{$s} eq 'LENGTH') {
 			assert(length($inputbuffer{$s}) == 4);
 			$parser_framelength{$s} = unpack('N', $inputbuffer{$s});
 			$parser_state{$s} = 'DATA';
-			$inputbuffer{$s} = '';			
+			$inputbuffer{$s} = '';
+
+			if ($parser_framelength{$s} > 1000) {
+				$::d_protocol && msg ("Client gave us insane length ".$parser_framelength{$s}." for slimproto frame. Disconnecting him.\n");
+				slimproto_close($s);
+				return;
+			}
+
 		} else {
 			assert($parser_state{$s} eq 'DATA');
 			assert(length($inputbuffer{$s}) == $parser_framelength{$s});
-			print "got complete slimproto frame.\n";
+			&process_slimproto_frame($s, $parser_frametype{$s}, $inputbuffer{$s});
 			$inputbuffer{$s} = '';
-			$parser_state{$s} = 'LENGTH';
+			$parser_frametype{$s} = '';
+			$parser_framelength{$s} = 0;
+			$parser_state{$s} = 'OP';
 		}
 	}
 
+	$::d_protocol && msg("new state: ".$parser_state{$s}."\n");
+	goto GETMORE;
 }
 
 
+sub process_slimproto_frame {
+	my ($s, $op, $data) = @_;
 
+	print "Got Slimptoto frame, op $op data $data\n";
 
+}
 
 1;
 
