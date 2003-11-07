6e9863096a100cc0bc47b9d30be1bfe9d5c70a77
Dean Blackketter <dean@slimdevices.com>
2003-11-07 19:18:54 +0000

    Typo.
    git-svn-id: file:///home/awy/mirror/slim/trunk/server@293 62299810-d8cb-41fd-93b7-d32162e5a4a4

--- Slim/Web/Pages.pm
+++ Slim/Web/Pages.pm
@@ -1,6 +1,6 @@
 package Slim::Web::Pages;
 
-# $Id: Pages.pm,v 1.17 2003/11/06 18:37:07 dean Exp $
+# $Id: Pages.pm,v 1.18 2003/11/07 19:18:54 dean Exp $
 # Slim Server Copyright (c) 2001, 2002, 2003 Sean Adams, Slim Devices Inc.
 # This program is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License, 
@@ -762,7 +762,7 @@ sub addsonginfo {
 				$loc = Slim::Utils::Misc::pathFromFileURL($loc);
 			}
 			$loc = Slim::Utils::Misc::fixPath($loc);
-			$songpath = $log;
+			$songpath = $loc;
 			my $curdir = Slim::Utils::Prefs::get('mp3dir');
 			if ($loc =~ /^\Q$curdir\E(.*)/) {
 				$url = '/music';
@@ -772,7 +772,7 @@ sub addsonginfo {
 				}
 				$url =~ s/\/\//\//;
 			} else {
-				$url = $log;				
+				$url = $loc;				
 			}
 	
 		}
