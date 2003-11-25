3298489b25d04e34f6c0b423f4e0fb10caa1fbe5
grotus <rlmoser@comcast.net>
2003-11-25 04:03:50 +0000

    A pretty much complete rewrite of Information.pm.
    Now uses INPUT.List for all menu management.
    
    Hopefully easier to read.  I'm not completely happy with the sorting
    of the module display, but it is the same as it was before.
    
    
    git-svn-id: file:///home/awy/mirror/slim/trunk/server@351 62299810-d8cb-41fd-93b7-d32162e5a4a4

--- Slim/Buttons/Information.pm
+++ Slim/Buttons/Information.pm
@@ -1,5 +1,5 @@
 #
-#	$Id: Information.pm,v 1.2 2003/11/20 16:58:02 dean Exp $
+#	$Id: Information.pm,v 1.3 2003/11/25 04:03:50 grotus Exp $
 #
 #	Author: Kevin Walsh <kevin@cursor.biz>
 #
@@ -43,95 +43,24 @@
 package Slim::Buttons::Information;
 
 use POSIX qw(strftime);
-use File::Spec::Functions qw(catfile);
+use File::Spec::Functions qw(catdir);
 use Slim::Utils::Strings qw(string);
 use strict;
 
 use vars qw($VERSION);
-$VERSION = substr(q$Revision: 1.2 $,10);
+$VERSION = substr(q$Revision: 1.3 $,10);
 
-my $modes_set;
 my $modules;
 my %enabled;
-my %context;
 
 Slim::Buttons::Common::addMode('information', getFunctions(), \&Slim::Buttons::Information::setMode);
 
-sub main_list {
-	return ['library','player','server','module'];
-}
-
-sub library_list {
-	return  [
-		[ 'TIME',         'time',    0,  \&Slim::Music::Info::total_time ],
-		[ 'ALBUMS',       'int',     0,  sub { Slim::Music::Info::albumCount([],[],[],[]) } ],
-		[ 'TRACKS',       'int',     0,  sub { Slim::Music::Info::songCount([],[],[],[]) } ],
-		[ 'ARTISTS',      'int',     0,  sub { Slim::Music::Info::artistCount([],[],[],[]) } ],
-		[ 'GENRES',       'int',     0,  sub { Slim::Music::Info::genreCount([],[],[],[]) } ],
-	];
-}
-
-sub server_list {
-	return [
-    [ 'VERSION',      'string',  0,  sub { $::VERSION } ],
-    [ 'SERVER_PORT',  'string',  0,  sub { 3483 } ],
-    [ 'SERVER_HTTP',  'string',  0,  sub { Slim::Utils::Prefs::get('httpport') } ],
-    [ 'CLIENTS',      'int',     1,  \&Slim::Player::Client::clientCount ],
-	];
-}
-
-sub player_list {
-	my $client = shift;
-	my @player_list = ( 
-		[ 'PLAYER_NAME',  'string',  1,  sub { shift->name } ],
-		[ 'PLAYER_MODEL', 'string',  1,  sub { shift->model() } ],
-		[ 'FIRMWARE',     'string',  1,  sub { shift->revision } ],
-		[ 'PLAYER_IP',    'string',  1,  sub { shift->ip } ],
-		[ 'PLAYER_PORT',  'string',  1,  sub { shift->port } ],
-		[ 'PLAYER_MAC',   'string',  1,  sub { uc(shift->macaddress) } ],
-	);
-
-	if (defined($client->signalStrength)) {
-		push (@player_list,[ 'PLAYER_SIGNAL_STRENGTH', 'string', 1, sub { return (shift->signalStrength() . '%'); } ]);
-	}
-	
-	return \@player_list;
-}
-
 sub module_list {
 	return undef unless $modules;
-	
 	return [sort { $modules->{$a} cmp $modules->{$b} } keys %$modules];
 }
 
-my %menu = (
-    main => {
-	lines => \&main_lines,
-	list => \&main_list,
-    },
-    library => {
-	lines => \&info_lines,
-	list => \&library_list,
-    },
-    player => {
-	lines => \&info_lines,
-	list => \&player_list,
-    },
-    server => {
-	lines => \&info_lines,
-	list => \&server_list,
-    },
-    module => {
-	lines => \&module_lines,
-	list => \&module_list,
-    },
-);
-
-my %format = (
-    'int' => sub {
-	Slim::Utils::Misc::delimitThousands(shift);
-    },
-    'time' => sub {
+sub timeFormat {
 	my $time = shift || 0;
 
 	sprintf(
@@ -140,225 +69,204 @@ my %format = (
 	    ($time / 60) % 60,
 	    $time % 60,
 	);
-    },
-    'string' => sub {
-	shift;
-    },
-);
+}
 
-my %functions = (
-    'left' => sub {
-		my $client = shift;
-	
-		Slim::Buttons::Common::popModeRight($client);
-	},
-    'right' => sub {
-		my $client = shift;
-		my $nextmode = find_nextmode($client);
+# since we just jump into INPUT.List, we don't need any functions of our own
+my %functions = ();
 
-		if (defined($nextmode)) {		
-			my $listfunc = $menu{$nextmode}->{list};
-		
-			if ($listfunc && $nextmode && ref(&$listfunc($client))) {
-				Slim::Buttons::Common::pushModeLeft(
-				$client,
-				"information-$nextmode",
-				);
-				return;
-			}
+# array for internal values of the player submenu
+my @player_list = ('PLAYER_NAME','PLAYER_MODEL','FIRMWARE','PLAYER_IP','PLAYER_PORT','PLAYER_MAC');
+
+# hash of parameters for the various menus, these will be passed to INPUT.List
+# Some of the parameters aren't used by INPUT.List, but it is handy to let them be
+# stored in the mode stack.
+my %menuParams = (
+	'main' => {
+		'header' => \&mainHeader
+		,'headerArgs' => 'C'
+		,'externRef' => sub {return string('INFORMATION_MENU_' . uc($_[0]));}
+		,'externRefArgs' => 'V'
+		,'listRef' => ['library','player','server','module']
+		,'overlayRef' => sub {return (undef,Slim::Hardware::VFD::symbol('rightarrow'));}
+		,'overlayRefArgs' => ''
+		,'callback' => \&mainExitHandler
+	}
+	,catdir('main','library') => {
+		'header' => \&infoHeader
+		,'headerArgs' => 'C'
+		,'listRef' => ['TIME','ALBUMS','TRACKS','ARTISTS','GENRES']
+		,'externRef' => \&infoDisplay
+		,'externRefArgs' => 'CV'
+		,'formatRef' => [\&timeFormat
+				,\&Slim::Utils::Misc::delimitThousands
+				,\&Slim::Utils::Misc::delimitThousands
+				,\&Slim::Utils::Misc::delimitThousands
+				,\&Slim::Utils::Misc::delimitThousands
+				]
+		,'valueFunctRef' => [\&Slim::Music::Info::total_time
+					,sub { Slim::Music::Info::albumCount([],[],[],[]) }
+					,sub { Slim::Music::Info::songCount([],[],[],[]) }
+					,sub { Slim::Music::Info::artistCount([],[],[],[]) }
+					,sub { Slim::Music::Info::genreCount([],[],[],[]) }
+					]
+		,'menuName' => 'library'
 		}
-		
-		Slim::Display::Animation::bumpRight($client);
-    },
-    'up' => sub {
-		my $client = shift;
-		my $ref = $context{$client};
-		$ref->{$ref->{current}} = Slim::Buttons::Common::scroll(
-			$client,
-			-1,
-			($#{$ref->{list}} + 1),
-			$ref->{$ref->{current}},
-		);
-		$client->update();
-    },
-    'down' => sub {
-		my $client = shift;
-		my $ref = $context{$client};
-		$ref->{$ref->{current}} = Slim::Buttons::Common::scroll(
-			$client,
-			1,
-			($#{$ref->{list}} + 1),
-			$ref->{$ref->{current}},
-		);
-		$client->update();
-    },
+	,catdir('main','player') => {
+		'header' => \&infoHeader
+		,'headerArgs' => 'C'
+		,'listRef' => \@player_list
+		,'externRef' => \&infoDisplay
+		,'externRefArgs' => 'CV'
+		,'valueFunctRef' => [sub { shift->name }
+					,sub { shift->model() }
+					,sub { shift->revision }
+					,sub { shift->ip }
+					,sub { shift->port }
+					,sub { uc(shift->macaddress) }
+					,sub { return (shift->signalStrength() . '%'); }]
+		,'menuName' => 'player'
+		}
+	,catdir('main','server') => {
+		'header' => \&infoHeader
+		,'headerArgs' => 'C'
+		,'listRef' => ['VERSION','SERVER_PORT','SERVER_HTTP','CLIENTS']
+		,'externRef' => \&infoDisplay
+		,'externRefArgs' => 'CV'
+		,'formatRef' => [undef,undef,undef,\&Slim::Utils::Misc::delimitThousands]
+		,'valueFunctRef' => [sub { $::VERSION }
+					, sub { 3483 }
+					, sub { Slim::Utils::Prefs::get('httpport') }
+					, \&Slim::Player::Client::clientCount ]
+		,'menuName' => 'server'
+	}
+		,catdir('main','module') => {
+		'header' => \&infoHeader
+		,'headerArgs' => 'C'
+		,'listRef' => undef #filled in setMode
+		,'externRef' => \&moduleDisplay
+		,'externRefArgs' => 'V'
+		,'menuName' => 'module'
+	}
+
 );
 
-#
-#	find_nextmode()
-#	---------------
-#	Return the name of the next mode when moving from the current
-#	list to a sub-list.
-#
-#	Context keys used:
-#
-#	    current	The name of the current list
-#	    (menuname)  Used as $ref->{$ref->{current}} which is the current
-#			position within the current list, numbered from zero
-#	    list	The current list data, stored as an arrayref
-#
-sub find_nextmode {
-    my $ref = $context{(shift)};
-    my $nextmode = $ref->{list}->[$ref->{$ref->{current}}];
 
-    return ref($nextmode) ? undef : $nextmode;
+# hash of current locations in the menu structure
+# This is keyed by the $client object, then the second level
+# is keyed by the menu.  When entering any menu, the valueRef parameter
+# passed to INPUT.List refers back to here.
+my %current;
+
+# function providing the second line of the display for the
+# library, server, and player menus
+sub infoDisplay {
+	my ($client,$value) = @_;
+	my $listIndex = Slim::Buttons::Common::param($client,'listIndex');
+	my $formatRef = Slim::Buttons::Common::param($client,'formatRef');
+	my $valueFunctRef = Slim::Buttons::Common::param($client,'valueFunctRef');
+	if (defined($formatRef) && defined($formatRef->[$listIndex])) {
+		return string('INFORMATION_' . uc($value)) . ': '
+		. $formatRef->[$listIndex]->($valueFunctRef->[$listIndex]->($client));
+	} else {
+		return string('INFORMATION_' . uc($value)) . ': '
+		. $valueFunctRef->[$listIndex]->($client);
+	}
 }
 
-#
-#	main_lines()
-#	------------
-#	Create and return the two-line display for the main menu.
-#
-sub main_lines {
-    my $client = shift;
-    my $ref = $context{$client};
-    my $current = $ref->{$ref->{current}};
-    my $list = $ref->{list};
-	
-    return (
-    	(
-	    string('INFORMATION') .
-	    ' (' .
-	    ($current + 1) .
-	    ' ' .
-	    string('OF') .
-	    ' ' .
-	    ($#$list + 1) .
-	    ')'
-	),
-	string(
-	    'INFORMATION_MENU_' .
-	    uc($list->[$current])
-	),
-	undef,
-	Slim::Hardware::VFD::symbol('rightarrow'),
-    );
+# function providing the second line of the display for the module menu
+sub moduleDisplay {
+	my $item = shift;
+	my @info;
+	push(@info,$modules->{$item});
+	push(@info,string('INFORMATION_DISABLED')) unless $enabled{$item};
+
+	my $version = eval {
+		no strict 'refs';
+		${"Plugins::${item}::VERSION"};
+	};
+	if ($@ || !$version) {
+		push(@info,string('INFORMATION_NO_VERSION'));
+	}
+	else {
+		$version =~ s/^\s+//;
+		$version =~ s/\s+$//;
+		push(@info,string('INFORMATION_VERSION') . ": $version");
+	}
+
+	return join(' ' . Slim::Hardware::VFD::symbol('rightarrow') . ' ',@info);
+
 }	
 
-#
-#	info_lines()
-#	------------
-#	Create and return the two-line display for the various information
-#	items (except module information).
-#
-sub info_lines {
-    my $client = shift;
-    my $ref = $context{$client};
-    my $current = $ref->{$ref->{current}};
-    my $item = $ref->{list}->[$current];
-
-    return (
-	(
-	    string('INFORMATION_MENU_' . uc($ref->{current})) .
-	    ' (' .
-	    ($current + 1) .
-	    ' ' .
-	    string('OF') .
-	    ' ' .
-	    ($#{$ref->{list}} + 1) .
-	    ')'
-	),
-	(
-	    string('INFORMATION_' . uc($item->[0])) .
-	    ': ' .
-	    $format{$item->[1]}->($item->[3]->($item->[2] ? $client : undef))
-	)
-    );
-}	
-
-#
-#	module_lines()
-#	--------------
-#	Create and return the two-line display for the module information.
-#
-sub module_lines {
-    my $client = shift;
-    my $ref = $context{$client};
-    my $current = $ref->{$ref->{current}};
-    my $item = $ref->{list}->[$current];
-    my @lines;
-    my @info;
-
-    $lines[0] = (
-	string('INFORMATION_MENU_' . uc($ref->{current})) .
-	' (' .
-	($current + 1) .
-	' ' .
-	string('OF') .
-	' ' .
-	($#{$ref->{list}} + 1) .
-	') ' .
-	"${item}"
-    );
-
-    push(@info,$modules->{$item});
-    push(@info,string('INFORMATION_DISABLED')) unless $enabled{$item};
-
-    my $version = eval {
-	no strict 'refs';
-	${"Plugins::${item}::VERSION"};
-    };
-    if ($@ || !$version) {
-	push(@info,string('INFORMATION_NO_VERSION'));
-    }
-    else {
-    	$version =~ s/^\s+//;
-    	$version =~ s/\s+$//;
-	push(@info,string('INFORMATION_VERSION') . ": $version");
-    }
-
-    $lines[1] = join(' ' . Slim::Hardware::VFD::symbol('rightarrow') . ' ',@info);
-    @lines;
-}	
-
-sub setmode_submenu {
-    my $client = shift;
-    my $ref = $context{$client};
-    if ($ref->{current} eq 'main') {
-		$ref->{current} = $ref->{list}->[$ref->{$ref->{current}}];
-    }
-    my $listfunc = $menu{$ref->{current}}->{list};
-    $ref->{list} = &$listfunc($client);
-    $ref->{$ref->{current}} ||= 0;
-    $client->lines($menu{$ref->{current}}->{lines});
-    $client->update();
+# function providing the top line of the display for all submenus
+sub infoHeader {
+	my $client = shift;
+	return string('INFORMATION_MENU_' . uc(Slim::Buttons::Common::param($client,'menuName')))
+		. ' ('
+		. (Slim::Buttons::Common::param($client,'listIndex') + 1)
+		. ' ' . string('OF') . ' '
+		. scalar(@{Slim::Buttons::Common::param($client,'listRef')})
+		. ')'
 }
 
+# function providing the top line of the display for the main menu
+sub mainHeader {
+	my $client = shift;
+	return string('INFORMATION') 
+		. ' (' 
+		. (Slim::Buttons::Common::param($client,'listIndex') + 1)
+		. ' ' . string('OF') . ' '
+		. scalar(@{Slim::Buttons::Common::param($client,'listRef')})
+		. ')';
+}
+
+# callback function for the main menu, handles descending into the submenus
+sub mainExitHandler {
+	my ($client,$exittype) = @_;
+	$exittype = uc($exittype);
+	if ($exittype eq 'LEFT') {
+		Slim::Buttons::Common::popModeRight($client);
+	} elsif ($exittype eq 'RIGHT') {
+		my $nextmenu = catdir('main',$current{$client}{'main'});
+		if (exists($menuParams{$nextmenu})) {
+			my %nextParams = %{$menuParams{$nextmenu}};
+			$current{$client}{$nextmenu} = $menuParams{$nextmenu}{'listRef'}[0] unless exists($current{$client}{$nextmenu});
+			$nextParams{'valueRef'} = \$current{$client}{$nextmenu};
+			if ($nextmenu eq catdir('main','player')) {
+				my @nextList = @player_list;
+				push @nextList, 'PLAYER_SIGNAL_STRENGTH' if defined($client->signalStrength());
+				$nextParams{'listRef'} = \@nextList;
+			}
+			Slim::Buttons::Common::pushModeLeft(
+				$client,
+				"INPUT.List",
+				\%nextParams
+			);
+		} else {
+			Slim::Display::Animation::bumpRight($client);
+		}
+	} else {
+		return;
+	}
+}
+
+# Standard button mode subs follow
 sub setMode {
 	my $client = shift;
-
-	unless ($modes_set) {
-		$modes_set = 1;
-		foreach (keys %menu) {
-			next if $_ eq 'main';
-
-			Slim::Buttons::Common::addMode(
-			"information-$_",
-			\%functions,
-			\&setmode_submenu,
-			);
-		}
+	my $method = shift;
+	if ($method eq 'pop') {
+		Slim::Buttons::Common::popModeRight($client);
+		return;
 	}
 	unless (ref($modules)) {
 		$modules = Slim::Buttons::Plugins::installedPlugins();
 		$enabled{$_} = 1 for (Slim::Buttons::Plugins::enabledPlugins($client));
+		$menuParams{catdir('main','module')}{'listRef'} = module_list();
 	}
 
-	$context{$client}->{current} = 'main';
-	$context{$client}->{list} = &main_list($client),
-	$context{$client}->{main} ||= 0;
-
-	$client->lines(\&main_lines);
+	$current{$client}{main} = 'library' unless exists($current{$client}{main});
+	my %params = %{$menuParams{'main'}};
+	$params{'valueRef'} = \$current{$client}{main};
+	Slim::Buttons::Common::pushMode($client,'INPUT.List',\%params);
 	$client->update();
 }
 
