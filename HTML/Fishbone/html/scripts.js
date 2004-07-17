8836c194def5e3191becb7c45b87f2cf7c2c09d9
Kevin Deane-Freeman <slim-mail@deane-freeman.com>
2004-07-17 17:14:53 +0000

    Skins: 	clean up Fishbone skin javascript, move to common file where 	possible
    git-svn-id: file:///home/awy/mirror/slim/trunk/server@1100 62299810-d8cb-41fd-93b7-d32162e5a4a4

---
+++ HTML/Fishbone/html/scripts.js
@@ -0,0 +1,96 @@
+var p = 1;
+
+
+function to_currentsong() {
+	if (window.location.hash == '') {
+		window.location.hash = 'currentsong';
+	}
+}
+
+function openRemote()
+{
+	window.open('status.html?player=[% player | uri %]&start=[% start %]&refresh=1', 'access', 'width=500,height=240');
+}
+
+function setCookie(name, value)
+{
+	var expires = new Date();
+	expires.setTime(expires.getTime() + 1000*60*60*24*365);
+	document.cookie =
+		name + "=" + escape(value) +
+		((expires == null) ? "" : ("; expires=" + expires.toGMTString()));
+}
+
+function checkReload()
+{
+			if (parent.playlist.location != '') parent.playlist.location.reload(true);
+}
+
+var p = 1;
+// Update the progress dialog with the current state
+function ProgressUpdate(mp,_progressEnd,_progressAt) 
+{
+	if (mp)_progressAt++;
+	if(_progressAt > _progressEnd) _progressAt = _progressAt % _progressEnd;
+	if (document.all) //if IE 4+
+	{
+		p = (document.body.clientWidth / _progressEnd) * _progressAt;
+		//document.all.progressBar.innerWidth = p+" ";
+		eval("document.progressBar.width=p");
+	}
+	else if (document.getElementById) //else if NS6+
+	{
+		p = (document.width / _progressEnd) * _progressAt;
+		document.getElementById("progressBar").width=p+" ";
+		//eval("document.progressBar.width=p");
+	}
+	setTimeout("ProgressUpdate("+mp+","+_progressEnd+","+_progressAt+")", 1000);
+}
+
+function Click(mp,end,at) 
+{
+	var s = '';
+	if (!mp) s = '_s';
+	if (document.all||document.getElementById)
+	document.write('<table border="0" cellspacing="0" cellpadding="0"><td height="5"><img id="progressBar" name="progressBar" src="html/pixel.green'+s+'.gif" width="1" height="5"></td></table>');
+	ProgressUpdate(mp,end,at)
+}
+
+function getArgs() {
+	var args = new Object();
+	var query = location.search.substring(1);
+	var pairs = query.split("&");
+	for(var i = 0; i < pairs.length; i++) {
+		var pos = pairs[i].indexOf('=');
+		if (pos == -1) continue;
+		var argname = pairs[i].substring(0,pos);
+		var value = pairs[i].substring(pos+1);
+		args[argname] = unescape(value);
+	}
+	return args;
+}
+
+function getPlayer(Player) 
+{
+	var search = Player + "=";
+	if (document.cookie.length > 0) {
+		offset = document.cookie.indexOf(search);
+		if (offset != -1) {
+			offset += search.length;
+			end = document.cookie.indexOf(";", offset);
+			if (end == -1)
+				end = document.cookie.length;
+			return unescape(document.cookie.substring(offset, end));
+		}
+	}
+	return "";
+}
+
+function setCookie(name, value)
+{
+	var expires = new Date();
+	expires.setTime(expires.getTime() + 1000*60*60*24*365);
+	document.cookie =
+		name + "=" + escape(value) +
+		((expires == null) ? "" : ("; expires=" + expires.toGMTString()));
+}
