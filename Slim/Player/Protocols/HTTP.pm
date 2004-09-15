09708afe45ddbe492fe6f8c1fbcf08fca8d7152d
Vidur Apparao <vidur@slimdevices.com>
2004-09-15 02:48:07 +0000

    Added missing infoUrl argument that was causing some redirections to not work.
    git-svn-id: file:///home/awy/mirror/slim/trunk/server@1275 62299810-d8cb-41fd-93b7-d32162e5a4a4

--- Slim/Player/Protocols/HTTP.pm
+++ Slim/Player/Protocols/HTTP.pm
@@ -1,6 +1,6 @@
 package Slim::Player::Protocols::HTTP;
 		  
-# $Id: HTTP.pm,v 1.1 2004/09/10 03:07:39 vidur Exp $
+# $Id: HTTP.pm,v 1.2 2004/09/15 02:48:07 vidur Exp $
 
 # SlimServer Copyright (c) 2001-2004 Vidur Apparao, Slim Devices Inc.
 # This program is free software; you can redistribute it and/or
@@ -188,7 +188,7 @@ sub request {
 		
 		my $oldtitle = Slim::Music::Info::title($infoUrl);
 		
-		$self = $class->open($redir);
+		$self = $class->open($redir, $infoUrl);
 		
 		# if we've opened the redirect, re-use the old title and new content type.
 		
