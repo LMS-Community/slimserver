fafdc0a1e855810d1d6523319cbed11e540f9b68
Dean Blackketter <dean@slimdevices.com>
2004-12-15 21:12:11 +0000

    Bumping version number to 6.0 in trunk.
    git-svn-id: file:///home/awy/mirror/slim/trunk/server@1596 62299810-d8cb-41fd-93b7-d32162e5a4a4

--- slimserver.pl
+++ slimserver.pl
@@ -165,7 +165,7 @@ use vars qw($VERSION @AUTHORS);
 	'Dan Sully',
 );
 
-$VERSION = '5.4.0';
+$VERSION = '6.0.0';
 
 # old preferences settings, only used by the .slim.conf configuration.
 # real settings are stored in the new preferences file:  .slim.pref
