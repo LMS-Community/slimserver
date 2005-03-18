5b50a443d8464df4140a4f76d6ebe4c0c595321d
Dan Sully <dsully@slimdevices.com>
2005-03-18 20:32:44 +0000

    MySQL patch to be the correct column type.
    git-svn-id: file:///home/awy/mirror/slim/trunk/server@2575 62299810-d8cb-41fd-93b7-d32162e5a4a4

--- SQL/mysql/Upgrades/6.sql
+++ SQL/mysql/Upgrades/6.sql
@@ -1,8 +1,8 @@
 -- 
 --
 
--- ALTER TABLE tracks ADD COLUMN multialbumsortkey varchar(255);
+ALTER TABLE tracks ADD COLUMN multialbumsortkey text;
 
--- ALTER TABLE tracks ADD INDEX trackSortKeyIndex (multialbumsortkey(255));
+ALTER TABLE tracks ADD INDEX trackSortKeyIndex (multialbumsortkey(255));
 
--- UPDATE metainformation SET version = 6;
+UPDATE metainformation SET version = 6;
