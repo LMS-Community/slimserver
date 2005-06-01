933cc3bd2ca6391a396cd55f43066b20528d84bc
Jacob D. Potter <jacobdp@>
2005-06-01 21:41:59 +0000

    Missed a file in the ExBrowse2 conversion.
    git-svn-id: file:///home/awy/mirror/slim/trunk/server@3320 62299810-d8cb-41fd-93b7-d32162e5a4a4

---
+++ HTML/ExBrowse2/html/main.js
@@ -0,0 +1,61 @@
+///////////////////////////////////////////
+//
+// Main 
+//
+///////////////////////////////////////////
+
+var statusRefs = 0;
+var playlistRefs = 0;
+var browseRefs = 0;
+var homeRefs = 0;
+
+var scrollcurrent = "";
+
+var currentPlayer;
+
+function mainload() {
+	document.getElementById("loading").style.display = "block";
+	setTimeout(continueload, 100);
+}
+
+function continueload() {
+	initStatus();
+	initPlaylist();
+	initHome();
+	document.getElementById("loading").firstChild.innerHTML = "Connecting...";
+	loadHome();
+}
+
+function maybeDoneLoading() {
+        // if (statusRefs > 0 && playlistRefs > 0 && homeRefs > 0) {
+        if (statusRefs > 0 && homeRefs > 0) {
+		setTimeout(hideLoadingScreen, 100);
+        }
+}
+
+function hideLoadingScreen() {
+	document.getElementById("loading").style.display = "none";
+	document.getElementById("browsemode").style.display = "inline";
+	document.getElementById("playersel").style.display = "inline";
+	document.getElementById("browseframe").style.display = "block";
+}
+
+function scrollfix(divname, scrollbase) {
+	if (!divname) {
+		divname = "noscroll";
+		scrollbase = "noscroll";
+	}
+
+	if (scrollcurrent != divname && divname != "browsescroll") {	
+		var div = document.getElementById(divname);
+		var s = div.scrollTop;
+		try { document.getElementById(scrollbase).focus();
+		} catch(e) {}
+		div.scrollTop = s;
+	}
+	scrollcurrent = divname;
+}
+
+function handlekey() {
+	// XXX FIXME: the iframe's onkeypress event will call this. Just a stub until keyboard shortcuts are done.
+}
