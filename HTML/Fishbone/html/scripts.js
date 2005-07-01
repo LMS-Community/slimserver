b7216c370f5760181f01ec668b576ae9403a17ea
Kevin Deane-Freeman <slim-mail@deane-freeman.com>
2005-07-01 19:19:36 +0000

    ok, so use parens sparingly in javascript conditionals. firefox didn't like it
    Bug: 1738
    git-svn-id: file:///home/awy/mirror/slim/trunk/server@3594 62299810-d8cb-41fd-93b7-d32162e5a4a4

--- HTML/Fishbone/html/scripts.js
+++ HTML/Fishbone/html/scripts.js
@@ -2,7 +2,7 @@ var p = 1;
 
 
 function to_currentsong() {
-	if (window.location.hash == '') || (navigator.appName=="Microsoft Internet Explorer")) {
+	if (window.location.hash == '' || navigator.appName=="Microsoft Internet Explorer") {
 		window.location.hash = 'currentsong';
 	}
 }
