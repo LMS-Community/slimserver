6bb4864df9a52a8f329971eabde38cd546494cc4
Dan Sully <dsully@slimdevices.com>
2005-08-10 20:32:54 +0000

    Need to use Slim::Utils::Misc here.
    git-svn-id: file:///home/awy/mirror/slim/trunk/server@3929 62299810-d8cb-41fd-93b7-d32162e5a4a4

--- Slim/Buttons/Home.pm
+++ Slim/Buttons/Home.pm
@@ -14,6 +14,7 @@ use Slim::Buttons::Common;
 use Slim::Buttons::Playlist;
 use Slim::Buttons::TrackInfo;
 use Slim::Buttons::RemoteTrackInfo;
+use Slim::Utils::Misc;
 
 our %home = ();
 our %defaultParams = ();
