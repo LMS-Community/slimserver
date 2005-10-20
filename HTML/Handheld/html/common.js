261c484fdf9704324a9a8e227fc9153d4f53e536
Michael Herger <mherger@logitech.com>
2005-10-20 04:55:36 +0000

    minor tweaks to the Handheld skin: - move Handheld specific javascript to its own html/common.js so we don't end up with double javascript functions - RandomPlay: don't hardcode bgcolor but use variables instead
    git-svn-id: file:///home/awy/mirror/slim/trunk/server@4708 62299810-d8cb-41fd-93b7-d32162e5a4a4

---
+++ HTML/Handheld/html/common.js
@@ -0,0 +1,11 @@
+<script language="JavaScript">
+	<!-- Start Hiding the Script
+	function switchPlayer(player_List) {
+		var newPlayer = player_List.options[player_List.selectedIndex].value;
+		var myString = new String(this.location);
+		var rExp     = /(\w\w(:|%3A)){5}(\w\w)/gi;
+		this.location = myString.replace(rExp, newPlayer);
+	}
+
+	// Stop Hiding script --->
+</script> 
