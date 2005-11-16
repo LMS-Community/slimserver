9c545c1a4e169a58a9fbd9186d96a06fe678d291
Kevin Deane-Freeman <slim-mail@deane-freeman.com>
2005-11-16 02:13:46 +0000

    EditPlaylist page function should be registered before Playlist pagefunction.
    fix targets for playlist links in current playlist of fishbone skin
    
    
    
    git-svn-id: file:///home/awy/mirror/slim/trunk/server@5203 62299810-d8cb-41fd-93b7-d32162e5a4a4

--- Slim/Web/Pages.pm
+++ Slim/Web/Pages.pm
@@ -60,9 +60,9 @@ sub init {
 	Slim::Web::Pages::BrowseTree::init();
 	Slim::Web::Pages::Search::init();
 	Slim::Web::Pages::Status::init();
+	Slim::Web::Pages::EditPlaylist::init(); # must precede Playlist::init();
 	Slim::Web::Pages::Playlist::init();
 	Slim::Web::Pages::History::init();
-	Slim::Web::Pages::EditPlaylist::init();
 }
 
 ### DEPRECATED stub for third party plugins
