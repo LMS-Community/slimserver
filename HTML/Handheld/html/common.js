9cd641192828023b1bd3895794a90f0a60a43113
Kevin Deane-Freeman <slim-mail@deane-freeman.com>
2006-01-26 04:36:08 +0000

    fix for handheld playerswitcher script. One report says this doesn't work, but my testing seems to be ok.
    Bug: 2759
    git-svn-id: file:///home/awy/mirror/slim/trunk/server@5850 62299810-d8cb-41fd-93b7-d32162e5a4a4

--- HTML/Handheld/html/common.js
+++ HTML/Handheld/html/common.js
@@ -1,9 +1,9 @@
 <script language="JavaScript">
 	<!-- Start Hiding the Script
 	function switchPlayer(player_List) {
-		var newPlayer = player_List.options[player_List.selectedIndex].value;
+		var newPlayer = "=" + player_List.options[player_List.selectedIndex].value;
 		var myString = new String(this.location);
-		var rExp     = /(\w\w(:|%3A)){5}(\w\w)/gi;
+		var rExp = /(=(\w\w(:|%3A)){5}(\w\w))|(=(\d{1,3}\.){3}\d{1,3})/gi;
 		this.location = myString.replace(rExp, newPlayer);
 	}
 
