e29d1f8b1310d6dfad26a3a64dfd3c57d547188f
Fred Thomas <slimdevices@thomascorner.com>
2006-04-17 23:58:06 +0000

    Preliminary and tentative code for CLI support by Picks plugin.
    Bug: 2012
    git-svn-id: file:///home/awy/mirror/slim/trunk/server@6959 62299810-d8cb-41fd-93b7-d32162e5a4a4

--- Plugins/Picks/Plugin.pm
+++ Plugins/Picks/Plugin.pm
@@ -19,9 +19,18 @@ sub enabled {
 }
 
 sub initPlugin {
-	$::d_plugins && msg("Picks Plugin initializing.\n");
+	$::d_plugins && msg("Picks: initPlugin()\n");
 
 	Slim::Buttons::Common::addMode('PLUGIN.Picks', getFunctions(), \&setMode);
+	
+#        |requires Client
+#        |  |is a Query
+#        |  |  |has Tags
+#        |  |  |  |Function to call
+#        C  Q  T  F
+    Slim::Control::Request::addDispatch(['picks', '_index', '_quantity'],
+        [0, 1, 1, \&picksQuery]);
+
 }
 
 sub getDisplayName {
@@ -57,6 +66,53 @@ sub setMode {
 	
 	# we'll handle the push in a callback
 	$client->param('handledTransition',1)
+}
+
+sub picksQuery {
+	my $request = shift;
+	
+	$::d_plugins && msg("Picks: picksQuery()\n");
+
+	# check this is the correct query.
+	if ($request->isNotQuery([['picks']])) {
+		$request->setStatusBadDispatch();
+		return;
+	}
+	
+	Slim::Buttons::XMLBrowser::getFeedViaHTTP($request, $FEED, \&_picksQuery_done, \&_picksQuery_error);
+	
+	$request->setStatusProcessing();
+}
+
+sub _picksQuery_done {
+	my $request = shift;
+	my $url     = shift;
+	my $feed    = shift;
+
+	my $loopname = '@loop';
+	my $cnt = 0;
+		
+	for my $item ( @{$feed->{'items'}} ) {
+		$request->addResultLoop($loopname, $cnt, 'name', $item->{'name'});
+		$request->addResultLoop($loopname, $cnt, 'hasitems', scalar @{$item->{'items'}});
+		$cnt++;
+	}
+
+	$request->setStatusDone();
+}
+
+sub _picksQuery_error {
+	my $request = shift;
+	my $url     = shift;
+	my $err     = shift;
+
+	$::d_plugins && msg("Picks: error retrieving <$url>:\n");
+	$::d_plugins && msg($err);
+	
+	$request->addResult("networkerror", 1);
+	$request->addResult('count', 0);
+
+	$request->setStatusDone();	
 }
 
 sub strings {
