1d410718a26d122984f2e8b3d90f50b5284eb1d3
Dan Sully <dsully@slimdevices.com>
2006-05-23 21:05:07 +0000

    git-svn-id: file:///home/awy/mirror/slim/trunk/server@7561 62299810-d8cb-41fd-93b7-d32162e5a4a4

--- Slim/Formats/Parse.pm
+++ Slim/Formats/Parse.pm
@@ -39,6 +39,7 @@ use Slim::Player::ProtocolHandlers;
 use Slim::Player::ProtocolHandlers;
 use Slim::Player::ProtocolHandlers;
 use Slim::Player::ProtocolHandlers;
+use Slim::Player::ProtocolHandlers;
 use Slim::Utils::Misc;
 
 sub registerParser {
--- Slim/Utils/Strings.pm
+++ Slim/Utils/Strings.pm
@@ -43,6 +43,7 @@ use File::Spec::Functions qw(:ALL);
 
 
 
+
 use Slim::Utils::Misc;
 
 #-------------------------------------------------
