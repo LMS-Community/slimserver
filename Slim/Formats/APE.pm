eac36f9a00744ba9a148f97394bd6c150b0db0d5
Dan Sully <dsully@slimdevices.com>
2006-05-23 20:54:50 +0000

    git-svn-id: file:///home/awy/mirror/slim/trunk/server@7532 62299810-d8cb-41fd-93b7-d32162e5a4a4

--- Slim/Formats/APE.pm
+++ Slim/Formats/APE.pm
@@ -1,7 +1,6 @@
 package Slim::Formats::APE;
 
-# $tagsd: APE.pm,v 1.0 2004/01/27 00:00:00 daniel Exp $
-# $Id: /mirror/slim/branches/split-scanner/Slim/Formats/APE.pm 1758 2005-01-18T21:22:50.586870Z dsully  $
+# $Id: APE.pm,v 1.1 2005/01/02 22:28:08 kdf Exp $
 
 # SlimServer Copyright (c) 2001-2004 Sean Adams, Slim Devices Inc.
 # This program is free software; you can redistribute it and/or
--- Slim/Utils/Strings.pm
+++ Slim/Utils/Strings.pm
@@ -19,6 +19,7 @@ use File::Spec::Functions qw(:ALL);
 
 
 
+
 use Slim::Utils::Misc;
 
 #-------------------------------------------------
