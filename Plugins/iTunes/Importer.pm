b3f2e557a0b563896b951deb35e6f61df4abcb11
Dan Sully <dsully@slimdevices.com>
2006-05-23 21:01:56 +0000

    r8665@wibble (orig r5415):  dsully | 2005-12-19 15:49:17 -0800  Bug: N/A  Description: Merge over iTunes fix for parsing Date Modified / Date Added out of the XML.
    git-svn-id: file:///home/awy/mirror/slim/trunk/server@7554 62299810-d8cb-41fd-93b7-d32162e5a4a4

--- Plugins/iTunes/Importer.pm
+++ Plugins/iTunes/Importer.pm
@@ -417,7 +417,7 @@ sub handleStartElement {
 	}
 
 	# Store this value somewhere.
-	if ($element eq 'string' || $element eq 'integer') {
+	if ($element eq 'string' || $element eq 'integer' || $element eq 'date') {
 		$inValue = 1;
 	}
 }
@@ -519,7 +519,7 @@ sub handleEndElement {
 		return;
 	}
 
-	if ($element eq 'string' || $element eq 'integer') {
+	if ($element eq 'string' || $element eq 'integer' || $element eq 'date') {
 		$inValue = 0;
 	}
 
