bd143d9027a5e9f89d82d9ad258230bb2d22ee26
Dan Sully <dsully@slimdevices.com>
2006-06-28 00:26:17 +0000

    Optimize tables on clear.
    git-svn-id: file:///home/awy/mirror/slim/trunk/server@8163 62299810-d8cb-41fd-93b7-d32162e5a4a4

--- SQL/mysql/schema_clear.sql
+++ SQL/mysql/schema_clear.sql
@@ -8,38 +8,60 @@ DELETE FROM tracks;
 
 ALTER TABLE tracks AUTO_INCREMENT = 1;
 
+OPTIMIZE TABLE tracks;
+
 DELETE FROM playlist_track;
 
 ALTER TABLE playlist_track AUTO_INCREMENT = 1;
+
+OPTIMIZE TABLE playlist_track;
 
 DELETE FROM albums;
 
 ALTER TABLE albums AUTO_INCREMENT = 1;
 
+OPTIMIZE TABLE albums;
+
 DELETE FROM contributors;
 
 ALTER TABLE contributors AUTO_INCREMENT = 1;
 
+OPTIMIZE TABLE contributors;
+
 DELETE FROM contributor_track;
 
+OPTIMIZE TABLE contributor_track;
+
 DELETE FROM contributor_album;
+
+OPTIMIZE TABLE contributor_album;
 
 DELETE FROM genres;
 
 ALTER TABLE genres AUTO_INCREMENT = 1;
 
+OPTIMIZE TABLE genres;
+
 DELETE FROM genre_track;
+
+OPTIMIZE TABLE genre_track;
 
 DELETE FROM comments;
 
 ALTER TABLE comments AUTO_INCREMENT = 1;
 
+OPTIMIZE TABLE comments;
+
 DELETE FROM pluginversion;
 
 ALTER TABLE pluginversion AUTO_INCREMENT = 1;
 
+OPTIMIZE TABLE pluginversion;
+
 DELETE FROM unreadable_tracks;
 
 ALTER TABLE unreadable_tracks AUTO_INCREMENT = 1;
+
+OPTIMIZE TABLE unreadable_tracks;
 
 SET foreign_key_checks = 1;
