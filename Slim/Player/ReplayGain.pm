84fdd0d94e6a41d9944e8455502d9845d5a12212
Dan Sully <dsully@slimdevices.com>
2006-06-29 21:46:42 +0000

    Check against ->id and not a stringified album name (CDBI brain damage).
    Bug: 3636
    git-svn-id: file:///home/awy/mirror/slim/trunk/server@8213 62299810-d8cb-41fd-93b7-d32162e5a4a4

--- Slim/Player/ReplayGain.pm
+++ Slim/Player/ReplayGain.pm
@@ -21,7 +21,7 @@ sub fetchGainMode {
 	my $class  = shift;
 	my $client = shift;
 	my $rgmode = $client->prefGet('replayGainMode');
-	
+
 	# Mode 0 is ignore replay gain
 	return undef if !$rgmode;
 
@@ -125,7 +125,7 @@ sub trackAlbumMatch {
 	}
 	
 	# Check for album and tracknum matches as expected
-	if ($compare_track->album && ($compare_track->album == $current_track->album) && 
+	if ($compare_track->album->id && ($compare_track->album->id == $current_track->album->id) && 
 		(($current_track->tracknum + $offset) == $compare_track->tracknum)) {
 
 		return 1;
