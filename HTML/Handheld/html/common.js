b2cc2aaca54d3c23f795c6df13f1ccdcfdf0dda4
Kevin Deane-Freeman <slim-mail@deane-freeman.com>
2006-07-05 02:26:49 +0000

    album gallery view for Handheld skin, some fixups for Nokia770 skin: add missing dummylink, proper reference for artwork listings
    Bug: 3321
    git-svn-id: file:///home/awy/mirror/slim/trunk/server@8284 62299810-d8cb-41fd-93b7-d32162e5a4a4

--- HTML/Handheld/html/common.js
+++ HTML/Handheld/html/common.js
@@ -1,10 +1,31 @@
-<script language="JavaScript">
+<script type="text/javascript">
 	<!-- Start Hiding the Script
 	function switchPlayer(player_List) {
 		var newPlayer = "=" + player_List.options[player_List.selectedIndex].value;
 		var myString = new String(this.location);
 		var rExp = /(=(\w\w(:|%3A)){5}(\w\w))|(=(\d{1,3}\.){3}\d{1,3})/gi;
 		this.location = myString.replace(rExp, newPlayer);
+	}
+
+	function setCookie(name, value) {
+		var expires = new Date();
+		expires.setTime(expires.getTime() + 1000*60*60*24*365);
+		document.cookie =
+			name + "=" + escape(value) +
+			((expires == null) ? "" : ("; expires=" + expires.toGMTString()));
+	}
+
+	function toggleGalleryView(artwork) {
+	
+		if (artwork) {
+			setCookie( 'SlimServer-albumView', "&artwork=1" );
+			this.location=this.location.href+"&artwork=1";
+		} else {
+			setCookie( 'SlimServer-albumView', "" );
+			myString = new String(this.location.href);
+			var rExp = /\&artwork=1/gi;
+			this.location=myString.replace(rExp, "");
+		}
 	}
 
 	// Stop Hiding script --->
