f95f75fa2a41c050a3dc643106636303f83d3f3b
Adrian Smith <triode1@btinternet.com>
2006-07-26 18:00:34 +0000

    check default fonts to decide when to display 1st line in 1 line mode
    git-svn-id: file:///home/awy/mirror/slim/trunk/server@8686 62299810-d8cb-41fd-93b7-d32162e5a4a4

--- Slim/Display/Graphics.pm
+++ Slim/Display/Graphics.pm
@@ -215,7 +215,7 @@ sub render {
 			}
 
 			# if in sinle line mode and nothing on line[1], copy line[0] - don't do in ticker mode
-			if (!defined($sfonts->{line}[0]) && (!$screen->{line}[1] || $screen->{line}[1] eq '') && 
+			if (!($sfonts->{line}[0] || $dfonts->{line}[0]) && (!$screen->{line}[1] || $screen->{line}[1] eq '') && 
 				!exists($screen->{ticker})) {
 				$screen->{line}[1] = $screen->{line}[0];
 			}
