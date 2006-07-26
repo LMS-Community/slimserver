93378c733fe943732d1e22804a777f32f66f7285
Dan Sully <dsully@slimdevices.com>
2006-07-26 23:01:20 +0000

    Upgrade the schema the correct way.
    Bug: 3820
    git-svn-id: file:///home/awy/mirror/slim/trunk/server@8697 62299810-d8cb-41fd-93b7-d32162e5a4a4

---
+++ SQL/mysql/schema_2_up.sql
@@ -0,0 +1,13 @@
+-- It's important that there is a newline between all 
+-- SQL statements, otherwise the parser will skip them.
+
+SET foreign_key_checks = 0;
+
+--
+-- Table: years
+--
+
+CREATE TABLE IF NOT EXISTS years (
+  id smallint(5) unsigned,
+  PRIMARY KEY (id)
+) TYPE=InnoDB CHARACTER SET utf8;
