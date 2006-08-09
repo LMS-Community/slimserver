20196ac3ef980c902ca62b2eb38b398086c402f9
Kevin Deane-Freeman <slim-mail@deane-freeman.com>
2006-08-09 07:44:31 +0000

    uri escaping not needed via javascript
    Bug: 3893
    git-svn-id: file:///home/awy/mirror/slim/trunk/server@8880 62299810-d8cb-41fd-93b7-d32162e5a4a4

--- HTML/EN/html/common.js
+++ HTML/EN/html/common.js
@@ -95,7 +95,7 @@ function toggleGalleryView(artwork) {
 
 function chooseAlbumOrderBy(value, option)
 {
-	var url = '[% webroot %]browsedb.html?hierarchy=[% hierarchy %]&amp;level=[% level %][% attributes %][% IF artwork %]&amp;artwork=1[% END %]&amp;player=[% playerURI %]';
+	var url = '[% webroot %]browsedb.html?hierarchy=[% hierarchy %]&level=[% level %][% attributes %][% IF artwork %]&artwork=1[% END %]&player=[% playerURI %]';
 
 	if (option) {
 		url = url + '&orderBy=' + option;
