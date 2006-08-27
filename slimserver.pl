32a0311b2991ee70b2911614105c6ba9a9fe7f98
Dan Sully <dsully@slimdevices.com>
2006-08-27 00:50:52 +0000

    Update trunk to version 7.0a1
    git-svn-id: file:///home/awy/mirror/slim/trunk/server@9179 62299810-d8cb-41fd-93b7-d32162e5a4a4

--- slimserver.pl
+++ slimserver.pl
@@ -172,7 +172,7 @@ use vars qw($VERSION $REVISION @AUTHORS);
 	'Dan Sully',
 );
 
-$VERSION  = '6.5b1';
+$VERSION  = '7.0a1';
 
 # old preferences settings, only used by the .slim.conf configuration.
 # real settings are stored in the new preferences file:  .slim.pref
