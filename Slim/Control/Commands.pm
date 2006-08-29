cc0f7a456a51262a776c3c04344e292d50411034
Fred Thomas <slimdevices@thomascorner.com>
2006-08-29 19:51:49 +0000

    Pause fix for trunk. Rolls back 9236.
    git-svn-id: file:///home/awy/mirror/slim/trunk/server@9256 62299810-d8cb-41fd-93b7-d32162e5a4a4

--- Slim/Control/Commands.pm
+++ Slim/Control/Commands.pm
@@ -385,7 +385,7 @@ sub playcontrolCommand {
 		
 		# pause 1, pause 0 and pause (toggle) are all supported, figure out which
 		# one we want...
-		if ($newvalue) {
+		if (defined $newvalue) {
 			$wantmode = $newvalue ? 'pause' : 'play';
 		} else {
 			$wantmode = ($curmode eq 'pause') ? 'play' : 'pause';
