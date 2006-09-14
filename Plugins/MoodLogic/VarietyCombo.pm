d2d8219fe388c3aeebd3b8862a8e9702ff6120a3
Kevin Deane-Freeman <slim-mail@deane-freeman.com>
2006-09-14 01:48:56 +0000

    extra cleanup for Variety combo after using INPUT.Bar
    git-svn-id: file:///home/awy/mirror/slim/trunk/server@9693 62299810-d8cb-41fd-93b7-d32162e5a4a4

--- Plugins/MoodLogic/VarietyCombo.pm
+++ Plugins/MoodLogic/VarietyCombo.pm
@@ -19,100 +19,7 @@ our %functions = ();
 sub init {
 	Slim::Buttons::Common::addMode('moodlogic_variety_combo', getFunctions(), \&setMode);
 
-	# Variety Combo Selection UI
-	%functions = (
-		
-		'up' => sub {
-			my $client = shift;
-			my $variety = Slim::Utils::Prefs::get('varietyCombo');
-			my $inc = 1;
-			my $rate = 50; #Hz maximum
-			my $accel = 15; #Hz/s
-
-			if (Slim::Hardware::IR::holdTime($client) > 0) {
-				$inc *= Slim::Hardware::IR::repeatCount($client,$rate,$accel);
-			} else {
-				$inc = 2;
-			}
-
-			$variety += $inc;
-			if ($variety > 100) { $variety = 100; };
-			Slim::Utils::Prefs::set('varietyCombo', $variety);
-			$client->update();
-		},
-
-		'down' => sub  {
-			my $client = shift;
-			my $variety = Slim::Utils::Prefs::get('varietyCombo');
-			my $inc = 1;
-			my $rate = 50; #Hz maximum
-			my $accel = 15; #Hz/s
-
-			if (Slim::Hardware::IR::holdTime($client) > 0) {
-				$inc *= Slim::Hardware::IR::repeatCount($client,$rate,$accel);
-			} else {
-				$inc = 2;
-			}
-
-			$variety -= $inc;
-			if ($variety < 0) { $variety = 0; };
-			Slim::Utils::Prefs::set('varietyCombo', $variety);
-			$client->update();
-		},
-		
-		'left' => sub  {
-			my $client = shift;
-			Slim::Buttons::Common::popModeRight($client);
-		},
-		
-		'right' => sub  {
-			my $client = shift;
-			my $instantMix;
-
-			my $mood   = $client->param('mood');
-			my $track  = $client->param('song');
-			my $artist = $client->param('artist');
-			my $genre  = $client->param('genre');
-
-			if (defined $track) {
-
-				$instantMix = Plugins::MoodLogic::Plugin::getMix($track->moodlogic_id, undef, 'song');
-
-			} elsif (defined $mood && defined $artist) {
-
-				$instantMix = Plugins::MoodLogic::Plugin::getMix($artist->moodlogic_id, $mood, 'artist');
-
-			} elsif (defined $mood && defined $genre) {
-
-				$instantMix = Plugins::MoodLogic::Plugin::getMix($genre->moodlogic_id, $mood, 'genre');
-			}
-
-			if (scalar @$instantMix) {
-
-				Slim::Buttons::Common::pushMode($client, 'moodlogic_instant_mix', { 'mix' => $instantMix });
-				specialPushLeft($client, 0);
-
-			} else {
-
-				$client->bumpRight()
-			}
-		},
-
-		'play' => sub  {
-			my $client = shift;
-
-			if (defined $client->param( 'song')) {
-
-				my $currentItem = $client->param( 'song');
-				Slim::Buttons::Common::pushMode($client, 'moodlogic_instant_mix', {'song' => $client->param( 'song')});
-				specialPushLeft($client, 0);
-
-			} else {
-
-				$client->bumpRight()
-			}
-		},
-	);
+	%functions = ();
 }
 
 sub getFunctions {
@@ -133,7 +40,7 @@ sub setMode {
 	Slim::Buttons::Common::pushMode($client, 'INPUT.Bar', {
 		'header'       => 'SETUP_VARIETYCOMBO',
 		'stringHeader' => 1,
-		'headerValue'  => sub { return " (".$_[1].")"; },
+		'headerValue'  => sub { return " (".$_[1].") ".$client->string('MUSICMAGIC_MIXRIGHT'); },
 		'onChange'     => sub {
 				Slim::Utils::Prefs::set('varietyCombo', $_[1])}
 			,
@@ -193,29 +100,6 @@ sub varietyExitHandler {
 	}
 }
 
-#
-# figure out the lines to be put up to display
-#
-sub lines {
-	my $client = shift;
-	my ($line1, $line2);
-	my $variety = Slim::Utils::Prefs::get('varietyCombo');
-	my $level = int($variety / 100 * 40);
-	
-	$line1 = $client->linesPerScreen() == 2 ? $client->string('SETUP_VARIETYCOMBO') : $client->string('SETUP_VARIETYCOMBO_DBL');
-	$line1 .= " (".$variety.")";
-
-	$line2 = $client->symbols($client->progressBar($client->displayWidth(), $level / 40));
-
-	if ($client->linesPerScreen() == 1) { $line2 = $line1; }
-
-	return {
-		'line1'    => $line1,
-		'line2'    => $line2,
-		'overlay1' => $client->string('MUSICMAGIC_MIXRIGHT'),
-	};
-}
-
 sub specialPushLeft {
 	my $client   = shift;
 	my $step     = shift;
@@ -228,17 +112,17 @@ sub specialPushLeft {
 	
 	if ($step == 0) {
 
-		$client->block({'line' => [$mixer] });
+		$client->update( {'line' => [$mixer]});
 		Slim::Utils::Timers::setTimer($client,$when,\&specialPushLeft,$step+1);
 
 	} elsif ($step == 3) {
 
-		$client->unblock;
-		$client->pushLeft({ 'line' => [$mixer."..."] }, undef);
+		#$client->unblock;
+		$client->pushLeft({ 'line' => [$mixer."..."]});
 
 	} else {
 
-		$client->update( { 'line' => [$mixer.("." x $step) ]} );
+		$client->update( { 'line' => [$mixer.("." x $step)]});
 		Slim::Utils::Timers::setTimer($client,$when,\&specialPushLeft,$step+1);
 	}
 }
