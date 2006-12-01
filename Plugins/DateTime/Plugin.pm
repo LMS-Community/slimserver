44eeb332968105690be771aec62e4f8bb90d3a18
Dan Sully <dsully@slimdevices.com>
2006-12-01 22:53:29 +0000

    Convert DateTime
    git-svn-id: file:///home/awy/mirror/slim/trunk/server@10823 62299810-d8cb-41fd-93b7-d32162e5a4a4

--- Plugins/DateTime/Plugin.pm
+++ Plugins/DateTime/Plugin.pm
@@ -1,23 +1,172 @@
+package Plugins::DateTime::Plugin;
+
 # SlimServer Copyright (c) 2001-2006 Slim Devices Inc.
 # This program is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License,
 # version 2.
 
 use strict;
+use Slim::Utils::DateTime;
+use Slim::Utils::Prefs;
 
-###########################################
-### Section 1. Change these as required ###
-###########################################
-
-package Plugins::DateTime::Plugin;
-
-use Slim::Control::Request;
-
-use vars qw($VERSION);
-$VERSION = substr(q$Revision: 1.6 $,10);
+use Plugins::DateTime::Settings;
 
 sub getDisplayName {
 	return 'PLUGIN_SCREENSAVER_DATETIME';
+}
+
+sub enabled {
+	return ($::VERSION ge '6.1');
+}
+
+sub setMode {
+	my $client = shift;
+	$client->lines(\&lines);
+
+	# setting this param will call client->update() frequently
+	$client->modeParam('modeUpdateInterval', 1); # seconds
+}
+
+sub initPlugin {
+
+	Plugins::DateTime::Settings->new;
+
+	# Default to screensaverDateFormat, screensaverTimeFormat to '' or update from '0'
+	# Having screensaverDateFormat set to '' means that the server wide formats are used
+
+	if (!Slim::Utils::Prefs::get('screensaverDateFormat')) {
+		Slim::Utils::Prefs::set('screensaverDateFormat','')
+	}
+
+	if (!Slim::Utils::Prefs::get('screensaverTimeFormat')) {
+		Slim::Utils::Prefs::set('screensaverTimeFormat','')
+	}
+}
+
+our %functions = (
+	'up' => sub  {
+		my $client = shift;
+		my $button = shift;
+		$client->bumpUp() if ($button !~ /repeat/);
+	},
+	'down' => sub  {
+	    my $client = shift;
+		my $button = shift;
+		$client->bumpDown() if ($button !~ /repeat/);;
+	},
+	'left' => sub  {
+		my $client = shift;
+		Slim::Buttons::Common::popModeRight($client);
+	},
+	'right' => sub  {
+		my $client = shift;
+		
+		my $saver = Slim::Player::Source::playmode($client) eq 'play' ? 'screensaver' : 'idlesaver';
+		
+		if ($client->prefGet($saver) ne 'SCREENSAVER.datetime') {
+			$client->prefSet($saver,'SCREENSAVER.datetime');
+		} else {
+			$client->prefSet($saver, $Slim::Player::Player::defaultPrefs->{$saver});
+		}
+	},
+	'stop' => sub {
+		my $client = shift;
+		Slim::Buttons::Common::pushMode($client, 'SCREENSAVER.datetime');
+	}
+);
+
+sub lines {
+	my $client = shift;
+	
+	my $saver = Slim::Player::Source::playmode($client) eq 'play' ? 'screensaver' : 'idlesaver';
+	my $line2 = $client->string('SETUP_SCREENSAVER_USE');
+	my $overlay2 = Slim::Buttons::Common::checkBoxOverlay($client, $client->prefGet($saver) eq 'SCREENSAVER.datetime');
+	
+	return {
+		'line'    => [ $client->string('PLUGIN_SCREENSAVER_DATETIME'), $line2 ],
+		'overlay' => [ undef, $overlay2 ]
+	};
+}
+
+sub getFunctions {
+	return \%functions;
+}
+
+###################################################################
+### Section 3. Your variables for your screensaver mode go here ###
+###################################################################
+
+# First, Register the screensaver mode here.  
+
+sub screenSaver {
+	Slim::Buttons::Common::addSaver(
+		'SCREENSAVER.datetime',
+		getScreensaverDatetime(),
+		\&setScreensaverDateTimeMode,
+		undef,
+		'PLUGIN_SCREENSAVER_DATETIME',
+	);
+}
+
+our %screensaverDateTimeFunctions = (
+	'done' => sub  {
+		my ($client ,$funct ,$functarg) = @_;
+
+		Slim::Buttons::Common::popMode($client);
+		$client->update();
+
+		# pass along ir code to new mode if requested
+		if (defined $functarg && $functarg eq 'passback') {
+			Slim::Hardware::IR::resendButton($client);
+		}
+	},
+);
+
+sub getScreensaverDatetime {
+	return \%screensaverDateTimeFunctions;
+}
+
+sub setScreensaverDateTimeMode() {
+	my $client = shift;
+	$client->lines(\&screensaverDateTimelines);
+
+	# setting this param will call client->update() frequently
+	$client->modeParam('modeUpdateInterval', 1); # seconds
+}
+
+# following is a an optimisation for graphics rendering given the frequency DateTime is displayed
+# by always returning the same hash for the font definition render does less work
+my $fontDef = {
+	'graphic-280x16'  => { 'overlay' => [ 'small.1'    ] },
+	'graphic-320x32'  => { 'overlay' => [ 'standard.1' ] },
+	'text'            => { 'displayoverlays' => 1        },
+};
+
+sub screensaverDateTimelines {
+	my $client = shift;
+	my ($sec,$min,$hour,$mday,$mon,$year,$wday,$yday,$isdst) = localtime(time);
+	my $alarmOn = $client->prefGet("alarm", 0) || $client->prefGet("alarm", $wday);
+
+	my $nextUpdate = $client->periodicUpdateTime();
+	Slim::Buttons::Common::syncPeriodicUpdates($client, int($nextUpdate)) if (($nextUpdate - int($nextUpdate)) > 0.01);
+
+	my $display = {
+		'center' => [ Slim::Utils::DateTime::longDateF(undef,Slim::Utils::Prefs::get('screensaverDateFormat')),
+				  Slim::Utils::DateTime::timeF(undef,Slim::Utils::Prefs::get('screensaverTimeFormat')), ],
+		'overlay'=> [ ($alarmOn ? $client->symbols('bell') : undef) ],
+		'fonts'  => $fontDef,
+	};
+	
+# BUG 3964: comment out until Dean has a final word on the UI for this.	
+# 	if ($client->display->hasScreen2) {
+# 		if ($client->display->linesPerScreen == 1) {
+# 			$display->{'screen2'}->{'center'} = [undef,Slim::Utils::DateTime::longDateF(undef,Slim::Utils::Prefs::get('screensaverDateFormat'))];
+# 		} else {
+# 			$display->{'screen2'} = {};
+# 		}
+# 	}
+
+	return $display;
 }
 
 sub strings { return '
@@ -56,203 +205,6 @@ SETUP_GROUP_DATETIME_DEFAULTDATE
 	NL	SlimServer standaard
 '};
 
-my $timeFormats = {'' => "SETUP_GROUP_DATETIME_DEFAULTTIME", %{Slim::Utils::DateTime::timeFormats()}};
-my $dateFormats = {'' => "SETUP_GROUP_DATETIME_DEFAULTDATE", %{Slim::Utils::DateTime::shortDateFormats()}, %{Slim::Utils::DateTime::longDateFormats()}};
-
-sub setupGroup {
-
-	my %setupGroup = (
-		'PrefOrder' => ['screensaverDateFormat','screensaverTimeFormat']
-		,'PrefsInTable' => 1
-		,'Suppress_PrefHead' => 1
-		,'Suppress_PrefDesc' => 1
-		,'Suppress_PrefLine' => 1
-		,'Suppress_PrefSub' => 1
-		,'GroupHead' => 'SETUP_GROUP_DATETIME'
-		,'GroupDesc' => 'SETUP_GROUP_DATETIME_DESC'
-		,'GroupLine' => 1
-		,'GroupSub' => 1
-	);
-
-	my %setupPrefs = (
-		"screensaverTimeFormat" => {
-			'validate' => \&Slim::Utils::Validate::inHash
-			,'validateArgs' => [$timeFormats,1]
-			,'options' => $timeFormats
-			,'optionSort' => 'K'
-		},
-		
-		"screensaverDateFormat" => {
-			'validate' => \&Slim::Utils::Validate::inHash
-			,'validateArgs' => [$dateFormats,1]
-			,'options' => $dateFormats
-			,'optionSort' => 'K'
-		},
-	);
-	
-	return (\%setupGroup, \%setupPrefs);
-}
-
-##################################################
-### Section 2. Your variables and code go here ###
-##################################################
-
-sub enabled {
-	return ($::VERSION ge '6.1');
-}
-
-sub setMode {
-	my $client = shift;
-	$client->lines(\&lines);
-
-	# setting this param will call client->update() frequently
-	$client->modeParam('modeUpdateInterval', 1); # seconds
-}
-
-sub initPlugin {
-	# Default to screensaverDateFormat, screensaverTimeFormat to '' or update from '0'
-	# Having screensaverDateFormat set to '' means that the server wide formats are used
-
-	unless (Slim::Utils::Prefs::get('screensaverDateFormat')) {
-		Slim::Utils::Prefs::set('screensaverDateFormat','')
-	}
-
-	unless (Slim::Utils::Prefs::get('screensaverTimeFormat')) {
-		Slim::Utils::Prefs::set('screensaverTimeFormat','')
-	}
-}
-
-our %functions = (
-	'up' => sub  {
-		my $client = shift;
-		my $button = shift;
-		$client->bumpUp() if ($button !~ /repeat/);
-	},
-	'down' => sub  {
-	    my $client = shift;
-		my $button = shift;
-		$client->bumpDown() if ($button !~ /repeat/);;
-	},
-	'left' => sub  {
-		my $client = shift;
-		Slim::Buttons::Common::popModeRight($client);
-	},
-	'right' => sub  {
-		my $client = shift;
-		
-		my $saver = Slim::Player::Source::playmode($client) eq 'play' ? 'screensaver' : 'idlesaver';
-		
-		if ($client->prefGet($saver) ne 'SCREENSAVER.datetime') {
-			$client->prefSet($saver,'SCREENSAVER.datetime');
-		} else {
-			$client->prefSet($saver,$Slim::Player::Player::defaultPrefs->{$saver});
-		}
-	},
-	'stop' => sub {
-		my $client = shift;
-		Slim::Buttons::Common::pushMode($client, 'SCREENSAVER.datetime');
-	}
-);
-
-sub lines {
-	my $client = shift;
-	
-	my $saver = Slim::Player::Source::playmode($client) eq 'play' ? 'screensaver' : 'idlesaver';
-	my $line2 = $client->string('SETUP_SCREENSAVER_USE');
-	my $overlay2 = Slim::Buttons::Common::checkBoxOverlay($client, $client->prefGet($saver) eq 'SCREENSAVER.datetime');
-	
-	return {
-		'line'    => [ $client->string('PLUGIN_SCREENSAVER_DATETIME'), $line2 ],
-		'overlay' => [ undef, $overlay2 ]
-	};
-}
-
-sub getFunctions {
-	return \%functions;
-}
-
-###################################################################
-### Section 3. Your variables for your screensaver mode go here ###
-###################################################################
-
-# First, Register the screensaver mode here.  Must make the call to addStrings in order to have plugin
-# localization available at this point.
-sub screenSaver {
-	Slim::Buttons::Common::addSaver(
-		'SCREENSAVER.datetime',
-		getScreensaverDatetime(),
-		\&setScreensaverDateTimeMode,
-		undef,
-		'PLUGIN_SCREENSAVER_DATETIME',
-	);
-}
-
-our %screensaverDateTimeFunctions = (
-	'done' => sub  {
-		my ($client ,$funct ,$functarg) = @_;
-
-		Slim::Buttons::Common::popMode($client);
-		$client->update();
-
-		# pass along ir code to new mode if requested
-		if (defined $functarg && $functarg eq 'passback') {
-			Slim::Hardware::IR::resendButton($client);
-		}
-	},
-);
-
-sub getScreensaverDatetime {
-	return \%screensaverDateTimeFunctions;
-}
-
-sub setScreensaverDateTimeMode() {
-	my $client = shift;
-	$client->lines(\&screensaverDateTimelines);
-
-	# setting this param will call client->update() frequently
-	$client->modeParam('modeUpdateInterval', 1); # seconds
-}
-
-
-# following is a an optimisation for graphics rendering given the frequency DateTime is displayed
-# by always returning the same hash for the font definition render does less work
-my $fontDef = {	'graphic-280x16'  => { 'overlay' => [ 'small.1'    ] },
-				'graphic-320x32'  => { 'overlay' => [ 'standard.1' ] },
-				'text'            => { 'displayoverlays' => 1        },
-};
-
-sub screensaverDateTimelines {
-	my $client = shift;
-	my ($sec,$min,$hour,$mday,$mon,$year,$wday,$yday,$isdst) = localtime(time);
-	my $alarmOn = $client->prefGet("alarm", 0) || $client->prefGet("alarm", $wday);
-
-	my $nextUpdate = $client->periodicUpdateTime();
-	Slim::Buttons::Common::syncPeriodicUpdates($client, int($nextUpdate)) if (($nextUpdate - int($nextUpdate)) > 0.01);
-
-	my $display = {
-		'center' => [ Slim::Utils::DateTime::longDateF(undef,Slim::Utils::Prefs::get('screensaverDateFormat')),
-				  Slim::Utils::DateTime::timeF(undef,Slim::Utils::Prefs::get('screensaverTimeFormat')), ],
-		'overlay'=> [ ($alarmOn ? $client->symbols('bell') : undef) ],
-		'fonts'  => $fontDef,
-	};
-	
-# BUG 3964: comment out until Dean has a final word on the UI for this.	
-# 	if ($client->display->hasScreen2) {
-# 		if ($client->display->linesPerScreen == 1) {
-# 			$display->{'screen2'}->{'center'} = [undef,Slim::Utils::DateTime::longDateF(undef,Slim::Utils::Prefs::get('screensaverDateFormat'))];
-# 		} else {
-# 			$display->{'screen2'} = {};
-# 		}
-# 	}
-
-	return $display;
-}
-
 1;
 
 __END__
-
-# Local Variables:
-# tab-width:4
-# indent-tabs-mode:t
-# End:
---
+++ Plugins/DateTime/Settings.pm
@@ -0,0 +1,52 @@
+package Plugins::DateTime::Settings;
+
+# SlimServer Copyright (C) 2001-2006 Slim Devices Inc.
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License,
+# version 2.
+
+use strict;
+use base qw(Slim::Web::Settings);
+
+my $timeFormats = Slim::Utils::DateTime::timeFormats();
+
+my $dateFormats = {
+	%{Slim::Utils::DateTime::shortDateFormats()},
+	%{Slim::Utils::DateTime::longDateFormats()}
+};
+
+sub name {
+        return 'PLUGIN_SCREENSAVER_DATETIME';
+}
+
+sub page {
+        return 'plugins/DateTime/settings/basic.html';
+}
+
+sub handler {
+        my ($class, $client, $params) = @_;
+
+	my @prefs = qw(
+		screensaverTimeFormat
+		screensaverDateFormat
+	);
+
+	for my $pref (@prefs) {
+
+		if ($params->{'submit'}) {
+
+			Slim::Utils::Prefs::set($pref, $params->{$pref});
+		}
+
+		$params->{'prefs'}->{$pref} = Slim::Utils::Prefs::get($pref);
+        }
+
+	$params->{'timeFormats'} = $timeFormats;
+	$params->{'dateFormats'} = $dateFormats;
+
+        return $class->SUPER::handler($client, $params);
+}
+
+1;
+
+__END__
