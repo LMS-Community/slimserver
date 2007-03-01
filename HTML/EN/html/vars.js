541cca99626f92ee1c814db31cafa6511033ef2c
Michael Herger <mherger@logitech.com>
2007-03-01 17:16:08 +0000

    make sure browserTarget is defined
    git-svn-id: file:///home/awy/mirror/slim/trunk/server@11553 62299810-d8cb-41fd-93b7-d32162e5a4a4

--- HTML/EN/html/vars.js
+++ HTML/EN/html/vars.js
@@ -4,5 +4,6 @@
 	var player        = "[% playerURI %]";
 	var url           = "[% statusroot %]";
 	var statusroot    = "[% statusroot %]";
-	[% IF browserTarget %]var browserTarget = "[% browserTarget %]";[% END %]
+	var browserTarget;
+	[% IF browserTarget %]browserTarget = "[% browserTarget %]";[% END %]
 	var orderByUrl    = 'browsedb.html?hierarchy=[% hierarchy %]&level=[% level %][% attributes %][% IF artwork %]&artwork=1[% END %]&player=' + player;
