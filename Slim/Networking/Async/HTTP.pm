644524c30a0f40e2bc50ddc5b29a776c2320e12e
Andy Grundman <agrundman@slimdevices.com>
2007-04-10 20:22:26 +0000

    Merge from 6.5: -r11738:11739, fix for broken Authorization header
    git-svn-id: file:///home/awy/mirror/slim/trunk/server@11740 62299810-d8cb-41fd-93b7-d32162e5a4a4

--- Slim/Networking/Async/HTTP.pm
+++ Slim/Networking/Async/HTTP.pm
@@ -161,7 +161,7 @@ sub add_headers {
 	
 	# handle basic auth if username, password provided
 	if ( my $userinfo = $self->request->uri->userinfo ) {
-		$headers->header( Authorization => 'Basic ' . encode_base64( $userinfo ) );
+		$headers->header( Authorization => 'Basic ' . encode_base64( $userinfo, '' ) );
 	}
 	
 	my $host = $self->request->uri->host;
