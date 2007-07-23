36ac42a1138052d327842d7ab3e83da90fff5158
Ben Klaas <bklaas@logitech.com>
2007-07-23 19:20:03 +0000

    Bug 5093: Nokia Opera browser in some OS releases spoofs IE enough that it drops into the IE skin redirect to Touch. Make the criteria for redirect stricter by looking for Nokia-specific substrings in user-agent.
    Bug: 5093
    git-svn-id: file:///home/awy/mirror/slim/trunk/server@12410 62299810-d8cb-41fd-93b7-d32162e5a4a4

--- Slim/Web/HTTP.pm
+++ Slim/Web/HTTP.pm
@@ -656,13 +656,18 @@ sub processHTTP {
 		
 		# BUG: 4911 detect Internet Explorer and redirect if using the Nokia770 skin, as IE will not support the styles
 		# Touch is similar in most ways and works nicely with IE
-
-		if ($request->header('user-agent') && $request->header('user-agent') =~ /MSIE/ && $request->header('user-agent') !~ /Maemo/ 
-				&& ($params->{'skinOverride'} || $prefs->get('skin')) eq 'Nokia770') {
-		
+		# BUG: 5093 make sure that Nokia Opera isn't spoofing as IE, causing incorrect redirect
+		if (	$request->header('user-agent') &&
+			$request->header('user-agent') =~ /MSIE/ &&
+			$request->header('user-agent') !~ /Opera/  && 
+			$request->header('user-agent') !~ /Linux/ && 
+			$request->header('user-agent') !~ /arm/ && 
+			($params->{'skinOverride'} || Slim::Utils::Prefs::get('skin')) eq 'Nokia770'
+			) 
+		{
 			$log->debug("Internet Explorer Detected with Nokia Skin, redirecting to Touch");
 			$params->{'skinOverride'} ='Touch';
-		}
+                }
 
 
 		# apply CSRF protection logic to "dangerous" commands
