901315847b8623501f2676bd1d70f68e256602a4
Michael Herger <mherger@logitech.com>
2007-08-29 08:00:28 +0000

    remove album popup, open album browse page instead
    git-svn-id: file:///home/awy/mirror/slim/trunk/server@12772 62299810-d8cb-41fd-93b7-d32162e5a4a4

--- HTML/Default/html/browse.js
+++ HTML/Default/html/browse.js
@@ -12,38 +12,6 @@ Browse = function(){
 		gotoAnchor : function(anchor){
 			if (el = Ext.get('anchor' + anchor))
 				el.scrollIntoView('browsedbList');
-		},
-		
-		hideInfoPopup : function(){
-			Ext.get('infoPopup').fadeOut({ duration: 0.4});
-			Ext.get('popupBackground').fadeOut({ duration: 0.4});
-			Ext.get('popupList').setStyle('overflow', 'hidden');
-			//new Effect.Appear('viewSelect', { duration:0.4 });
-		},
-
-		popUpInfo : function(attributes) {
-			
-			// here we go-- get the album track details via an ajax call
-			// pop up a list of the tracks in an inline div, including play/add buttons next to tracks
-			// add a close button for the div to hide it
-			if (Ext.get('infoPopup')) {
-				Ext.get('popupBackground').fadeIn({ endOpacity: 0.5, duration: 0.4});
-				
-				Ext.get('infoPopup').setStyle('border', '1px solid white');
-				
-				Ext.get("popupcontent").load({
-					url: webroot + 'browsedb.html',
-					params: 'ajaxUpdate=1&player=' + player + '&' + attributes,
-					callback: this.onInfoUpdated
-					//text: "Loading Foo..."
-				});
-			}
-		},
-	
-		onInfoUpdated : function(){
-			
-			Ext.get('infoPopup').fadeIn({ endOpacity: 1, duration: 0.4});
-			Utils.init();
 		}
 	};
 }();
