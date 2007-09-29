bb7afa86de156ac338cc4ec1dadce801e1e4c233
Kevin Deane-Freeman <slim-mail@deane-freeman.com>
2007-09-29 03:19:46 +0000

    add server status page as a central info page for all things connected to SqueezeCenter.  No links from main web ui at this point, and looking for feedback on what needs to be added.  Jive info should be added at some point once they start reporting useful info to SqueezeCenter. url to use is http://<squeezecenterip>:9000/settings/server/status.html.
    Bug: 5437
    git-svn-id: file:///home/awy/mirror/slim/trunk/server@13349 62299810-d8cb-41fd-93b7-d32162e5a4a4

---
+++ Slim/Web/Settings/Server/Status.pm
@@ -0,0 +1,59 @@
+package Slim::Web::Settings::Server::Status;
+
+# $Id: Basic.pm 13299 2007-09-27 08:59:36Z mherger $
+
+# SqueezeCenter Copyright (c) 2001-2007 Logitech.
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License,
+# version 2.
+
+use strict;
+use base qw(Slim::Web::Settings);
+
+use Slim::Utils::Log;
+use Slim::Utils::Prefs;
+
+sub name {
+	return 'SERVER_STATUS';
+}
+
+sub page {
+	return 'settings/server/status.html';
+}
+
+sub handler {
+	my ($class, $client, $paramRef) = @_;
+
+	my @versions = Slim::Utils::Misc::settingsDiagString();
+
+	$paramRef->{server}{'versionInfo'}              = join( "<br />\n", @versions ) . "\n";
+	$paramRef->{server}{'INFORMATION_HOSTNAME'}     = Slim::Utils::Network::hostName;
+	$paramRef->{server}{'INFORMATION_SERVER_PORT'}  = preferences('server')->get('httpport');
+	$paramRef->{server}{'networkProxy'}             = preferences('server')->get('networkproxy');
+	$paramRef->{server}{'INFORMATION_CLIENTS'}      = Slim::Player::Client::clientCount;
+
+	$paramRef->{library}{'INFORMATION_TRACKS'}  = Slim::Utils::Misc::delimitThousands(Slim::Schema->count('Track', { 'me.audio' => 1 }));
+	$paramRef->{library}{'INFORMATION_ALBUMS'}  = Slim::Utils::Misc::delimitThousands(Slim::Schema->count('Album'));
+	$paramRef->{library}{'INFORMATION_ARTISTS'} = Slim::Utils::Misc::delimitThousands(Slim::Schema->rs('Contributor')->browse->count);
+	$paramRef->{library}{'INFORMATION_GENRES'}  = Slim::Utils::Misc::delimitThousands(Slim::Schema->count('Genre'));
+	$paramRef->{library}{'INFORMATION_TIME'}    = Slim::Buttons::Information::timeFormat(Slim::Schema->totalTime);
+
+	for my $client (Slim::Player::Client::clients()) {
+		$paramRef->{clients}{$client->id}{'INFORMATION_PLAYER_NAME_ABBR'}       = $client->name;
+		$paramRef->{clients}{$client->id}{'INFORMATION_PLAYER_MODEL_ABBR'}      = Slim::Buttons::Information::playerModel($client);
+		$paramRef->{clients}{$client->id}{'INFORMATION_FIRMWARE_ABBR'}          = $client->revision;
+		$paramRef->{clients}{$client->id}{'PLAYER_IP_ADDRESS'}                  = $client->ipport;
+		$paramRef->{clients}{$client->id}{'PLAYER_MAC_ADDRESS'}                 = $client->macaddress;
+		$paramRef->{clients}{$client->id}{'INFORMATION_PLAYER_SIGNAL_STRENGTH'} = $client->signalStrength;
+		$paramRef->{clients}{$client->id}{'INFORMATION_PLAYER_VOLTAGE'}         = $client->voltage;
+	}
+	
+	# TODO Get something useful from any Jive devices on the network.
+	#$paramRef->{controllers} = undef;
+	
+	return Slim::Web::HTTP::filltemplatefile($class->page, $paramRef);
+}
+
+1;
+
+__END__
