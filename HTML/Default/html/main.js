98fc54c2bed14a798bd08471c47f6402eb7a266a
Ben Klaas <bklaas@logitech.com>
2007-10-04 15:41:23 +0000

    Changed resized cover.png calls to cover.gif.
    Gif transparency will work properly after GD gets upgraded in SC from 2.30 to current, which is in process. 
    Eventually this should be reworked so gif is only used when IE is detected, since it's the one with png issues.
    
    
    
    git-svn-id: file:///home/awy/mirror/slim/trunk/server@13546 62299810-d8cb-41fd-93b7-d32162e5a4a4

--- HTML/Default/html/main.js
+++ HTML/Default/html/main.js
@@ -851,8 +851,8 @@ Player = function(){
 					Ext.get('ctrlCurrentSongInfoCollapsed').update(currentTitle);
 
 					if (result.playlist_loop[0].id && (el = Ext.get('ctrlCurrentArt'))) {
-						var coverart = '<a href="' + webroot + 'browsedb.html?hierarchy=album,track&amp;level=1&amp;album.id=' + result.playlist_loop[0].album_id + '&amp;player=' + player + '" target="browser"><img src="/music/' + result.playlist_loop[0].id + '/cover_96x96_f.png"></a>';
-						var popup    = '<img src="/music/' + result.playlist_loop[0].id + '/cover_250x250_f.png" width="250">';
+						var coverart = '<a href="' + webroot + 'browsedb.html?hierarchy=album,track&amp;level=1&amp;album.id=' + result.playlist_loop[0].album_id + '&amp;player=' + player + '" target="browser"><img src="/music/' + result.playlist_loop[0].id + '/cover_96x96_f.gif"></a>';
+						var popup    = '<img src="/music/' + result.playlist_loop[0].id + '/cover_250x250_f.gif" width="250">';
 
 						if (result.playlist_loop[0].artwork_url) {
 							coverart = '<img src="' + result.playlist_loop[0].artwork_url + '" height="96" width="96" />';
@@ -887,7 +887,7 @@ Player = function(){
 					Ext.get('ctrlBitrate').update('');
 					Ext.get('ctrlCurrentArtist').update('');
 					Ext.get('ctrlCurrentAlbum').update('');
-					Ext.get('ctrlCurrentArt').update('<img src="/music/0/cover_96x96_f.png">');
+					Ext.get('ctrlCurrentArt').update('<img src="/music/0/cover_96x96_f.gif">');
 				}
 
 				playerStatus = {
