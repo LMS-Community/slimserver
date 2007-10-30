8259da619f69dffeeb02455fb7e34c4ceecee125
Andy Grundman <agrundman@slimdevices.com>
2007-10-30 16:11:03 +0000

    Remove some legacy code in Pandora
    git-svn-id: file:///home/awy/mirror/slim/trunk/server@14209 62299810-d8cb-41fd-93b7-d32162e5a4a4

--- Slim/Plugin/Pandora/ProtocolHandler.pm
+++ Slim/Plugin/Pandora/ProtocolHandler.pm
@@ -194,9 +194,6 @@ sub gotNextTrack {
 	# after the next track begins buffering
 	$client->pluginData( prevTrack => $client->pluginData('currentTrack') );
 	
-	# Save the time difference between SN and SqueezeCenter
-	$track->{timediff} = $track->{now} - time();
-	
 	# Save metadata for this track, and save the previous track
 	$client->pluginData( currentTrack => $track );
 	
