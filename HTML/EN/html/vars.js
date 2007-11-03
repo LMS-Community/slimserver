1e62e613059bf8485a933d4de205cbad70ad636e
Kevin Deane-Freeman <slim-mail@deane-freeman.com>
2007-11-03 01:26:28 +0000

    fix orderByUrl
    Bug: 6010
    git-svn-id: file:///home/awy/mirror/slim/trunk/server@14334 62299810-d8cb-41fd-93b7-d32162e5a4a4

--- HTML/EN/html/vars.js
+++ HTML/EN/html/vars.js
@@ -7,4 +7,4 @@
 	var statusroot    = "[% statusroot %]";
 	var browserTarget;
 	[% IF browserTarget %]browserTarget = "[% browserTarget %]";[% END %]
-	var orderByUrl    = 'browsedb.html?hierarchy=[% hierarchy %]&amp;level=[% level %][% attributes %]&amp;artwork=[% IF artwork; artwork; ELSE; '0'; END %]&amp;player=' + player;
+	var orderByUrl    = 'browsedb.html?hierarchy=[% hierarchy %]&level=[% level %][% attributes %]&artwork=[% IF artwork; artwork; ELSE; '0'; END %]&player=' + player;
