fa261380da6127e183adc3442c89b56bb7184098
Andy Grundman <agrundman@slimdevices.com>
2008-01-18 23:38:07 +0000

    Always use the master's track start time here
    git-svn-id: file:///home/awy/mirror/slim/trunk/server@16464 62299810-d8cb-41fd-93b7-d32162e5a4a4

--- Slim/Player/Squeezebox.pm
+++ Slim/Player/Squeezebox.pm
@@ -286,7 +286,7 @@ sub buffering {
 	
 	# If the track has started, stop displaying buffering status
 	# currentPlaylistChangeTime is set to time() after a track start event
-	if ( $client->trackStartTime() > $client->bufferStarted() ) {
+	if ( $client->masterOrSelf->trackStartTime() > $client->bufferStarted() ) {
 		$client->update();
 		return;
 	}
