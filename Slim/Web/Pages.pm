3d3dbb8738121226685e0004ce9ab04e9b1cfae1
Michael Herger <mherger@logitech.com>
2008-01-28 16:10:00 +0000

    don't need to fetch the title any more, this is done in getMetadataFor()
    Bug: 6482
    git-svn-id: file:///home/awy/mirror/slim/branches/7.0/server@16833 62299810-d8cb-41fd-93b7-d32162e5a4a4

--- Slim/Web/Pages.pm
+++ Slim/Web/Pages.pm
@@ -292,11 +292,6 @@ sub addSongInfo {
 		if ( $handler && $handler->can('getMetadataFor') ) {
 			$params->{'plugin_meta'} = $handler->getMetadataFor( $client, $url );
 
-			my $current_title = Slim::Music::Info::getCurrentTitle($client, $track->url, 'web');
-			if ( $current_title && !$params->{'plugin_meta'}->{'title'} ) {
-				$params->{'plugin_meta'}->{'title'} = $current_title;
-			}
-
 			# Strip extension from icon path
 			if ( $params->{'plugin_meta'}->{'icon'} ) {
 				$params->{'plugin_meta'}->{'icon'} =~ s/\.png$//;
