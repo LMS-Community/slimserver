30f2d8675d6efa749ce2bf3bf4cac236c434f598
Michael Herger <mherger@logitech.com>
2008-02-01 13:02:31 +0000

    migrate playtrackalbum from former server wide to per player setting
    Bug: 6872
    git-svn-id: file:///home/awy/mirror/slim/branches/7.0/server@17078 62299810-d8cb-41fd-93b7-d32162e5a4a4

--- Slim/Utils/Prefs.pm
+++ Slim/Utils/Prefs.pm
@@ -331,6 +331,18 @@ sub init {
 		1;
 	} );
 	
+	# migrate 'play other songs' pref from server to per-player
+	$prefs->migrateClient( 4, sub {
+		my ( $cprefs, $client ) = @_;
+		my $playtrackalbum = preferences('server')->get('playtrackalbum');
+		
+		# copy server pref as a default client pref
+		unless (defined $cprefs->get( 'playtrackalbum' )) {
+			$cprefs->set( 'playtrackalbum', $playtrackalbum );
+		}
+		1;
+	} );
+
 	# initialise any new prefs
 	$prefs->init(\%defaults);
 
