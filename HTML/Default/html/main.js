8de7c0fec336d500fa17d26e7434a3286e10f633
Michael Herger <mherger@logitech.com>
2008-02-04 21:23:36 +0000

    use same link for cover art as for title (songinfo.html)
    Bug: 6938
    git-svn-id: file:///home/awy/mirror/slim/branches/7.0/server@17188 62299810-d8cb-41fd-93b7-d32162e5a4a4

--- HTML/Default/html/main.js
+++ HTML/Default/html/main.js
@@ -1025,13 +1025,13 @@ Player = function(){
 				Ext.get('ctrlCurrentSongInfoCollapsed').update(currentTitle);
 
 				if (result.playlist_loop[0].id && (el = Ext.get('ctrlCurrentArt'))) {
-						
-					var coverart = '<a href="' + webroot + 'browsedb.html?hierarchy=album,track&amp;level=1&amp;album.id=' + result.playlist_loop[0].album_id + '&amp;player=' + player + '" target="browser"><img src="/music/' + result.playlist_loop[0].id + '/cover_96x96_p.' + coverFileSuffix + '"></a>';
+
+					var coverart = '<a href="' + webroot + infoLink + '?player=' + player + '&amp;item=' + result.playlist_loop[0].id + '" target="browser"><img src="/music/' + result.playlist_loop[0].id + '/cover_96x96_p.' + coverFileSuffix + '"></a>';
 					// the bg color must match the qtip's background color, as otherwise the image shows white on white
 					var popup    = '<img src="/music/' + result.playlist_loop[0].id + '/cover_250xX_f_e0e8f3.jpg" width="250">';
 
 					if (result.playlist_loop[0].artwork_url) {
-						coverart = '<img src="' + result.playlist_loop[0].artwork_url + '" height="96" width="96" />';
+						coverart = '<a href="' + webroot + infoLink + '?player=' + player + '&amp;item=' + result.playlist_loop[0].id + '" target="browser"><img src="' + result.playlist_loop[0].artwork_url + '" height="96" width="96" /></a>';
 						popup    = '<img src="' + result.playlist_loop[0].artwork_url + '" width="250" />';
 					}
 
