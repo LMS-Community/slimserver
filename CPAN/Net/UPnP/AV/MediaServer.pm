b8e9518df4d1a1be70da2f6f33a1dc6934ad5f7f
Andy Grundman <agrundman@slimdevices.com>
2008-04-01 21:06:24 +0000

    Bug 6602, support 3-digit version number comparison via version.pm.  Update to Net::UPnP 1.2.1
    Bug: 6602
    git-svn-id: file:///home/awy/mirror/slim/7.0/trunk/server@18269 62299810-d8cb-41fd-93b7-d32162e5a4a4

---
+++ CPAN/Net/UPnP/AV/MediaServer.pm
@@ -0,0 +1,376 @@
+package Net::UPnP::AV::MediaServer;
+
+#-----------------------------------------------------------------
+# Net::UPnP::AV::MediaServer
+#-----------------------------------------------------------------
+
+use strict;
+use warnings;
+
+use Net::UPnP::HTTP;
+use Net::UPnP::Device;
+use Net::UPnP::Service;
+use Net::UPnP::AV::Container;
+use Net::UPnP::AV::Item;
+
+use vars qw($_DEVICE $DEVICE_TYPE $CONTENTDIRECTORY_SERVICE_TYPE);
+
+$_DEVICE = 'device';
+
+$DEVICE_TYPE = 'urn:schemas-upnp-org:device:MediaServer:1';
+$CONTENTDIRECTORY_SERVICE_TYPE = 'urn:schemas-upnp-org:service:ContentDirectory:1';
+
+#------------------------------
+# new
+#------------------------------
+
+sub new {
+	my($class) = shift;
+	my($this) = {
+		$Net::UPnP::AV::MediaServer::_DEVICE => undef,
+	};
+	bless $this, $class;
+}
+
+#------------------------------
+# device
+#------------------------------
+
+sub setdevice() {
+	my($this) = shift;
+	if (@_) {
+		$this->{$Net::UPnP::AV::MediaServer::_DEVICE} = $_[0];
+	}
+}
+
+sub getdevice() {
+	my($this) = shift;
+	$this->{$Net::UPnP::AV::MediaServer::_DEVICE};
+}
+
+#------------------------------
+# browse
+#------------------------------
+
+sub browse {
+	my($this) = shift;
+	my %args = (
+		ObjectID => 0,	
+		BrowseFlag => 'BrowseDirectChildren',
+		Filter => '*',
+		StartingIndex => 0,
+		RequestedCount => 0,
+		SortCriteria => '',
+		@_,
+	);
+	
+	my ($objid, $browseFlag, $filter, $startIdx, $reqCount, $sortCriteria) = @_;
+	my (
+		$dev,
+		$condir_service,
+		%req_arg,
+		$action_res,
+	);
+	
+	$dev = $this->getdevice();
+	$condir_service = $dev->getservicebyname($Net::UPnP::AV::MediaServer::CONTENTDIRECTORY_SERVICE_TYPE);
+	
+	%req_arg = (
+			'ObjectID' => $args{ObjectID},
+			'BrowseFlag' => $args{BrowseFlag},
+			'Filter' => $args{Filter},
+			'StartingIndex' => $args{StartingIndex},
+			'RequestedCount' => $args{RequestedCount},
+			'SortCriteria' => $args{SortCriteria},
+		);
+	
+	$condir_service->postaction("Browse", \%req_arg);
+}
+
+sub browsedirectchildren {
+	my($this) = shift;
+	my %args = (
+		ObjectID => 0,	
+		Filter => '*',
+		StartingIndex => 0,
+		RequestedCount => 0,
+		SortCriteria => '',
+		@_,
+	);
+	$this->browse (
+			ObjectID => $args{ObjectID},
+			BrowseFlag => 'BrowseDirectChildren',
+			Filter => $args{Filter},
+			StartingIndex => $args{StartingIndex},
+			RequestedCount => $args{RequestedCount},
+			SortCriteria => $args{SortCriteria}
+			);
+}
+
+sub browsemetadata {
+	my($this) = shift;
+	my %args = (
+		ObjectID => 0,	
+		Filter => '*',
+		StartingIndex => 0,
+		RequestedCount => 0,
+		SortCriteria => '',
+		@_,
+	);
+	$this->browse (
+			ObjectID => $args{ObjectID},
+			BrowseFlag => 'BrowseMetadata',
+			Filter => $args{Filter},
+			StartingIndex => $args{StartingIndex},
+			RequestedCount => $args{RequestedCount},
+			SortCriteria => $args{SortCriteria}
+			);
+}
+
+#------------------------------
+# getdirectchildren
+#------------------------------
+
+sub getcontentlist {
+	my($this) = shift;
+	my %args = (
+		ObjectID => 0,	
+		Filter => '*',
+		StartingIndex => 0,
+		RequestedCount => 0,
+		SortCriteria => '',
+		@_,
+	);
+	my (
+		@content_list,
+		$action_res,
+		$arg_list,
+		$result,
+		$content,
+		$container,
+		$item,
+	);
+	
+	@content_list = ();
+	$action_res = $this->browsedirectchildren(
+			ObjectID => $args{ObjectID},
+			Filter => $args{Filter},
+			StartingIndex => $args{StartingIndex},
+			RequestedCount => $args{RequestedCount},
+			SortCriteria => $args{SortCriteria}
+			);
+	if ($action_res->getstatuscode() != 200) {
+		return @content_list;
+	}
+	$arg_list = $action_res->getargumentlist();
+	unless ($arg_list->{'Result'}) {
+		return @content_list;
+	}
+	$result = $arg_list->{'Result'};
+
+	while ($result =~ m/<container(.*?)<\/container>/sgi) {
+		$content = $1;
+		$container = Net::UPnP::AV::Container->new();
+		if ($content =~ m/id=\"(.*?)\"/si) {
+			$container->setid($1);
+		}
+		if ($content =~ m/<dc:title>(.*)<\/dc:title>/si) {
+			$container->settitle($1);
+		}
+		if ($content =~ m/<dc:date>(.*)<\/dc:date>/si) {
+			$container->setdate($1);
+		}
+		push (@content_list, $container);
+		#print "container(" . $container->getid() . ") = " . $container->gettitle() . "\n";
+		#print $1;
+	}
+
+	while ($result =~ m/<item(.*?)<\/item>/sgi) {
+		$content = $1;
+		$item= Net::UPnP::AV::Item->new();
+		if ($content =~ m/id=\"(.*?)\"/si) {
+			$item->setid($1);
+		}
+		if ($content =~ m/<dc:title>(.*)<\/dc:title>/si) {
+			$item->settitle($1);
+		}
+		if ($content =~ m/<dc:date>(.*)<\/dc:date>/si) {
+			$item->setdate($1);
+		}
+		if ($content =~ m/<res[^>]*>(.*?)<\/res>/si) {
+			$item->seturl(Net::UPnP::HTTP::xmldecode($1));
+		}
+		if ($content =~ m/protocolInfo=\"http-get:[^:]*:([^:]*):.*\"/si) {
+			$item->setcontenttype($1);
+		}
+		elsif ($content =~ m/protocolInfo=\"[^:]*:[^:]:([^:]*):.*\"/si) {
+			$item->setcontenttype($1);
+		}
+		push (@content_list, $item);
+	}
+
+	@content_list;
+}
+
+#------------------------------
+# getsystemupdateid
+#------------------------------
+
+sub getsystemupdateid {
+	my($this) = shift;
+
+	my (
+		$dev,
+		$condir_service,
+		$query_res,
+	);
+	
+	$dev = $this->getdevice();
+	$condir_service = $dev->getservicebyname($Net::UPnP::AV::MediaServer::CONTENTDIRECTORY_SERVICE_TYPE);
+	
+	$query_res = $condir_service->postquery("SystemUpdateID");
+
+	if ($query_res->getstatuscode() != 200) {
+		return "";
+	}
+	
+	return $query_res->getvalue();
+}
+
+1;
+
+__END__
+
+=head1 NAME
+
+Net::UPnP::AV::MediaServer - Perl extension for UPnP.
+
+=head1 SYNOPSIS
+
+    use Net::UPnP::ControlPoint;
+    use Net::UPnP::AV::MediaServer;
+
+    my $obj = Net::UPnP::ControlPoint->new();
+
+    @dev_list = $obj->search(st =>'upnp:rootdevice', mx => 3);
+
+    $devNum= 0;
+    foreach $dev (@dev_list) {
+        $device_type = $dev->getdevicetype();
+        if  ($device_type ne 'urn:schemas-upnp-org:device:MediaServer:1') {
+            next;
+        }
+        print "[$devNum] : " . $dev->getfriendlyname() . "\n";
+        unless ($dev->getservicebyname('urn:schemas-upnp-org:service:ContentDirectory:1')) {
+            next;
+        }
+        $mediaServer = Net::UPnP::AV::MediaServer->new();
+        $mediaServer->setdevice($dev);
+        @content_list = $mediaServer->getcontentlist(ObjectID => 0);
+        foreach $content (@content_list) {
+            print_content($mediaServer, $content, 1);
+        }
+        $devNum++;
+    }
+
+    sub print_content {
+        my ($mediaServer, $content, $indent) = @_;
+        my $id = $content->getid();
+        my $title = $content->gettitle();
+        for ($n=0; $n<$indent; $n++) {
+            print "\t";
+        }
+        print "$id = $title";
+        if ($content->isitem()) {
+            print " (" . $content->geturl();
+            if (length($content->getdate())) {
+                print " - " . $content->getdate();
+            }
+            print " - " . $content->getcontenttype() . ")";
+        }
+        print "\n";
+        unless ($content->iscontainer()) {
+            return;
+        }
+        @child_content_list = $mediaServer->getcontentlist(ObjectID => $id );
+        if (@child_content_list <= 0) {
+            return;
+        }
+        $indent++;
+        foreach my $child_content (@child_content_list) {
+            print_content($mediaServer, $child_content, $indent);
+        }
+    }
+
+=head1 DESCRIPTION
+
+The package is a extention UPnP/AV media server.
+
+=head1 METHODS
+
+=over 4
+
+=item B<new> - create new Net::UPnP::AV::MediaServer.
+
+    $mservier = Net::UPnP::AV::MediaServer();
+
+Creates a new object. Read `perldoc perlboot` if you don't understand that.
+
+The new object is not associated with any UPnP devices. Please use setdevice() to set the device.
+
+=item B<setdevice> - set a UPnP devices
+
+    $mservier->setdevice($dev);
+
+Set a device to the object.
+
+=item B<browse> - browse the content directory.
+	
+    @action_response = $mservier->browse(
+                                        ObjectID => $objid, # 0	
+                                        BrowseFlag => $browseFlag, # 'BrowseDirectChildren'
+                                        Filter => $filter, # "*'
+                                        StartingIndex => $startIndex, # 0
+                                        RequestedCount => $reqCount, # 0
+                                        SortCriteria => $sortCrit # ''
+                                        );
+
+Browse the content directory and return the action response, L<Net::UPnP::ActionResponse>.
+
+=item B<getcontentlist> - get the content list.
+	
+    @content_list = $mservier->getcontentlist(
+                                        ObjectID => $objid, # 0	
+                                        Filter => $filter, # "*'
+                                        StartingIndex => $startIndex, # 0
+                                        RequestedCount => $reqCount, # 0
+                                        SortCriteria => $sortCrit # ''
+                                        );
+
+Browse the content directory and return the content list. Please see L<Net::UPnP::AV::Content>, L<Net::UPnP::AV::Item> and L<Net::UPnP::AV::Container>.
+
+=back
+
+=head1 SEE ALSO
+
+L<Net::UPnP::AV::Content>
+
+L<Net::UPnP::AV::Item>
+
+L<Net::UPnP::AV::Container>
+
+=head1 AUTHOR
+
+Satoshi Konno
+skonno@cybergarage.org
+
+CyberGarage
+http://www.cybergarage.org
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright (C) 2005 by Satoshi Konno
+
+It may be used, redistributed, and/or modified under the terms of BSD License.
+
+=cut
---
+++ CPAN/Net/UPnP/Service.pm
@@ -0,0 +1,448 @@
+package Net::UPnP::Service;
+
+#-----------------------------------------------------------------
+# Net::Net::UPnP::Service
+#-----------------------------------------------------------------
+
+use strict;
+use warnings;
+
+use Net::UPnP;
+use Net::UPnP::ActionResponse;
+use Net::UPnP::QueryResponse;
+
+use vars qw($_DEVICE $_DEVICE_DESCRIPTION $SERVICETYPE $SERVICEID $SCPDURL $CONTROLURL $EVENTSUBURL);
+
+$_DEVICE = 'device';
+$_DEVICE_DESCRIPTION = 'device_description';
+
+$SERVICETYPE = 'serviceType';
+$SERVICEID = 'serviceId';
+$SCPDURL = 'SCPDURL';
+$CONTROLURL = 'controlURL';
+$EVENTSUBURL = 'eventSubURL';
+
+#------------------------------
+# new
+#------------------------------
+
+sub new {
+	my($class) = shift;
+	my($this) = {
+		$Net::UPnP::Service::_DEVICE  => undef,
+		$Net::UPnP::Service::_DEVICE_DESCRIPTION => '',
+	};
+	bless $this, $class;
+}
+
+#------------------------------
+# device
+#------------------------------
+
+sub setdevice() {
+	my($this) = shift;
+	if (@_) {
+		$this->{$Net::UPnP::Service::_DEVICE} = $_[0];
+	}
+}
+
+sub getdevice() {
+	my($this) = shift;
+	$this->{$Net::UPnP::Service::_DEVICE};
+}
+
+#------------------------------
+# device description
+#------------------------------
+
+sub setdevicedescription() {
+	my($this) = shift;
+	$this->{$Net::UPnP::Service::_DEVICE_DESCRIPTION} = $_[0];
+ }
+
+sub getdevicedescription() {
+	my($this) = shift;
+	my %args = (
+		name => undef,	
+		@_,
+	);
+	if ($args{name}) {
+		unless ($this->{$Net::UPnP::Service::_DEVICE_DESCRIPTION} =~ m/<$args{name}>(.*)<\/$args{name}>/i) {
+			return '';
+		}
+	 	return $1;
+	}
+	$this->{$Net::UPnP::Service::_DEVICE_DESCRIPTION};
+ }
+
+#------------------------------
+# getservicetype
+#------------------------------
+
+sub getservicetype() {
+	my($this) = shift;
+	$this->getdevicedescription(name => $Net::UPnP::Service::SERVICETYPE);
+ }
+
+#------------------------------
+# getserviceid
+#------------------------------
+
+sub getserviceid() {
+	my($this) = shift;
+	$this->getdevicedescription(name => $Net::UPnP::Service::SERVICEID);
+ }
+
+#------------------------------
+# getscpdurl
+#------------------------------
+
+sub getscpdurl() {
+	my($this) = shift;
+	$this->getdevicedescription(name => $Net::UPnP::Service::SCPDURL);
+ }
+
+#------------------------------
+# getcontrolurl
+#------------------------------
+
+sub getcontrolurl() {
+	my($this) = shift;
+	$this->getdevicedescription(name => $Net::UPnP::Service::CONTROLURL);
+ }
+
+#------------------------------
+# geteventsuburl
+#------------------------------
+
+sub geteventsuburl() {
+	my($this) = shift;
+	$this->getdevicedescription(name => $Net::UPnP::Service::EVENTSUBURL);
+ }
+
+#------------------------------
+# getposturl
+#------------------------------
+
+sub getposturl() {
+	my($this) = shift;
+	my ($ctrl_url) = @_;
+	my (
+		$dev,
+		$location_url,
+		$url_base,
+	);
+	
+	$dev = $this->getdevice();
+	
+	$location_url = $dev->getlocation();
+	$url_base = $dev->geturlbase();
+	$ctrl_url = $this->getcontrolurl();
+
+	#print "$location_url\n";
+	#print "$url_base\n";
+	#print "$ctrl_url\n";
+		
+	unless ($ctrl_url =~ m/http:\/\/(.*)/i) {
+		if (0 < length($url_base)) {
+			# Thanks for Thus0 (2005/01/12)
+			if (rindex($url_base, '/') == (length($url_base)-1) && index($ctrl_url, '/') == 0) {
+				$ctrl_url = $url_base . substr($ctrl_url, 1);
+			} else {
+				$ctrl_url = $url_base . $ctrl_url;
+			}
+		}
+		else {
+			if ($location_url =~ m/http:\/\/([0-9a-z.]+)[:]*([0-9]*)\/(.*)/i) {
+				if (defined($3) && 0 < length($3)) {
+					$ctrl_url = "http:\/\/" . $1 . ":" . $2 . $ctrl_url;
+				} else {
+					$ctrl_url = "http:\/\/" . $1 . ":" . $2 . "\/" . $ctrl_url;
+				}
+			} else {
+				$ctrl_url = $location_url .  $ctrl_url;
+			}
+		}
+	}
+	
+	return $ctrl_url;
+}
+
+#------------------------------
+# postaction
+#------------------------------
+
+sub postaction() {
+	my($this) = shift;
+	my ($action_name, $action_arg) = @_;
+	my (
+		$dev,
+		$ctrl_url,
+		$service_type,
+		$soap_action,
+		$soap_content,
+		$arg_name,
+		$arg_value,
+		$post_addr,
+		$post_port,
+		$post_path,
+		$http_req,
+		$post_res,
+		$action_res,
+		$key,
+	);
+	
+	$action_res = Net::UPnP::ActionResponse->new();
+	
+	$dev = $this->getdevice();
+	$ctrl_url = $this->getcontrolurl();
+	$ctrl_url = $this->getposturl($ctrl_url);
+	
+	unless ($ctrl_url =~ m/http:\/\/([0-9a-z.]+)[:]*([0-9]*)\/(.*)/i) {
+		#print "Invalid URL : $ctrl_url\n";
+		$post_res = Net::UPnP::HTTPResponse->new();
+		$action_res->sethttpresponse($post_res);
+		return $action_res;
+	}
+	$post_addr = $1;
+	$post_port = $2;
+	if (index($3, '/') == 0) {
+		$post_path = $3;
+	}
+	else {
+		$post_path = "\/" . $3;
+	}
+
+	$service_type = $this->getservicetype();
+	$soap_action = "\"" . $service_type . "#" . $action_name . "\"";
+
+
+$soap_content = <<"SOAP_CONTENT";
+<?xml version=\"1.0\" encoding=\"utf-8\"?>
+<s:Envelope xmlns:s=\"http:\/\/schemas.xmlsoap.org\/soap\/envelope\/\" s:encodingStyle=\"http:\/\/schemas.xmlsoap.org\/soap\/encoding/\">
+\t<s:Body>
+\t\t<u:$action_name xmlns:u=\"$service_type\">
+SOAP_CONTENT
+
+	if (ref $action_arg) {
+		while (($arg_name, $arg_value) = each (%{$action_arg} ) ) {
+			if (length($arg_value) <= 0) {
+				$soap_content .= "\t\t\t<$arg_name \/>\n";
+				next;
+			}
+			$soap_content .= "\t\t\t<$arg_name>$arg_value<\/$arg_name>\n";
+		}
+	}
+
+$soap_content .= <<"SOAP_CONTENT";
+\t\t</u:$action_name>
+\t</s:Body>
+</s:Envelope>
+SOAP_CONTENT
+
+	$http_req = Net::UPnP::HTTP->new();
+	$post_res = $http_req->postsoap($post_addr, $post_port, $post_path, $soap_action, $soap_content);
+
+	$action_res->sethttpresponse($post_res);
+	
+	return $action_res;
+}
+
+#------------------------------
+# postcontrol
+#------------------------------
+
+sub postcontrol() {
+	my($this) = shift;
+	my ($action_name, $action_arg) = @_;
+	return $this->postaction($action_name, $action_arg);
+}
+
+#------------------------------
+# postquery
+#------------------------------
+
+sub postquery() {
+	my($this) = shift;
+	my ($var_name) = @_;
+	my (
+		$dev,
+		$ctrl_url,
+		$service_type,
+		$soap_action,
+		$soap_content,
+		$post_addr,
+		$post_port,
+		$post_path,
+		$http_req,
+		$post_res,
+		$query_res,
+	);
+	
+	$query_res = Net::UPnP::QueryResponse->new();
+	
+	$dev = $this->getdevice();
+	$ctrl_url = $this->getcontrolurl();
+	$ctrl_url = $this->getposturl($ctrl_url);
+	
+	unless ($ctrl_url =~ m/http:\/\/([0-9a-z.]+)[:]*([0-9]*)\/(.*)/i) {
+		#print "Invalid URL : $ctrl_url\n";
+		$post_res = Net::UPnP::HTTPResponse->new();
+		$query_res->sethttpresponse($post_res);
+		return $query_res;
+	}
+	$post_addr = $1;
+	$post_port = $2;
+	if (index($3, '/') == 0) {
+		$post_path = $3;
+	}
+	else {
+		$post_path = "\/" . $3;
+	}
+	
+	$service_type = $this->getservicetype();
+	$soap_action = "\"urn:schemas-upnp-org:control-1-0#QueryStateVariable\"";
+
+$soap_content = <<"SOAP_CONTENT";
+<?xml version=\"1.0\" encoding=\"utf-8\"?>
+<s:Envelope xmlns:s=\"http:\/\/schemas.xmlsoap.org\/soap\/envelope\/\" s:encodingStyle=\"http:\/\/schemas.xmlsoap.org\/soap\/encoding/\">
+\t<s:Body>
+\t\t<u:QueryStateVariable xmlns:u=\"urn:schemas-upnp-org:control-1-0\">
+\t\t\t<u:varName>$var_name</u:varName>
+\t\t</u:QueryStateVariable>
+\t</s:Body>
+</s:Envelope>
+SOAP_CONTENT
+
+	$http_req = Net::UPnP::HTTP->new();
+	$post_res = $http_req->postsoap($post_addr, $post_port, $post_path, $soap_action, $soap_content);
+
+	$query_res->sethttpresponse($post_res);
+	
+	return $query_res;
+}
+
+1;
+
+__END__
+
+=head1 NAME
+
+Net::UPnP::Service - Perl extension for UPnP.
+
+=head1 SYNOPSIS
+
+    use Net::UPnP::ControlPoint;
+
+    my $obj = Net::UPnP::ControlPoint->new();
+
+    @dev_list = $obj->search(st =>'upnp:rootdevice', mx => 3);
+
+    $devNum= 0;
+    foreach $dev (@dev_list) {
+        $device_type = $dev->getdevicetype();
+        if  ($device_type ne 'urn:schemas-upnp-org:device:MediaServer:1') {
+            next;
+        }
+        print "[$devNum] : " . $dev->getfriendlyname() . "\n";
+        unless ($dev->getservicebyname('urn:schemas-upnp-org:service:ContentDirectory:1')) {
+            next;
+        }
+        $condir_service = $dev->getservicebyname('urn:schemas-upnp-org:service:ContentDirectory:1');
+        unless (defined(condir_service)) {
+            next;
+        }
+        %action_in_arg = (
+                'ObjectID' => 0,
+                'BrowseFlag' => 'BrowseDirectChildren',
+                'Filter' => '*',
+                'StartingIndex' => 0,
+                'RequestedCount' => 0,
+                'SortCriteria' => '',
+            );
+        $action_res = $condir_service->postcontrol('Browse', \%action_in_arg);
+        unless ($action_res->getstatuscode() == 200) {
+        	next;
+        }
+        $actrion_out_arg = $action_res->getargumentlist();
+        unless ($actrion_out_arg->{'Result'}) {
+            next;
+        }
+        $result = $actrion_out_arg->{'Result'};
+        while ($result =~ m/<dc:title>(.*?)<\/dc:title>/sgi) {
+            print "\t$1\n";
+        }
+        $devNum++;
+    }
+
+=head1 DESCRIPTION
+
+The package is used a object of UPnP service.
+
+=head1 METHODS
+
+=over 4
+
+=item B<getdevice> - get the device.
+
+    $description = $service->getdevice();
+
+Get the parent device of the service.
+
+=item B<getdevicedescription> - get the service description of the device description.
+
+    $description = $service->getdevicedescription(
+    	                        name => $name # undef
+                             );
+
+Get the service description of the device description. 
+	
+The function returns the all description when the name parameter is not specified, otherwise return a value the specified name.
+
+=item B<getservicetype> - get the service type.
+
+    $service_type = $service->getservicetype();
+
+Get the service type.
+
+=item B<getserviceid> - get the service id.
+
+    $service_id = $service->getserviceid();
+
+Get the service id.
+
+=item B<postaction> - post a action control.
+
+    $action_res = $service->postcontrol($action_name, \%action_arg);
+
+Post a action control to the device, and return L<Net::UPnP::ActionResponse>.
+
+The method was renamed from postcontrol(), but the old name is deprecated.
+	
+=item B<postquery> - post a query control.
+
+    $query_res = $service->postcontrol($var_name);
+
+Post a query control to the device, and return L<Net::UPnP::QueryResponse>.
+
+=back
+
+=head1 SEE ALSO
+
+L<Net::UPnP::ActionResponse>
+
+=head1 AUTHOR
+
+Satoshi Konno
+skonno@cybergarage.org
+
+CyberGarage
+http://www.cybergarage.org
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright (C) 2005 by Satoshi Konno
+
+It may be used, redistributed, and/or modified under the terms of BSD License.
+
+=cut
