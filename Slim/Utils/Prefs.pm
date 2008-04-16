34f453a7301620396f73af4c61482e09015b8d56
Michael Herger <mherger@logitech.com>
2008-04-16 16:52:51 +0000

    don't utf8 decode custom artwork file names (coverArt)
    Bug: 7815
    git-svn-id: file:///home/awy/mirror/slim/7.0/trunk/server@18822 62299810-d8cb-41fd-93b7-d32162e5a4a4

--- Slim/Utils/Prefs.pm
+++ Slim/Utils/Prefs.pm
@@ -100,7 +100,7 @@ $path ||= Slim::Utils::OSDetect::dirsFor('prefs');
 my $prefs = preferences('server');
 
 # make sure these server prefs has the utf8flag turned off before they get used
-$prefs->setUtf8Off(qw(audiodir playlistdir cachedir));
+$prefs->setUtf8Off(qw(audiodir playlistdir cachedir coverArt));
 
 
 =head2 preferences( $namespace )
