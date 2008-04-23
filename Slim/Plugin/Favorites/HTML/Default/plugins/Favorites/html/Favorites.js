7666d1f12e8526c41a30d32a90c08ae33d35337b
Michael Herger <mherger@logitech.com>
2008-04-23 13:48:41 +0000

    fix Favorites - missed one file
    Bug: 7951
    git-svn-id: file:///home/awy/mirror/slim/7.1/trunk/server@19088 62299810-d8cb-41fd-93b7-d32162e5a4a4

--- Slim/Plugin/Favorites/HTML/Default/plugins/Favorites/html/Favorites.js
+++ Slim/Plugin/Favorites/HTML/Default/plugins/Favorites/html/Favorites.js
@@ -1,6 +1,8 @@
 var Favorites = function(){
 	return {
 		init : function(session, offset){
+			SqueezeJS.UI.ScrollPanel.init();
+
 			new SqueezeJS.UI.Sortable({
 				el: 'draglist',
 				selector: 'ol#draglist li',
@@ -36,3 +38,49 @@ var Favorites = function(){
 		}
 	}
 }();
+
+// XXX some legacy stuff - should eventually go away
+
+// request and update with new list html, requires a 'mainbody' div defined in the document
+// templates should use the ajaxUpdate param to block headers and footers.
+function ajaxUpdate(url, params, callback) {
+	var el = Ext.get('mainbody');
+
+	if (el) {
+		var um = el.getUpdateManager();
+
+		if (um)
+			um.loadScripts = true;
+
+		el.load(url, params + '&ajaxUpdate=1&player=' + player, callback || SqueezeJS.UI.ScrollPanel.init);
+	}
+}
+
+// some prototype JS compatibility classes
+var Element = function(){
+	return {
+		remove: function(el) {
+			if (el = Ext.get(el))
+				el.remove();
+			SqueezeJS.UI.ScrollPanel.init();
+		}
+	}
+}();
+
+// pass an array of div element ids to be hidden on the page
+function hideElements(elements) {
+	showElements(elements, 'none');
+}
+
+// pass an array of div element ids to be shown on the page
+function showElements(elements, style) {
+	var el;
+
+	if (!style)
+		style = 'block';
+
+	for (var i = 0; i < elements.length; i++) {
+		if (el = Ext.get(elements[i]))
+			el.setStyle('display', style);
+	}
+}
