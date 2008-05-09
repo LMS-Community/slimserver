51ce5dc39219ade7ba4ad1810168abba956bf37f
Michael Herger <mherger@logitech.com>
2008-05-09 12:47:49 +0000

    Queries were returning Slim::Utils::Strings::string() values in most places, ignoring client specific language settings; add $request->string() method, returning client specific string if possible
    Bug: 6810
    git-svn-id: file:///home/awy/mirror/slim/7.0/trunk/server@19579 62299810-d8cb-41fd-93b7-d32162e5a4a4

--- Slim/Control/Request.pm
+++ Slim/Control/Request.pm
@@ -1493,6 +1493,16 @@ sub sliceResultLoop {
 	}
 }
 
+sub string {
+	my $self   = shift;
+	my $string = shift;
+
+	my $client = $self->client();
+
+	return $client ? $client->string($string) : Slim::Utils::Strings::string($string);
+}
+
+
 # sortResultLoop
 # sort the result loop $loop using field $field.
 sub sortResultLoop {
