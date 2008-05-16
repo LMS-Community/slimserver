cee19418b19ffe35129fde43415997f2928c46fb
Andy Grundman <agrundman@slimdevices.com>
2008-05-16 13:54:55 +0000

    Bump version to 7.0.2
    git-svn-id: file:///home/awy/mirror/slim/7.0/trunk/server@19779 62299810-d8cb-41fd-93b7-d32162e5a4a4

--- slimserver.pl
+++ slimserver.pl
@@ -216,7 +216,7 @@ our @AUTHORS = (
 
 my $prefs        = preferences('server');
 
-our $VERSION     = '7.0.1';
+our $VERSION     = '7.0.2';
 our $REVISION    = undef;
 our $BUILDDATE   = undef;
 our $audiodir    = undef;
