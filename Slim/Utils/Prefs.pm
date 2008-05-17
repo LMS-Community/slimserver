0118a67a4e65bffbd58e6ed3185324e218493c62
Adrian Smith <triode1@btinternet.com>
2008-05-17 19:31:26 +0000

    - store some state for IR lookups to simplify each lookup - for INPUT.* modes also check the mapping for the calling mode - make arrow_left.hold = dead when showing home
    git-svn-id: file:///home/awy/mirror/slim/7.1/trunk/server@19835 62299810-d8cb-41fd-93b7-d32162e5a4a4

--- Slim/Utils/Prefs.pm
+++ Slim/Utils/Prefs.pm
@@ -479,6 +479,12 @@ sub init {
 	$prefs->setChange( sub {
 		Slim::Control::Jive::buildCaches();
 	}, 'variousArtistAutoIdentification' );
+
+	# Reset IR state if preference change
+	$prefs->setChange( sub {
+		my $client = $_[2] || return;
+		Slim::Hardware::IR::initClient($client);
+	}, qw(disabledirsets irmap) );
 }
 
 =head2 writeAll( )
