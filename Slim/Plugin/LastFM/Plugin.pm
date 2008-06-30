07bad017976fafae2e18b1f65587b8c6089bfd1d
Michael Herger <mherger@logitech.com>
2008-06-30 10:04:48 +0000

    fix call to clientString()
    git-svn-id: file:///home/awy/mirror/slim/7.1/trunk/server@21319 62299810-d8cb-41fd-93b7-d32162e5a4a4

--- Slim/Plugin/LastFM/Plugin.pm
+++ Slim/Plugin/LastFM/Plugin.pm
@@ -232,7 +232,7 @@ sub trackInfoMenu {
 	if ( $artist ) {
 		return {
 			type => 'link',
-			name => Slim::Utils::Strings::clientString('PLUGIN_LFM_ON_LASTFM'),
+			name => Slim::Utils::Strings::clientString($client, 'PLUGIN_LFM_ON_LASTFM'),
 			url  => $snURL,
 		};
 	}
