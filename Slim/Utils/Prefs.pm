cd850eec8ba21986c58a65994d829b443c886200
Andy Grundman <agrundman@slimdevices.com>
2008-07-22 19:05:51 +0000

    Bug 8690, reset fixed digital volume pref now that it affects analog too
    Bug: 8690
    git-svn-id: file:///home/awy/mirror/slim/7.1/trunk/server@21997 62299810-d8cb-41fd-93b7-d32162e5a4a4

--- Slim/Utils/Prefs.pm
+++ Slim/Utils/Prefs.pm
@@ -357,6 +357,18 @@ sub init {
 			}
 			1;
 		} );
+		
+		# Bug 8690, reset fixed digital volume pref because it now affects analog outputs
+		$prefs->migrateClient( 5, sub {
+			my ( $cprefs, $client ) = @_;
+			my $dvc = $cprefs->get('digitalVolumeControl');
+			if ( defined $dvc && $dvc == 0 ) {
+				$cprefs->set( digitalVolumeControl => 1 );
+				if ( $cprefs->get('volume') > 50 ) {
+					$cprefs->set( volume => 50 );
+				}
+			}
+		} );
 	}
 
 	# initialise any new prefs
