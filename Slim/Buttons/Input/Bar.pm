1460d6856aac7b1efb9df0890f7680238c9090d9
Caleb Crome <ccrome@logitech.com>
2008-07-31 20:21:26 +0000

    Per dean's request, made volume acceleration symmetrical
    git-svn-id: file:///home/awy/mirror/slim/7.2/private-branches/boom/server@22277 62299810-d8cb-41fd-93b7-d32162e5a4a4

--- Slim/Buttons/Input/Bar.pm
+++ Slim/Buttons/Input/Bar.pm
@@ -342,9 +342,9 @@ sub changePos {
 		# be the same in both cases.
 		# 
 		if ($dir > 0) {
-			$knobAccelerationConstant = $client->modeParam('knobaccelup') || .1; # Accel going up.
+			$knobAccelerationConstant = $client->modeParam('knobaccelup') || .05; # Accel going up.
 		} else {
-			$knobAccelerationConstant = $client->modeParam('knobacceldown') ||.1; # Accel going down. 
+			$knobAccelerationConstant = $client->modeParam('knobacceldown') ||.05; # Accel going down. 
 		}
 		my $velocity      = $client->knobData->{'_velocity'};
 		my $acceleration  = $client->knobData->{'_acceleration'};
@@ -355,7 +355,7 @@ sub changePos {
 				$deltaX = 1;
 			}
 		} elsif ($deltaX < 0) {
-			if ($deltaX > 1) {
+			if ($deltaX > -1) {
 				$deltaX = -1;
 			}
 		} else {
