af95a5f36b935bb723ea485338714f32856c624d
Andy Grundman <agrundman@slimdevices.com>
2008-08-05 21:30:06 +0000

    Additional debug for RTC
    git-svn-id: file:///home/awy/mirror/slim/7.2/private-branches/boom/server@22403 62299810-d8cb-41fd-93b7-d32162e5a4a4

--- Slim/Utils/Alarm.pm
+++ Slim/Utils/Alarm.pm
@@ -1296,7 +1296,8 @@ sub setRTCAlarm {
 			}
 
 			# Alarm times are "floating" so no need to adjust for local time
-			$log->debug("Setting RTC alarm to $alarmTime");
+			$log->debug( "Setting RTC alarm to $alarmTime, volume " . $nextAlarm->volume );
+			
 			$client->setRTCAlarm($alarmTime, $nextAlarm->volume);
 
 			$clearRTCAlarm = 0;
