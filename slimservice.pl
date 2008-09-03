41a5c6617c4b527c5009e08d04e8d7094acc4871
Michael Herger <mherger@logitech.com>
2008-09-03 09:42:47 +0000

    bump a few version numbers
    git-svn-id: file:///home/awy/mirror/slim/7.3/trunk/server@23022 62299810-d8cb-41fd-93b7-d32162e5a4a4

--- slimservice.pl
+++ slimservice.pl
@@ -189,7 +189,7 @@ our @AUTHORS = (
 );
 my $prefs        = preferences('server');
 
-our $VERSION     = '7.2-sn';
+our $VERSION     = '7.3-sn';
 our $REVISION    = undef;
 our $audiodir    = undef
 our $playlistdir = undef;
