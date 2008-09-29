42afab4af1828137014bcb87318b6dee1e114f61
Andy Grundman <agrundman@slimdevices.com>
2008-09-29 19:39:03 +0000

    Bug 6859, bug 8220,  bug 8626, bug 9022, new Shoutcast directory
    Bug: 6859
    git-svn-id: file:///home/awy/mirror/slim/7.3/trunk/server@23330 62299810-d8cb-41fd-93b7-d32162e5a4a4

--- Slim/Plugin/ShoutcastBrowser/Plugin.pm
+++ Slim/Plugin/ShoutcastBrowser/Plugin.pm
@@ -1,193 +1,44 @@
 package Slim::Plugin::ShoutcastBrowser::Plugin;
 
-# $Id$
+# $Id: Plugin.pm 7196 2006-04-28 22:00:45Z andy $
+
+# SqueezeCenter Copyright 2001-2007 Logitech, Vidur Apparao.
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License,
+# version 2.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
 
 use strict;
-use base qw(Slim::Plugin::Base);
-
-use Slim::Buttons::Common;
-use Slim::Buttons::XMLBrowser;
-use Slim::Formats::XML;
-use Slim::Utils::Log;
-use Slim::Utils::Misc;
-
-if ( !main::SLIM_SERVICE ) {
- 	require Slim::Web::XMLBrowser;
-}
+use base qw(Slim::Plugin::OPMLBased);
 
 use Slim::Networking::SqueezeNetwork;
-
-sub FEED {
-	'http://'
-	. Slim::Networking::SqueezeNetwork->get_server("content")
-	. '/shoutcast/index.opml';
-}
-
-sub SEARCH {
-	'http://'
-	. Slim::Networking::SqueezeNetwork->get_server("sn")
-	. '/api/opensearch/shoutcast/opensearch.xml';
-}
-
-my $cli_next;
 
 sub initPlugin {
 	my $class = shift;
 
 	Slim::Player::ProtocolHandlers->registerIconHandler(
-		qr/(?:shoutcast\.com|squeezenetwork\.com.*\/shoutcast\/)/, 
+		qr/shoutcast/, 
 		sub { return $class->_pluginDataFor('icon'); }
 	);
 
-#        |requires Client
-#        |  |is a Query
-#        |  |  |has Tags
-#        |  |  |  |Function to call
-#        C  Q  T  F
-    Slim::Control::Request::addDispatch(['shoutcast', 'items', '_index', '_quantity'],
-        [1, 1, 1, \&cliQuery]);
-	Slim::Control::Request::addDispatch(['shoutcast', 'playlist', '_method' ],
-		[1, 1, 1, \&cliQuery]);
-	$cli_next = Slim::Control::Request::addDispatch(['radios', '_index', '_quantity' ],
-		[0, 1, 1, \&cliRadiosQuery]);
-
-	$class->initJive();
-
-	$class->SUPER::initPlugin();
+	$class->SUPER::initPlugin(
+		feed   => Slim::Networking::SqueezeNetwork->url('/api/shoutcast/v1/opml'),
+		tag    => 'shoutcast',
+		menu   => 'radios',
+		weight => 50,
+	);
 }
 
-# add "hidden" item to Jive home menu 
-# this allows ShoutCast to be optionally added to the 
-# top-level menu through the CustomizeHomeMenu applet
-sub initJive {
-	my ( $class ) = @_;
-
-	my $icon   = Slim::Plugin::ShoutcastBrowser::Plugin->_pluginDataFor('icon'),
-	my $name = $class->getDisplayName();
-        my @jiveMenu = ({
-		stringToken    => $name,
-		id             => 'pluginShoutcast',
-		node           => 'radios',
-		displayWhenOff => 0,
-		window         => { 
-				'icon-id' => $icon,
-				titleStyle => 'album',
-		},
-		actions => {
-			go =>          {
-				player => 0,
-				cmd    => [ 'shoutcast', 'items' ],
-				params => {
-					menu => 'shoutcast',
-				},
-			},
-		},
-	});
-
-	Slim::Control::Jive::registerPluginMenu(\@jiveMenu);
+sub playerMenu () {
+	return 'RADIO';
 }
 
 sub getDisplayName {
 	return 'PLUGIN_SHOUTCASTBROWSER_MODULE_NAME';
-}
-
-sub setMode {
-	my $class  = shift;
-	my $client = shift;
-	my $method = shift;
-
-	if ($method eq 'pop') {
-		Slim::Buttons::Common::popMode($client);
-		return;
-	}
-
-	# use INPUT.Choice to display the list of feeds
-	my %params = (
-		header   => 'PLUGIN_SHOUTCASTBROWSER_CONNECTING',
-		modeName => 'ShoutcastBrowser Plugin',
-		url      => FEED(),
-		search   => SEARCH(),
-		title    => $client->string( getDisplayName() ),
-	);
-
-	Slim::Buttons::Common::pushMode($client, 'xmlbrowser', \%params);
-	
-	# we'll handle the push in a callback
-	$client->modeParam('handledTransition', 1)
-}
-
-sub cliQuery {
-	my $request = shift;
-	
-	Slim::Buttons::XMLBrowser::cliQuery('shoutcast', FEED(), $request);
-}
-
-sub cliRadiosQuery {
-	my $request = shift;
-	
-	my $menu = $request->getParam('menu');
-
-	my $data;
-	# what we want the query to report about ourself
-	if (defined $menu) {
-		$data = {
-			'text' => $request->string( getDisplayName() ),  # nice name
-			'icon-id' => Slim::Plugin::ShoutcastBrowser::Plugin->_pluginDataFor('icon'),
-			'weight'  => 50,
-			'actions' => {
-				'go' => {
-					'cmd' => ['shoutcast', 'items'],
-					'params' => {
-						'menu' => 'shoutcast',
-					},
-				},
-			},
-			window    => {
-				titleStyle => 'album',
-			},
-		};
-		
-		if ( main::SLIM_SERVICE ) {
-			# Bug 7110, icons are full URLs so we must use icon not icon-id
-			$data->{icon} = delete $data->{'icon-id'};
-			
-			# Bug 7230, send pre-thumbnailed URL
-			$data->{icon} =~ s/\.png$/_56x56_p\.png/;
-		}
-	}
-	else {
-		$data = {
-			'cmd' => 'shoutcast',                    # cmd label
-			'name' => $request->string( getDisplayName() ),  # nice name
-			'type' => 'xmlbrowser',              # type
-		};
-	}
-	
-	# let our super duper function do all the hard work
-	Slim::Control::Queries::dynamicAutoQuery($request, 'radios', $cli_next, $data);
-}
-
-sub webPages {
-	my $class = shift;
-
-	my $title = getDisplayName();
-	my $url   = 'plugins/ShoutcastBrowser/index.html';
-
-	Slim::Web::Pages->addPageLinks('radios', { $title => $url });
-	
-	Slim::Web::HTTP::protectURI($url);
-
-	Slim::Web::HTTP::addPageFunction($url => sub {
-		my $client = $_[0];
-		
-		Slim::Web::XMLBrowser->handleWebIndex( {
-			client => $client,
-			feed   => FEED(),
-			title  => $title,
-			search => SEARCH(), 
-			args   => \@_
-		} );
-	});
 }
 
 1;
