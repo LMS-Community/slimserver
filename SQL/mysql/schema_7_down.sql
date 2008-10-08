d452ef1ece080e7025e5aa71ca52edc9835e7992
Andy Grundman <agrundman@slimdevices.com>
2008-10-08 04:23:33 +0000

    Revert bad patch for bug 9423
    git-svn-id: file:///home/awy/mirror/slim/7.3/trunk/server@23458 62299810-d8cb-41fd-93b7-d32162e5a4a4

--- SQL/mysql/schema_7_down.sql
+++ SQL/mysql/schema_7_down.sql
@@ -1,3 +1,1 @@
-ALTER TABLE tracks_persistent CONVERT TO CHARACTER SET utf8 COLLATE utf8_unicode_ci;
-
 ALTER TABLE tracks DROP extid;
--- SQL/mysql/schema_7_up.sql
+++ SQL/mysql/schema_7_up.sql
@@ -1,6 +1,3 @@
--- Bug 9423, incorrect collate was placed on tracks_persistent
-ALTER TABLE tracks_persistent CONVERT TO CHARACTER SET utf8 COLLATE utf8_general_ci;
-
 -- Add extid (external ID) column to tracks, to link with iTunes or other external library
 ALTER TABLE tracks ADD extid varchar(64);
 ALTER TABLE tracks ADD INDEX trackExtId (extid);
