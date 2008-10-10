164b09ce97ab473692e5b5d2cf24f699f6ff4695
Michael Herger <mherger@logitech.com>
2008-10-10 13:21:53 +0000

    use the ReadyNAS default music folder if available
    git-svn-id: file:///home/awy/mirror/slim/7.3/trunk/server@23511 62299810-d8cb-41fd-93b7-d32162e5a4a4

--- Slim/Utils/OS/ReadyNAS.pm
+++ Slim/Utils/OS/ReadyNAS.pm
@@ -1,6 +1,8 @@
 package Slim::Utils::OS::ReadyNAS;
 
 use strict;
+use File::Spec::Functions qw(:ALL);
+
 use base qw(Slim::Utils::OS::Debian);
 
 sub dontSetUserAndGroup { 1 }
@@ -13,6 +15,31 @@ sub initPrefs {
 	$prefs->{resampleArtwork}   = 0;
 	$prefs->{disableStatistics} = 1;
 }
+
+
+sub dirsFor {
+	my ($class, $dir) = @_;
+
+	my @dirs;
+
+	if ($dir =~ /^(?:music|playlists)$/) {
+
+		# let's do some optimistic tests
+		my $path = catdir('/', 'media', 'Music');
+		
+		unless ($path && -r $path) {
+			$path = $class->SUPER::dirsFor($dir);
+		}
+		
+		push @dirs, $path;
+	}
+	else {
+		@dirs = $class->SUPER::dirsFor($dir);
+	}
+
+	return wantarray() ? @dirs : $dirs[0];
+}
+
 
 sub ignoredItems {
 	return (
