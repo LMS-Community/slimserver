9e0dcda2bca0b5492d1ed0a1a89c7c9160a507f8
Michael Herger <mherger@logitech.com>
2008-10-15 12:06:53 +0000

    silence a warning
    git-svn-id: file:///home/awy/mirror/slim/7.3/trunk/server@23573 62299810-d8cb-41fd-93b7-d32162e5a4a4

--- Slim/Utils/Prefs.pm
+++ Slim/Utils/Prefs.pm
@@ -276,7 +276,7 @@ sub init {
 				my $old = Slim::Utils::Prefs::OldPrefs->get($pref);
 
 				# bug 7237: don't migrate dbsource if we're upgrading from SS6.3
-				next if $pref eq 'dbsource' && $old =~ /SQLite/i;
+				next if $pref eq 'dbsource' && $old && $old =~ /SQLite/i;
 
 				$prefs->set($pref, $old) if !$prefs->exists($pref) && defined $old;
 			}
