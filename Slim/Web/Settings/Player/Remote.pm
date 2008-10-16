abdb9c1f86290ede1f5edcc392988383aae5e4de
Kevin Deane-Freeman <slim-mail@deane-freeman.com>
2008-10-16 06:04:05 +0000

    don't show remote settings page for squeezeplay.
    git-svn-id: file:///home/awy/mirror/slim/7.3/trunk/server@23589 62299810-d8cb-41fd-93b7-d32162e5a4a4

--- Slim/Web/Settings/Player/Remote.pm
+++ Slim/Web/Settings/Player/Remote.pm
@@ -32,7 +32,7 @@ sub validFor {
 	my $class = shift;
 	my $client = shift;
 	
-	return $client->isPlayer && !$client->isa('Slim::Player::Receiver');
+	return $client->isPlayer && !$client->isa('Slim::Player::Receiver') && !$client->isa('Slim::Player::SqueezePlay');
 }
 
 sub prefs {
