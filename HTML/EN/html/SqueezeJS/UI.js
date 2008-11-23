820f3da30bd16daca39e4efa3aab335de4ed9dfb
Michael Herger <mherger@logitech.com>
2008-11-23 10:22:53 +0000

    only initialize SortableTable if Ext.grid is available; fix issue with scroll wheel in Webkit based browsers
    git-svn-id: file:///home/awy/mirror/slim/7.3/trunk/server@24024 62299810-d8cb-41fd-93b7-d32162e5a4a4

--- HTML/EN/html/SqueezeJS/UI.js
+++ HTML/EN/html/SqueezeJS/UI.js
@@ -2132,73 +2132,115 @@ SqueezeJS.UI.ScannerInfoExtended = function(){
 }();
 
 
-// create sortable table from HTML table, basically a copy of the TableGrid sample
-// http://extjs.com/deploy/dev/examples/grid/from-markup.html
-SqueezeJS.UI.SortableTable = function(table, config) {
-	config = config || {};
+// only load the following if Ext.grid is available
+if (Ext.grid) {
+	// XXXX fix an issue with grids in Safari/Chrome/Webkit Ext 2.2
+	Ext.override(Ext.grid.GridView, {
+		layout : function(){
+			if (!this.mainBody) {
+				return;
+			}
+			var g = this.grid;
+			var c = g.getGridEl();
+			var csize = c.getSize(true);
+			var vw = csize.width;
+			if(vw < 20 || csize.height < 20){
+				return;
+			}
+			if(g.autoHeight){
+				this.scroller.dom.style.overflow = 'visible';
+				this.scroller.dom.style.position = 'static';
+			}else{
+				this.el.setSize(csize.width, csize.height);
+				var hdHeight = this.mainHd.getHeight();
+				var vh = csize.height - (hdHeight);
+				this.scroller.setSize(vw, vh);
+				if(this.innerHd){
+					this.innerHd.style.width = (vw)+'px';
+				}
+			}
+			if(this.forceFit){
+				if(this.lastViewWidth != vw){
+					this.fitColumns(false, false);
+					this.lastViewWidth = vw;
+				}
+			}else {
+				this.autoExpand();
+				this.syncHeaderScroll();
+			}
+			this.onLayout(vw, vh);
+		}
+	});
+	// END XXXXX
 
-	// a few defaults
-	Ext.applyIf(config, {
-		stripeRows: true,
-		enableColumnHide: false,
-		enableHdMenu: false,
-		disableSelection: true,
-		border: false
-	});
-
-	Ext.apply(this, config);
-	var cf = config.fields || [], ch = config.columns || [];
-	table = Ext.get(table);
+	// create sortable table from HTML table, basically a copy of the TableGrid sample
+	// http://extjs.com/deploy/dev/examples/grid/from-markup.html
+	SqueezeJS.UI.SortableTable = function(table, config) {
+		config = config || {};
 	
-	var ct = table.insertSibling();
+		// a few defaults
+		Ext.applyIf(config, {
+			stripeRows: true,
+			enableColumnHide: false,
+			enableHdMenu: false,
+			disableSelection: true,
+			border: false
+		});
 	
-	var fields = [], cols = [];
-	var headers = table.query("thead th");
-	for (var i = 0, h; h = headers[i]; i++) {
-		var text = h.innerHTML;
-		var name = 'tcol-'+i;
-	
-		fields.push(Ext.applyIf(cf[i] || {}, {
-			name: name,
-			mapping: 'td:nth('+(i+1)+')/@innerHTML'
-		}));
-	
-		cols.push(Ext.applyIf(ch[i] || {}, {
-			'header': text,
-			'dataIndex': name,
-			'width': h.offsetWidth,
-			'tooltip': h.title,
-			'sortable': true
-		}));
-	}
-	
-	var ds  = new Ext.data.Store({
-		reader: new Ext.data.XmlReader({
-			record:'tbody tr'
-		}, fields)
-	});
-	
-	ds.loadData(table.dom);
-	
-	var cm = new Ext.grid.ColumnModel(cols);
-	
-	if (config.width || config.height) {
-		ct.setSize(config.width || 'auto', config.height || 'auto');
-	} else {
-		ct.setWidth(table.getWidth());
-	}
-	
-	if (config.remove !== false) {
-		table.remove();
-	}
-	
-	Ext.applyIf(this, {
-		'ds': ds,
-		'cm': cm,
-		autoHeight: true,
-		autoWidth: true
-	});
-	
-	SqueezeJS.UI.SortableTable.superclass.constructor.call(this, ct, {});
-};
-Ext.extend(SqueezeJS.UI.SortableTable, Ext.grid.GridPanel);
+		Ext.apply(this, config);
+		var cf = config.fields || [], ch = config.columns || [];
+		table = Ext.get(table);
+		
+		var ct = table.insertSibling();
+		
+		var fields = [], cols = [];
+		var headers = table.query("thead th");
+		for (var i = 0, h; h = headers[i]; i++) {
+			var text = h.innerHTML;
+			var name = 'tcol-'+i;
+		
+			fields.push(Ext.applyIf(cf[i] || {}, {
+				name: name,
+				mapping: 'td:nth('+(i+1)+')/@innerHTML'
+			}));
+		
+			cols.push(Ext.applyIf(ch[i] || {}, {
+				'header': text,
+				'dataIndex': name,
+				'width': h.offsetWidth,
+				'tooltip': h.title,
+				'sortable': true
+			}));
+		}
+		
+		var ds  = new Ext.data.Store({
+			reader: new Ext.data.XmlReader({
+				record:'tbody tr'
+			}, fields)
+		});
+		
+		ds.loadData(table.dom);
+		
+		var cm = new Ext.grid.ColumnModel(cols);
+		
+		if (config.width || config.height) {
+			ct.setSize(config.width || 'auto', config.height || 'auto');
+		} else {
+			ct.setWidth(table.getWidth());
+		}
+		
+		if (config.remove !== false) {
+			table.remove();
+		}
+		
+		Ext.applyIf(this, {
+			'ds': ds,
+			'cm': cm,
+			autoHeight: true,
+			autoWidth: true
+		});
+		
+		SqueezeJS.UI.SortableTable.superclass.constructor.call(this, ct, {});
+	};
+	Ext.extend(SqueezeJS.UI.SortableTable, Ext.grid.GridPanel);
+}
