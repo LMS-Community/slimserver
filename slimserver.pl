b6eb5b1e7b380156b1900bd5a11e434a9714d427
Andy Grundman <agrundman@slimdevices.com>
2008-12-22 21:06:43 +0000

    7.3.1 -> 7.3.2
    git-svn-id: file:///home/awy/mirror/slim/7.3/trunk/server@24397 62299810-d8cb-41fd-93b7-d32162e5a4a4

--- slimserver.pl
+++ slimserver.pl
@@ -211,7 +211,7 @@ our @AUTHORS = (
 
 my $prefs        = preferences('server');
 
-our $VERSION     = '7.3.1';
+our $VERSION     = '7.3.2';
 our $REVISION    = undef;
 our $BUILDDATE   = undef;
 our $audiodir    = undef;
