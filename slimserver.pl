88c792503a221533518f696766c90c668d38b074
Michael Herger <mherger@logitech.com>
2009-01-05 12:23:24 +0000

    fix update message encoding
    Bug: 10540
    git-svn-id: file:///home/awy/mirror/slim/7.3/trunk/server@24500 62299810-d8cb-41fd-93b7-d32162e5a4a4

--- slimserver.pl
+++ slimserver.pl
@@ -962,7 +962,7 @@ sub checkVersionCB {
 
 	# store result in global variable, to be displayed by browser
 	if ($http->{code} =~ /^2\d\d/) {
-		$::newVersion = $http->content();
+		$::newVersion = Slim::Utils::Unicode::utf8decode( $http->content() );
 		chomp($::newVersion);
 	}
 	else {
