80043f1c9e07c097a3a813b4a557b8ed3b967181
Adrian Smith <triode1@btinternet.com>
2009-01-19 19:47:20 +0000

    revert change 24706 as it breaks querying for SB2/3 type
    Bug: 10753
    git-svn-id: file:///home/awy/mirror/slim/7.4/trunk/server@24709 62299810-d8cb-41fd-93b7-d32162e5a4a4

--- Slim/Control/Queries.pm
+++ Slim/Control/Queries.pm
@@ -3107,7 +3107,7 @@ sub serverstatusQuery {
 				$request->addResultLoop('players_loop', $cnt, 
 					'name', $eachclient->name());
 				$request->addResultLoop('players_loop', $cnt, 
-					'model', $eachclient->model());
+					'model', $eachclient->model(1));
 				$request->addResultLoop('players_loop', $cnt, 
 					'power', $eachclient->power());
 				$request->addResultLoop('players_loop', $cnt, 
