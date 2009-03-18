d1b598da69d14c242bd6067edf076dadaee1fef9
Andy Grundman <agrundman@slimdevices.com>
2009-03-18 15:04:10 +0000

    Bug 10761, Formats::WMA doesn't map WM/ArtistSortOrder, patch from moonbase
    Bug: 10761
    git-svn-id: file:///home/awy/mirror/slim/7.3/trunk/server@25607 62299810-d8cb-41fd-93b7-d32162e5a4a4

--- Slim/Formats/WMA.pm
+++ Slim/Formats/WMA.pm
@@ -24,6 +24,7 @@ my %tagMapping = (
 	'GAIN_ALBUM_GAIN'    => 'REPLAYGAIN_ALBUM_GAIN',
 	'GAIN_ALBUM_PEAK'    => 'REPLAYGAIN_ALBUM_PEAK',
 	'PARTOFSET'          => 'DISC',
+	'ARTISTSORTORDER'    => 'ARTISTSORT',
 );
 
 {
