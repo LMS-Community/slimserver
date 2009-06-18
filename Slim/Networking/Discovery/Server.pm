484215efee24386a39f5861341b1d88cc9af24c3
Michael Herger <michael@slimdevices.com>
2009-06-18 07:52:20 +0000

    silence some undef warnings which could show up every minute
    git-svn-id: file:///home/awy/mirror/slim/7.4/trunk/server@27145 62299810-d8cb-41fd-93b7-d32162e5a4a4

--- Slim/Networking/Discovery/Server.pm
+++ Slim/Networking/Discovery/Server.pm
@@ -91,8 +91,8 @@ purge servers from the list when they haven't been discovered in two poll cycles
 sub _purge_server_list {
 	foreach my $server (keys %{$server_list}) {
 		
-		if ($server_list->{$server}->{timestamp} < time() - (2 * POLL_INTERVAL)) {
-			
+		if (!$server_list->{$server}->{timestamp} || $server_list->{$server}->{timestamp} < time() - (2 * POLL_INTERVAL)) {
+
 			delete $server_list->{$server};
 		}
 	}
