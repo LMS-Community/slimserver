3de6048686cecbcc8d467d784798a918099df7c7
Andy Grundman <agrundman@slimdevices.com>
2009-09-22 13:46:55 +0000

    7.4.1 -> 7.5.0
    git-svn-id: file:///home/awy/mirror/slim/7.5/trunk/server@28597 62299810-d8cb-41fd-93b7-d32162e5a4a4

--- scanner.pl
+++ scanner.pl
@@ -98,7 +98,7 @@ if ( INFOLOG || DEBUGLOG ) {
 	require Slim::Utils::PerlRunTime;
 }
 
-our $VERSION     = '7.4.1';
+our $VERSION     = '7.5.0';
 our $REVISION    = undef;
 our $BUILDDATE   = undef;
 
