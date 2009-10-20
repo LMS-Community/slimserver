4c25522bf28530ae3ec48b6cec7ab26080148c07
Michael Herger <michael@slimdevices.com>
2009-10-20 10:27:43 +0000

    Bug: 14602 +1 Description: add proxy support when checking mysb.com connectivity
    Bug: 14602
    git-svn-id: file:///home/awy/mirror/slim/7.5/trunk/server@28945 62299810-d8cb-41fd-93b7-d32162e5a4a4

--- Slim/GUI/ControlPanel/Advanced.pm
+++ Slim/GUI/ControlPanel/Advanced.pm
@@ -11,9 +11,6 @@ use base 'Wx::Panel';
 use Wx qw(:everything);
 use Wx::Event qw(EVT_BUTTON EVT_TIMER);
 use File::Spec::Functions qw(catfile);
-use LWP::Simple qw($ua get);
-
-$ua->timeout(10);
 
 use Slim::Utils::Light;
 use Slim::Utils::ServiceManager;
