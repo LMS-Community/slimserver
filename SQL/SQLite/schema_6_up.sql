869c22448147bcbb3b5237ceb325ae197d3def69
Andy Grundman <agrundman@slimdevices.com>
2009-10-20 19:00:55 +0000

    Fix script to put SQLite tracks_persistent data in another database file
    git-svn-id: file:///home/awy/mirror/slim/7.5/branches/embedded/server@28960 62299810-d8cb-41fd-93b7-d32162e5a4a4

--- SQL/SQLite/schema_6_up.sql
+++ SQL/SQLite/schema_6_up.sql
@@ -1,7 +1,7 @@
 -- Table to store persistent track information, e.g. ratings and playcounts
 -- This data survives a rescan
-DROP TABLE IF EXISTS tracks_persistent;
-CREATE TABLE tracks_persistent (
+DROP TABLE IF EXISTS persistentdb.tracks_persistent;
+CREATE TABLE persistentdb.tracks_persistent (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
   url text NOT NULL,
   musicbrainz_id varchar(40),	-- musicbrainz uuid (36 bytes of text)
@@ -11,9 +11,9 @@ CREATE TABLE tracks_persistent (
   lastPlayed int(10)
 );
 
-CREATE INDEX trackMusicBrainzIndex ON tracks_persistent (musicbrainz_id);
-CREATE INDEX trackUrlIndex ON tracks_persistent (url);
-CREATE INDEX trackAddedIndex ON tracks_persistent (added);
-CREATE INDEX trackRatingIndex ON tracks_persistent (rating);
-CREATE INDEX trackPlayCountIndex ON tracks_persistent (playCount);
-CREATE INDEX trackLastPlayedIndex ON tracks_persistent (lastPlayed);
+CREATE INDEX persistentdb.trackMusicBrainzIndex ON tracks_persistent (musicbrainz_id);
+CREATE INDEX persistentdb.trackUrlIndex ON tracks_persistent (url);
+CREATE INDEX persistentdb.trackAddedIndex ON tracks_persistent (added);
+CREATE INDEX persistentdb.trackRatingIndex ON tracks_persistent (rating);
+CREATE INDEX persistentdb.trackPlayCountIndex ON tracks_persistent (playCount);
+CREATE INDEX persistentdb.trackLastPlayedIndex ON tracks_persistent (lastPlayed);
