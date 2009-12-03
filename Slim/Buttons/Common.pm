968d383f5fc2a9c68e5c690ed47ec8ccc4b5f513
Michael Herger <michael@slimdevices.com>
2009-12-03 14:44:20 +0000

    revert accidental change
    git-svn-id: file:///home/awy/mirror/slim/7.5/trunk/server@29532 62299810-d8cb-41fd-93b7-d32162e5a4a4

--- Slim/Buttons/Common.pm
+++ Slim/Buttons/Common.pm
@@ -40,7 +40,6 @@ use warnings;
 use Scalar::Util qw(blessed);
 
 use Slim::Buttons::Alarm;
-use Slim::Buttons::GlobalSearch;
 use Slim::Buttons::SqueezeNetwork;
 use Slim::Buttons::Volume;
 use Slim::Buttons::XMLBrowser;
@@ -126,7 +125,6 @@ sub init {
 	Slim::Buttons::Block::init();
 	Slim::Buttons::BrowseDB::init();
 	Slim::Buttons::BrowseTree::init();
-	Slim::Buttons::GlobalSearch::init();
 	Slim::Buttons::Information::init();
 	Slim::Buttons::Playlist::init();
 	Slim::Buttons::XMLBrowser::init();
