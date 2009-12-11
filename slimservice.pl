d0dec4b01aaca9a18de070d983beb47807632412
Michael Herger <michael@slimdevices.com>
2009-12-11 14:54:03 +0000

    enable global search in slimservice mode
    Bug: 15286
    git-svn-id: file:///home/awy/mirror/slim/7.5/trunk/server@29587 62299810-d8cb-41fd-93b7-d32162e5a4a4

--- slimservice.pl
+++ slimservice.pl
@@ -162,6 +162,7 @@ use Slim::Menu::GenreInfo;
 use Slim::Menu::YearInfo;
 use Slim::Menu::SystemInfo;
 use Slim::Menu::PlaylistInfo;
+use Slim::Menu::GlobalSearch;
 use Slim::Music::Info;
 #use Slim::Music::Import;
 #use Slim::Music::MusicFolderScan;
@@ -391,6 +392,7 @@ sub init {
 	Slim::Menu::YearInfo->init();
 	Slim::Menu::SystemInfo->init();
 	Slim::Menu::PlaylistInfo->init();
+	Slim::Menu::GlobalSearch->init();
 	
 	main::INFOLOG && $log->info('Squeezebox Server Alarms init...');
 	Slim::Utils::Alarm->init();
