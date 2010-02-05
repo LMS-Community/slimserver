c6d909d6f57c834f661e0bb2c756ffb602aecdf5
Andy Grundman <agrundman@slimdevices.com>
2010-02-05 21:45:34 +0000

    Fix typo
    git-svn-id: file:///home/awy/mirror/slim/7.5/trunk/server@30046 62299810-d8cb-41fd-93b7-d32162e5a4a4

--- scanner.pl
+++ scanner.pl
@@ -26,7 +26,7 @@ use constant PERFMON      => 0;
 use constant DEBUGLOG     => ( grep { /--nodebuglog/ } @ARGV ) ? 0 : 1;
 use constant INFOLOG      => ( grep { /--noinfolog/ } @ARGV ) ? 0 : 1;
 use constant SB1SLIMP3SYNC=> 0;
-use constant WEBUI        => 0,
+use constant WEBUI        => 0;
 use constant ISWINDOWS    => ( $^O =~ /^m?s?win/i ) ? 1 : 0;
 use constant ISMAC        => ( $^O =~ /darwin/i ) ? 1 : 0;
 
