efbd373e64eed9c65ce5c81982a9b7f628e4729a
Andy Grundman <agrundman@logitech.com>
2010-04-06 15:22:29 +0000

    r62253@macbookpro-eth (orig r30430):  andy | 2010-03-30 07:56:32 -0400  Branch for 7.5.0 RC  r62254@macbookpro-eth (orig r30431):  andy | 2010-03-30 08:05:14 -0400  Fixed bug 15877, reset MySB hostname to production  r62256@macbookpro-eth (orig r30433):  andy | 2010-03-30 15:20:42 -0400  New Rhapsody logo
    git-svn-id: file:///home/awy/mirror/slim/7.5/trunk/server@30474 62299810-d8cb-41fd-93b7-d32162e5a4a4

--- Slim/Networking/SqueezeNetwork.pm
+++ Slim/Networking/SqueezeNetwork.pm
@@ -86,11 +86,6 @@ sub get_server {
 		$stype = 'test';
 	}
 	
-	# TODO - to be removed before release!
-	if ( $stype eq 'sn' ) {
-		$stype = 'test';
-	}
-
 	return $_Servers->{$stype}
 		|| die "No hostname known for server type '$stype'";
 }
