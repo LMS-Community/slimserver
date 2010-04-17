bfb68367b09f8a798102f10561be4d59bb3e7ab6
Adrian Smith <triode1@btinternet.com>
2010-04-17 13:58:59 +0000

    we can only use plugin downloader from the web interface so avoid loading it otherwise - avoids loading this module on TinySC
    Bug: 16086
    git-svn-id: file:///home/awy/mirror/slim/7.5/trunk/server@30660 62299810-d8cb-41fd-93b7-d32162e5a4a4

--- Slim/Utils/PluginManager.pm
+++ Slim/Utils/PluginManager.pm
@@ -61,7 +61,7 @@ sub init {
 		1;
 	});
 
-	if ( !$main::SLIM_SERVICE ) {
+	if ( $main::WEBUI ) {
 		eval {
 			require Slim::Utils::PluginDownloader;
 			$downloader = 'Slim::Utils::PluginDownloader';
