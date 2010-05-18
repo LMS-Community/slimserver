4ef1c940e5f1dada099dfb4171c34fd5cec91908
Andy Grundman <agrundman@logitech.com>
2010-05-18 13:56:41 +0000

    Backport Class::XSAccessor 1.05 update from 7.6
    git-svn-id: file:///home/awy/mirror/slim/7.5/trunk/server@30783 62299810-d8cb-41fd-93b7-d32162e5a4a4

--- CPAN/Class/XSAccessor.pm
+++ CPAN/Class/XSAccessor.pm
@@ -1,103 +1,90 @@
 package Class::XSAccessor;
 
 use 5.006;
+
 use strict;
 use warnings;
+
 use Carp qw/croak/;
 
-our $VERSION = '1.03';
+our $VERSION = '1.05';
 
 require XSLoader;
 XSLoader::load('Class::XSAccessor', $VERSION);
+
+require Class::XSAccessor::Heavy;
+
+sub _make_hash {
+  my $ref = shift;
+
+  if (ref ($ref)) {
+    if (ref($ref) eq 'ARRAY') {
+      $ref = { map { $_ => $_ } @$ref }
+    } 
+  } else {
+    $ref = { $ref, $ref };
+  }
+
+  return $ref;
+}
 
 sub import {
   my $own_class = shift;
   my ($caller_pkg) = caller();
 
-  my %opts = @_;
+  # Support both { getters => ... } and plain getters => ...
+  my %opts = ref($_[0]) eq 'HASH' ? %{$_[0]} : @_;
 
   $caller_pkg = $opts{class} if defined $opts{class};
 
-  my $replace = $opts{replace} || 0;
-  my $chained = $opts{chained} || 0;
-
-  # TODO: Refactor. This code sucks really bad.
-  
-  my $read_subs      = $opts{getters} || {};
-  my $set_subs       = $opts{setters} || {};
-  my $acc_subs       = $opts{accessors} || {};
-  my $pred_subs      = $opts{predicates} || {};
+  # TODO: Refactor. Move more duplicated code to ::Heavy
+  my $read_subs      = _make_hash($opts{getters} || {});
+  my $set_subs       = _make_hash($opts{setters} || {});
+  my $acc_subs       = _make_hash($opts{accessors} || {});
+  my $pred_subs      = _make_hash($opts{predicates} || {});
+  my $test_subs      = _make_hash($opts{__tests__} || {});
   my $construct_subs = $opts{constructors} || [defined($opts{constructor}) ? $opts{constructor} : ()];
   my $true_subs      = $opts{true} || [];
   my $false_subs     = $opts{false} || [];
 
-  foreach my $subname (keys %$read_subs) {
-    my $hashkey = $read_subs->{$subname};
-    _generate_method($caller_pkg, $subname, $hashkey, $replace, $chained, "getter");
+  foreach my $subtype ( ["getter", $read_subs],
+                        ["setter", $set_subs],
+                        ["accessor", $acc_subs],
+                        ["test", $test_subs],
+                        ["pred_subs", $pred_subs] )
+  {
+    my $subs = $subtype->[1];
+    foreach my $subname (keys %$subs) {
+      my $hashkey = $subs->{$subname};
+      _generate_method($caller_pkg, $subname, $hashkey, \%opts, $subtype->[0]);
+    }
   }
 
-  foreach my $subname (keys %$set_subs) {
-    my $hashkey = $set_subs->{$subname};
-    _generate_method($caller_pkg, $subname, $hashkey, $replace, $chained, "setter");
-  }
-
-  foreach my $subname (keys %$acc_subs) {
-    my $hashkey = $acc_subs->{$subname};
-    _generate_method($caller_pkg, $subname, $hashkey, $replace, $chained, "accessor");
-  }
-
-  foreach my $subname (keys %$pred_subs) {
-    my $hashkey = $pred_subs->{$subname};
-    _generate_method($caller_pkg, $subname, $hashkey, $replace, $chained, "predicate");
-  }
-  
-  foreach my $subname (@$construct_subs) {
-    _generate_method($caller_pkg, $subname, "", $replace, $chained, "constructor");
-  }
-
-  foreach my $subname (@$true_subs) {
-    _generate_method($caller_pkg, $subname, "", $replace, $chained, "true");
-  }
-
-  foreach my $subname (@$false_subs) {
-    _generate_method($caller_pkg, $subname, "", $replace, $chained, "false");
+  foreach my $subtype ( ["constructor", $construct_subs],
+                        ["true", $true_subs],
+                        ["false", $false_subs] )
+  {
+    foreach my $subname (@{$subtype->[1]}) {
+      _generate_method($caller_pkg, $subname, "", \%opts, $subtype->[0]);
+    }
   }
 }
 
 sub _generate_method {
-  my ($caller_pkg, $subname, $hashkey, $replace, $chained, $type) = @_;
+  my ($caller_pkg, $subname, $hashkey, $opts, $type) = @_;
 
-  if (not defined $hashkey) {
-    croak("Cannot use undef as a hash key for generating an XS $type accessor. (Sub: $subname)");
-  }
+  croak("Cannot use undef as a hash key for generating an XS $type accessor. (Sub: $subname)")
+    if not defined $hashkey;
 
-  if ($subname !~ /::/) {
-    $subname = "${caller_pkg}::$subname";
-  }
+  $subname = "${caller_pkg}::$subname" if $subname !~ /::/;
 
-  if (not $replace) {
-    my $sub_package = $subname;
-    $sub_package =~ s/([^:]+)$// or die;
-    my $bare_subname = $1;
-    
-    my $sym;
-    {
-      no strict 'refs';
-      $sym = \%{"$sub_package"};
-    }
-    no warnings;
-    local *s = $sym->{$bare_subname};
-    my $coderef = *s{CODE};
-    if ($coderef) {
-      croak("Cannot replace existing subroutine '$bare_subname' in package '$sub_package' with XS $type accessor. If you wish to force a replacement, add the 'replace => 1' parameter to the arguments of 'use ".__PACKAGE__."'.");
-    }
-  }
+  Class::XSAccessor::Heavy::check_sub_existence($subname) if not $opts->{replace};
 
   if ($type eq 'getter') {
     newxs_getter($subname, $hashkey);
   }
   elsif ($type eq 'setter') {
-    newxs_setter($subname, $hashkey, $chained);
+    newxs_setter($subname, $hashkey, $opts->{chained}||0);
   }
   elsif ($type eq 'predicate') {
     newxs_predicate($subname, $hashkey);
@@ -111,13 +98,16 @@ sub _generate_method {
   elsif ($type eq 'false') {
     newxs_boolean($subname, 0);
   }
+  elsif ($type eq 'test') {
+    newxs_test($subname, $hashkey);
+  }
   else {
-    newxs_accessor($subname, $hashkey, $chained);
+    newxs_accessor($subname, $hashkey, $opts->{chained}||0);
   }
 }
 
-
 1;
+
 __END__
 
 =head1 NAME
@@ -125,11 +115,12 @@ __END__
 Class::XSAccessor - Generate fast XS accessors without runtime compilation
 
 =head1 SYNOPSIS
-  
+
   package MyClass;
   use Class::XSAccessor
+    replace     => 1,   # Replace existing methods (if any)
     constructor => 'new',
-    getters => {
+    getters     => {
       get_foo => 'foo', # 'foo' is the hash key to access
       get_bar => 'bar',
     },
@@ -145,12 +136,24 @@ Class::XSAccessor - Generate fast XS accessors without runtime compilation
       has_foo => 'foo',
       has_bar => 'bar',
     }
-    true => [ 'is_token', 'is_whitespace' ],
+    true  => [ 'is_token', 'is_whitespace' ],
     false => [ 'significant' ];
-
+  
   # The imported methods are implemented in fast XS.
   
   # normal class code here.
+
+As of version 1.05, some alternative syntax forms are available:
+
+  package MyClass;
+  
+  # Options can be passed as a HASH reference, if preferred,
+  # which can also help Perl::Tidy to format the statement correctly.
+  use Class::XSAccessor {
+     # If the name => key values are always identical,
+     # the following shorthand can be used.
+     accessors => [ 'foo', 'bar' ],
+  };
 
 =head1 DESCRIPTION
 
@@ -162,10 +165,10 @@ L<Class::XSAccessor::Array> implements the same interface for objects
 that use arrays for their internal representation.
 
 Since version 0.10, the module can also generate simple constructors
-(implemented in XS) for you. Simply supply the
+(implemented in XS). Simply supply the
 C<constructor =E<gt> 'constructor_name'> option or the
 C<constructors =E<gt> ['new', 'create', 'spawn']> option.
-These constructors do the equivalent of the following perl code:
+These constructors do the equivalent of the following Perl code:
 
   sub new {
     my $class = shift;
@@ -176,32 +179,32 @@ That means they can be called on objects and classes but will not
 clone objects entirely. Parameters to C<new()> are added to the
 object.
 
-The XS accessor methods were between 1.6 and 2.5 times faster than typical
-pure-perl accessors in some simple benchmarking.
+The XS accessor methods are between 2 and 3 times faster than typical
+pure-Perl accessors in some simple benchmarking.
 The lower factor applies to the potentially slightly obscure
 C<sub set_foo_pp {$_[0]-E<gt>{foo} = $_[1]}>, so if you usually
-write clear code, a factor of two speed-up is a good estimate.
+write clear code, a factor of 2.5 speed-up is a good estimate.
 
-The method names may be fully qualified. In the example of the
-synopsis, you could have written C<MyClass::get_foo> instead
-of C<get_foo>. This way, you can install methods in classes other
-than the current class. See also: The C<class> option below.
+The method names may be fully qualified. The example in the synopsis could
+have been written as C<MyClass::get_foo> instead
+of C<get_foo>. This way, methods can be installed in classes other
+than the current class. See also: the C<class> option below.
 
-By default, the setters return the new value that was set
-and the accessors (mutators) do the same. You can change this behaviour
-with the C<chained> option, see below. The predicates obviously return a boolean.
+By default, the setters return the new value that was set,
+and the accessors (mutators) do the same. This behaviour can be changed
+with the C<chained> option - see below. The predicates return a boolean.
 
-Since version 1.01, you can generate extremely simply methods which
-simply return true or false (and always do so). If that seems like a
-really superfluous thing to you, then think of a large class hierarchy
-with interfaces such as PPI. This is implemented as the C<true>
-and C<false> options, see synopsis.
+Since version 1.01, C<Class::XSAccessor> can generate extremely simple methods which
+just return true or false (and always do so). If that seems like a
+really superfluous thing to you, then consider a large class hierarchy
+with interfaces such as L<PPI>. These methods are provided by the C<true>
+and C<false> options - see the synopsis.
 
 =head1 OPTIONS
 
-In addition to specifying the types and names of accessors, you can add options
-which modify behaviour. The options are specified as key/value pairs just as the
-accessor declaration. Example:
+In addition to specifying the types and names of accessors, additional options
+can be supplied which modify behaviour. The options are specified as key/value pairs
+in the same manner as the accessor declaration. For example:
 
   use Class::XSAccessor
     getters => {
@@ -229,42 +232,44 @@ in the same C<use Class::XSAccessor ...> statement.
 
 =head2 class
 
-By default, the accessors are generated in the calling class. Using
-the C<class> option, you can explicitly specify where the methods
-are to be generated.
+By default, the accessors are generated in the calling class. The
+the C<class> option allows the target class to be specified.
 
 =head1 CAVEATS
 
-Probably wouldn't work if your objects are I<tied> hashes. But that's a strange thing to do anyway.
+Probably won't work for objects based on I<tied> hashes. But that's a strange thing to do anyway.
 
 Scary code exploiting strange XS features.
 
 If you think writing an accessor in XS should be a laughably simple exercise, then
 please contemplate how you could instantiate a new XS accessor for a new hash key
-that's only known at run-time. Note that compiling C code at run-time a la Inline::C
+that's only known at run-time. Note that compiling C code at run-time a la L<Inline::C|Inline::C>
 is a no go.
 
-Threading. With version 1.00, a memory leak has been B<fixed> that would leak a small amount of
-memory if you loaded C<Class::XSAccessor>-based classes in a subthread that hadn't been loaded
-in the "main" thread before. If the subthread then terminated, a hash key and an int per
-associated method used ot be lost. Note that this mattered only if classes were B<only> loaded
+Threading. With version 1.00, a memory leak has been B<fixed>. Previously, a small amount of
+memory would leak if C<Class::XSAccessor>-based classes were loaded in a subthread without having
+been loaded in the "main" thread. If the subthread then terminated, a hash key and an int per
+associated method used to be lost. Note that this mattered only if classes were B<only> loaded
 in a sort of throw-away thread.
 
-In the new implementation as of 1.00, the memory will not be released again either in the above
-situation. But it will be recycled when the same class or a similar class is loaded
-again in B<any> thread.
+In the new implementation, as of 1.00, the memory will still not be released, in the same situation,
+but it will be recycled when the same class, or a similar class, is loaded again in B<any> thread.
 
 =head1 SEE ALSO
 
-L<Class::XSAccessor::Array>
+=over
 
-L<AutoXS>
+=item * L<Class::XSAccessor::Array>
+
+=item * L<AutoXS>
+
+=back
 
 =head1 AUTHOR
 
-Steffen Mueller, E<lt>smueller@cpan.orgE<gt>
+Steffen Mueller E<lt>smueller@cpan.orgE<gt>
 
-Chocolateboy, E<lt>chocolate@cpan.orgE<gt>
+Chocolateboy E<lt>chocolate@cpan.orgE<gt>
 
 =head1 COPYRIGHT AND LICENSE
 
@@ -275,4 +280,3 @@ it under the same terms as Perl itself, either Perl version 5.8 or,
 at your option, any later version of Perl 5 you may have available.
 
 =cut
-
