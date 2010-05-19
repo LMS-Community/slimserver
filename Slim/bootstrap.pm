ce58bcacea6b4537ba9f4611c65a5a5ebd067487
Michael Herger <mherger@logitech.com>
2010-05-19 09:00:45 +0000

    Bug: 14593 (kind of) Description: only Linux/Unix users should compile modules themselves. We provide everything for OSX/Windows. Compiling manually on one of these platforms might add more trouble than relieve.
    Bug: 14593
    git-svn-id: file:///home/awy/mirror/slim/7.5/trunk/server@30787 62299810-d8cb-41fd-93b7-d32162e5a4a4

--- Slim/bootstrap.pm
+++ Slim/bootstrap.pm
@@ -198,8 +198,13 @@ sub loadModules {
 
 NOTE:
 
-Please use the buildme.sh script located here:
+If you're running some unsupported Linux/Unix platform, please use the buildme.sh 
+script located here:
+
 http://svn.slimdevices.com/repos/slim/7.5/trunk/vendor/CPAN/
+
+You should never need to do this if you're on Windows or Mac OSX. If the installers
+don't work for you, ask for help and/or report a bug.
 
 If 7.5 is outdated by the time you read this, Replace "7.5" with the major version
 of Squeezebox Server you are running.
