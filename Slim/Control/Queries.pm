b57cd3f6a77a158c79937697798b1931be1ad48f
Andy Grundman <agrundman@logitech.com>
2010-07-12 20:17:56 +0000

    statusQuery_filter is called too much to have this stupid log call
    git-svn-id: file:///home/awy/mirror/slim/7.5/trunk/server@31042 62299810-d8cb-41fd-93b7-d32162e5a4a4

--- Slim/Control/Queries.pm
+++ Slim/Control/Queries.pm
@@ -3429,9 +3429,6 @@ sub sleepQuery {
 # the filter function decides, based on a notified request, if the status
 # query must be re-executed.
 sub statusQuery_filter {
-
-	$log->debug('statusQuery_filter()');
-
 	my $self = shift;
 	my $request = shift;
 	
