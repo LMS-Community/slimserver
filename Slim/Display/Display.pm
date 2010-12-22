b8651734d3c72834d9c61cdb59869a97328c3829
Andy Grundman <agrundman@logitech.com>
2010-12-22 17:04:29 +0000

    Lower grfs chunk size so we can compress it and avoid packet fragmentation
    git-svn-id: file:///home/awy/mirror/slim/7.5/trunk/server@31664 62299810-d8cb-41fd-93b7-d32162e5a4a4

--- Slim/Display/Display.pm
+++ Slim/Display/Display.pm
@@ -631,8 +631,8 @@ sub scrollInit {
 		my $offset = 0;
 		
 		while ($length > 0) {
-			if ( $length > 2900 ) {
-				$length = 2900;
+			if ( $length > 1400 ) {
+				$length = 1400;
 			}
 			
 			my $data = $header . pack('n', $offset) . substr( $scrollbits, 0, $length, '' );
