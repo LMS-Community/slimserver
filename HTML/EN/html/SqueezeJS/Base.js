96f9bfc4055164e6aae6b6cefb73fab39434867c
Alan Young <ayoung1@logitech.com>
2011-03-05 17:21:01 +0000

    task : XMLBrowser Library Browser Ensure all browselibrary links have a 'linktitle'.
    git-svn-id: file:///home/awy/mirror/slim/7.6/branches/onebrowser/server@32000 62299810-d8cb-41fd-93b7-d32162e5a4a4

--- HTML/EN/html/SqueezeJS/Base.js
+++ HTML/EN/html/SqueezeJS/Base.js
@@ -607,9 +607,9 @@ SqueezeJS.SonginfoParser = {
 		},
 		linked : {
 			title : new Ext.Template('<a href="' + webroot +'{link}?player={player}&amp;item={id}" target="browser">{title}</a>'),
-			album : new Ext.Template('<a href="' + webroot + 'clixmlbrowser/clicmd=browselibrary+items&amp;mode=tracks&amp;album_id={id}&amp;player={player}/" target="browser">{album}</a>'),
-			contributor : new Ext.Template('<a href="' + webroot + 'clixmlbrowser/clicmd=browselibrary+items&amp;mode=albums&amp;artist_id={id}&amp;player={player}/" target="browser">{contributor}</a>'),
-			year : new Ext.Template('<a href="' + webroot + 'clixmlbrowser/clicmd=browselibrary+items&amp;mode=albums&amp;year={year}&amp;player={player}/" target="browser">{year}</a>'),
+			album : new Ext.Template('<a href="' + webroot + 'clixmlbrowser/clicmd=browselibrary+items&amp;mode=tracks&amp;linktitle={title}&amp;album_id={id}&amp;player={player}/" target="browser">{album}</a>'),
+			contributor : new Ext.Template('<a href="' + webroot + 'clixmlbrowser/clicmd=browselibrary+items&amp;mode=albums&amp;linktitle={title}&amp;artist_id={id}&amp;player={player}/" target="browser">{contributor}</a>'),
+			year : new Ext.Template('<a href="' + webroot + 'clixmlbrowser/clicmd=browselibrary+items&amp;mode=albums&amp;linktitle={title}&amp;year={year}&amp;player={player}/" target="browser">{year}</a>'),
 			coverart : new Ext.Template('<a href="' + webroot + '{link}?player={player}&amp;item={id}" target="browser"><img src="{src}" {width} {height}></a>')
 		}
 	},
@@ -662,6 +662,7 @@ SqueezeJS.SonginfoParser = {
 		return this.tpl[((noLink || id == null) ? 'raw' : 'linked')].album.apply({
 			id: id,
 			album: album,
+			title: encodeURIComponent('xyX (' + album + ')'),
 			player: SqueezeJS.getPlayer()
 		});
 	},
@@ -688,6 +689,7 @@ SqueezeJS.SonginfoParser = {
 							contributorList += this.tpl[((ids[i] && !noLink) ? 'linked' : 'raw')].contributor.apply({ 
 								id: (ids[i] || null),
 								contributor: contributors[i],
+								title: encodeURIComponent('xyX (' + contributors[i] + ')'),
 								player: SqueezeJS.getPlayer()
 							});
 						}
@@ -707,6 +709,7 @@ SqueezeJS.SonginfoParser = {
 
 		return this.tpl[(noLink || !year ? 'raw' : 'linked')].year.apply({
 			year: year,
+			title: encodeURIComponent('xyX (' + year + ')'),
 			player: SqueezeJS.getPlayer()
 		});
 	},
