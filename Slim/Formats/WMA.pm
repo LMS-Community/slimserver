d44be9553f544b94afd83926a20d298416f70535
Andy Grundman <agrundman@logitech.com>
2011-03-06 16:16:42 +0000

    Fixed bug 16991, map WMA 'Compilation' tag in adition to 'compilation'. Yes it might make sense to make all tag mapping case-insensitive but I don't want to commit to that unless absolutely necessary
    Bug: 16991
    git-svn-id: file:///home/awy/mirror/slim/7.6/trunk/server@32009 62299810-d8cb-41fd-93b7-d32162e5a4a4

--- Slim/Formats/WMA.pm
+++ Slim/Formats/WMA.pm
@@ -27,6 +27,7 @@ my %tagMapping = (
 	'WM/TrackNumber'        => 'TRACKNUM',
 	'WM/PartOfACompilation' => 'COMPILATION',
 	'compilation'           => 'COMPILATION', # Bug 16991
+	'Compilation'           => 'COMPILATION', # Bug 16991
 	'Description'           => 'COMMENT',
 	'replaygain_track_gain' => 'REPLAYGAIN_TRACK_GAIN',
 	'replaygain_track_peak' => 'REPLAYGAIN_TRACK_PEAK',
