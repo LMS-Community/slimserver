9186d8d48916c967c899198354660439df060260
Michael Herger <mherger@logitech.com>
2011-07-17 10:20:13 +0000

    don't allow NULL values in timestamps
    git-svn-id: file:///home/awy/mirror/slim/7.6/branches/lms/server@32724 62299810-d8cb-41fd-93b7-d32162e5a4a4

--- SQL/SQLite/schema_15_up.sql
+++ SQL/SQLite/schema_15_up.sql
@@ -1,9 +1,9 @@
 
 -- to be set to EXIF DateTimeOriginal or similar
-ALTER TABLE images ADD original_time int(10) default NULL;
+ALTER TABLE images ADD original_time int(10) NOT NULL DEFAULT 0;
 CREATE INDEX imageDateTimeOriginal ON images (original_time);
 
-ALTER TABLE images ADD orientation int(10) default NULL;
+ALTER TABLE images ADD orientation int(10) NOT NULL DEFAULT 0;
 
 ALTER TABLE images ADD album blob default NULL;
 
