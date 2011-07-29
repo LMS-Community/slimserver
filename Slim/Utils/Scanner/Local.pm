c0c11be9431c4aa6ef03b908049302247bf4ae3d
Michael Herger <mherger@logitech.com>
2011-07-29 11:23:41 +0000

    slightly change logic. Only remove missing tracks when looking for audio tracks.
    Bug: 16037
    git-svn-id: file:///home/awy/mirror/slim/7.6/trunk/server@32928 62299810-d8cb-41fd-93b7-d32162e5a4a4

--- Slim/Utils/Scanner/Local.pm
+++ Slim/Utils/Scanner/Local.pm
@@ -220,11 +220,11 @@ sub rescan {
 			WHERE scanned_files.url LIKE '$basedir%'
 		};
 		
-		# only remove missing tracks on audiodir scans, but not when searching for playlists only
+		# only remove missing tracks when looking for audio tracks
 		my $inDBOnlyCount = 0;
 		($inDBOnlyCount) = $dbh->selectrow_array( qq{
 			SELECT COUNT(*) FROM ( $inDBOnlySQL ) AS t1
-		} ) if $args->{types} ne 'list';
+		} ) if $args->{types} =~ 'audio';
     	
 		my ($onDiskOnlyCount) = $dbh->selectrow_array( qq{
 			SELECT COUNT(*) FROM ( $onDiskOnlySQL ) AS t1
