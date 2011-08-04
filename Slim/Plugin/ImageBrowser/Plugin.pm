89df2606a4d017a29a16f7f258dea3cf368df947
Michael Herger <mherger@logitech.com>
2011-08-04 18:48:12 +0000

    simple image browser plugin built on top of the UPnP code. Allows browsing images in web UI and SP.
    git-svn-id: file:///home/awy/mirror/slim/7.7/trunk/server@32984 62299810-d8cb-41fd-93b7-d32162e5a4a4

---
+++ Slim/Plugin/ImageBrowser/Plugin.pm
@@ -0,0 +1,190 @@
+package Slim::Plugin::ImageBrowser::Plugin;
+
+# $Id:  $
+
+use strict;
+use base qw(Slim::Plugin::OPMLBased);
+use XML::Simple;
+use JSON::XS::VersionOneAndTwo;
+
+use Slim::Utils::Log;
+use Slim::Utils::Strings qw(string);
+use Slim::Plugin::UPnP::Common::Utils qw(absURL);
+
+my $log = Slim::Utils::Log->addLogCategory( {
+	category     => 'plugin.imagebrowser',
+	defaultLevel => 'WARN',
+	description  => 'PLUGIN_IMAGEBROWSER_MODULE_NAME',
+} );
+
+use constant CONTENTURL => 'plugins/imagegallery/content.json';
+use constant PLUGIN_TAG => 'imagebrowser';
+
+sub initPlugin {
+	my $class = shift;
+
+	$class->SUPER::initPlugin(
+		feed   => absURL( '/' . CONTENTURL ),
+		tag    => PLUGIN_TAG,
+		node   => 'extras',		# used for SP
+		menu   => 'plugins',	# used in web UI
+	);
+	
+	Slim::Control::Request::addDispatch(
+		[ PLUGIN_TAG, 'slideshow' ],
+		[ 0, 1, 1, \&cliSlideshowQuery ]
+	);
+	
+	
+	Slim::Web::Pages->addPageFunction( CONTENTURL, \&renderOPML );
+}
+
+# we depend on Slim::Plugin::UPnP::Plugin
+# can probably be removed if we move SOAP::Lite to the main CPAN folder 
+my $enabled;
+sub condition {
+	
+	return $enabled if defined $enabled;
+	
+	eval {
+		require Slim::Plugin::UPnP::Plugin;
+		require Slim::Plugin::UPnP::MediaServer::ContentDirectory;
+		$enabled = 1;
+	};
+
+	if ($@) {
+		$enabled = 0;
+		main::DEBUGLOG && $log->is_debug && $log->debug($@);
+		$log->warn("The ImageBrowser plugin requires the UPnP plugin to be enabled");
+		return 0;
+	}	
+
+	return 1;
+}
+
+
+# fetch content from UPnP handler, and convert into OPML as understood by XMLBrowser
+sub renderOPML {
+	my ($client, $params) = @_;
+	
+	my ($data, $count, $total) = Slim::Plugin::UPnP::MediaServer::ContentDirectory->Browse(undef, {
+		BrowseFlag     => 'BrowseDirectChildren',
+		Filter         => '*',
+		ObjectID       => $params->{id} || '/images',
+		RequestedCount => 999999,		# get them all, XMLBrowser does the paging... a good idea?
+		SortCriteria   => $params->{sort} || '+dc:title',
+		StartingIndex  => $params->{start} || 0,
+	});	
+	
+	$data = $data->name('Result')->value();
+	
+	my $xml = eval { XMLin($data) };
+
+	if ( $@ ) {
+		$log->error( "Unable to parse: " . $@ );
+		$xml = {};
+	}
+	
+	my $items = [];
+
+	foreach my $itemLoop ($xml->{container}, $xml->{item}) {
+	
+		# normalize hash?!?
+		if ($itemLoop->{id} && $itemLoop->{'upnp:class'}) {
+			$itemLoop = {
+				$itemLoop->{id} => $itemLoop,
+			};
+		}
+	
+		while ( my ($id, $item) = each( %$itemLoop ) ) {
+			if ( $item->{'upnp:class'} =~ /^object.container/ ) {
+				push @$items, {
+					type => 'link',
+					text => $item->{'dc:title'},
+					url  => absURL( '/' . CONTENTURL ) . "?id=$id",
+				}
+			}
+			
+			elsif ( $item->{'upnp:class'} eq 'object.item.imageItem.photo' ) {
+				my ($id) = $item->{'upnp:icon'} =~ m{(?:music|image)/([0-9a-f]{8})/};
+				
+				push @$items, {
+					type => 'link',
+					text => $item->{'dc:title'},
+					owner => $item->{'upnp:album'},
+					date => $item->{'dc:date'},
+					weblink => $item->{res}->{content},
+					image => $item->{'upnp:icon'},
+					jive => $id ? {
+						actions => {
+							go => {
+								player => 0,
+								cmd => [ PLUGIN_TAG, 'slideshow' ],
+								params => {
+									id => $id,
+									slideshow => 1,
+								}
+							},
+						}
+					} : undef
+				};
+			}
+			
+			else {
+				# what here?
+				$log->error('unhandled upnp:class? ' . Data::Dump::dump($item));
+			}
+		}
+		
+	}
+	
+	push @$items, {
+		type => 'text',
+		text => Slim::Utils::Strings::clientString($client, 'EMPTY'),
+	} if not scalar @$items;
+	
+	main::DEBUGLOG && $log->is_debug && $log->debug( Data::Dump::dump($items) );
+	
+	# create OPML
+	$data = eval { to_json({
+		body => {
+			outline => $items,
+		},
+		head => {
+			title => Slim::Utils::Strings::clientString($client, 'PLUGIN_IMAGEBROWSER_MODULE_NAME'),
+			cachetime => 0,		# don't cache result
+		},
+	}) };
+
+	return \$data;
+}
+
+sub cliSlideshowQuery {
+	my $request = shift;
+
+	if ($request->isNotQuery([[PLUGIN_TAG]])) {
+		$request->setStatusBadDispatch();
+		return;
+	}
+	
+	$request->setStatusProcessing();
+	
+	my $id = $request->getParam('id');
+	
+	my $sub = Slim::Control::Request->new( undef, [ 'image_titles', 0, 1, 'image_id:' . $id, 'tags:tnl' ] );
+	$sub->execute();
+	
+	my $results = $sub->getResults;
+	my $imageInfo = $results->{images_loop}->[0];
+	
+	$request->addResult( data => [{
+		image => "image/$id/cover{resizeParams}",
+		name  => $imageInfo->{title},
+		date  => $imageInfo->{date} || '',
+		owner => $imageInfo->{album} || '',
+	}] );
+	$request->addResult( offset => 0 );
+	$request->setStatusDone();
+}
+
+1;
