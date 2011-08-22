96de7ad2b5b31a1c1fcf38fa0923103ccc17a4d9
Michael Herger <mherger@logitech.com>
2011-08-22 15:06:40 +0000

    URI encode scan message, as it might break JS execution
    git-svn-id: file:///home/awy/mirror/slim/7.6/trunk/server@33145 62299810-d8cb-41fd-93b7-d32162e5a4a4

--- HTML/EN/html/SqueezeJS/UI.js
+++ HTML/EN/html/SqueezeJS/UI.js
@@ -2172,14 +2172,14 @@ SqueezeJS.UI.ScannerInfoExtended = function(){
 			}
 
 			if (result.message && result['total_time']) {
-				Ext.get('message').update(result.message + '<br>' + SqueezeJS.string('total_time') + '&nbsp;' + result.total_time);
+				Ext.get('message').update(decodeURIComponent(result.message) + '<br>' + SqueezeJS.string('total_time') + '&nbsp;' + result.total_time);
 				
 				if (Ext.get('abortscanlink'))
 					Ext.get('abortscanlink').hide();
 			}
 
 			else
-				Ext.get('message').update(result.message);
+				Ext.get('message').update(decodeURIComponent(result.message));
 
 			progressTimer.delay(5000)
 		}
