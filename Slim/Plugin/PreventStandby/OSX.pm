df5c824afc1347b8bd51852269be7807e69bf248
mherger <mherger@logitech.com>
2012-09-12 11:10:16 +0200

    Improve activity detection on OSX to prevent standby
    - use shorter polling interval, as 10.8 would go back to sleep within
    30s
    - re-factor code to provide a base class for OS dependent
    implementations
    
    Bugzilla 8141
    
    Change-Id: Ifa91c978cf892b51bfd73446b61aa57466e63eab

--- Slim/Plugin/PreventStandby/OSX.pm
+++ Slim/Plugin/PreventStandby/OSX.pm
@@ -5,13 +5,18 @@ package Slim::Plugin::PreventStandby::OSX;
 # modify it under the terms of the GNU General Public License,
 # version 2.
 
+use base qw(Slim::Plugin::PreventStandby::OS);
+
 use strict;
 use Proc::Background;
 
-use Slim::Utils::Log;
 use Slim::Plugin::PreventStandby::Plugin;
+use Slim::Utils::Log;
+use Slim::Utils::Prefs;
+use Slim::Utils::Timers;
 
 my $log   = logger('plugin.preventstandby');
+my $prefs = preferences('plugin.preventstandby');
 
 my $command;
 my $process;
@@ -26,30 +31,57 @@ sub new {
 	}
 	else {
 		$log->warn("Didn't find pmset tool - standby can't be prevented!");
+		return;
 	}
 	
 	return $class;
 }
 
-sub isBusy {
-	my ($class, $currenttime) = @_;
-	return !Slim::Plugin::PreventStandby::Plugin->_hasResumed($currenttime) && Slim::Plugin::PreventStandby::Plugin->_playersBusy();
-}
-
-# shut down caffeinate when the plugin is being shut down
+# shut down pmset when the plugin is being shut down
 sub cleanup {
 	$process->die if $process;
 };
 
 sub setBusy {
+	my ($class, $currenttime) = @_;
+
+	# Bug 8141: when coming out of standby, we don't want to keep the system alive for the full defined idle time
+	# OSX 10.8+ often resumes for some clean up work etc. In those cases only keep LMS awake for a few minutes
+	# before we can determine that we are really busy. Then either go back to sleep or to business as usual.
+	if ( Slim::Plugin::PreventStandby::Plugin->_hasResumed($currenttime) ) {
+		my $idletime = $prefs->get('idletime');
+
+		Slim::Utils::Timers::killTimers( undef, \&_setShortIdleTime );
+		if ($idletime && $idletime > 2) {
+			Slim::Utils::Timers::setTimer(
+				undef, 
+				time + 60, 
+				\&_setShortIdleTime
+			);
+		}
+	}
+	
 	if (!$process || !$process->alive) {
-		$log->debug("Injecting some caffein to keep system alive: '$command'");
+		main::DEBUGLOG && $log->debug("Injecting some caffeine to keep system alive: '$command'");
 		$process = Proc::Background->new($command);
 	}
 }
 
+sub _setShortIdleTime {
+	my $idletime = $prefs->get('idletime');
+
+	main::DEBUGLOG && $log->debug("System came out of standby - keep alive for only a few minutes");
+
+	Slim::Plugin::PreventStandby::Plugin->hasBeenIdle($idletime - 2) if $idletime && $idletime > 2;
+}
+
 sub setIdle {
 	shift->cleanup;
+}
+
+# use short polling interval, or we might miss some activity after resume
+sub pollInterval {
+	return 10;
 }
 
 1;
