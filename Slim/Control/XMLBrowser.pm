963d445bd2f574baa6c34986fa3b978f6273d25e
mherger <mherger@logitech.com>
2013-04-24 16:17:30 +0200

    Aggressively cache artwork URLs for http streams.

--- Slim/Control/XMLBrowser.pm
+++ Slim/Control/XMLBrowser.pm
@@ -1018,6 +1018,16 @@ sub _cliQuery_done {
 						}
 					}
 					
+					my $isPlayable = (
+						   $item->{play} 
+						|| $item->{playlist} 
+						|| ($item->{type} && ($item->{type} eq 'audio' || $item->{type} eq 'playlist'))
+					);
+			
+					# keep track of station icons
+					if ( $isPlayable && $item->{url} =~ /^http/ && $item->{url} !~ m|\.com/api/\w+/v1/opml| && (my $cover = ($item->{image} || $item->{cover})) ) {
+						$cache->set("remote_image_" . $item->{url}, $cover, 86400);
+					}
 					
 					if ($menuMode) {
 						my %hash;
@@ -1092,12 +1102,6 @@ sub _cliQuery_done {
 							$itemText = ( $item->{line1} || $nameOrTitle ) . "\n" . $line2;
 						}
 						$hash{'text'} = $itemText;
-						
-						my $isPlayable = (
-							   $item->{play} 
-							|| $item->{playlist} 
-							|| ($item->{type} && ($item->{type} eq 'audio' || $item->{type} eq 'playlist'))
-						);
 						
 						if ($isPlayable) {
 							my $presetParams = _favoritesParams($item);
