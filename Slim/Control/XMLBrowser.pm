51a95b5d0d3d50489b4c9f3c405a7bdd3c6f6734
mherger <mherger@logitech.com>
2013-04-24 12:33:38 +0200

    Keep track of station icons to be displayed in Now Playing screens etc.

--- Slim/Control/XMLBrowser.pm
+++ Slim/Control/XMLBrowser.pm
@@ -692,6 +692,7 @@ sub _cliQuery_done {
 						ct      => $subFeed->{'mime'},
 						secs    => $subFeed->{'duration'},
 						bitrate => $subFeed->{'bitrate'},
+						cover   => $subFeed->{'image'} || $subFeed->{'cover'} || $request->getParam('icon'),
 					} );
 				
 					$client->execute([ 'playlist', $method, $url ]);
@@ -730,6 +731,7 @@ sub _cliQuery_done {
 						ct      => $item->{'mime'},
 						secs    => $item->{'duration'},
 						bitrate => $item->{'bitrate'},
+						cover   => $subFeed->{'image'} || $subFeed->{'cover'} || $request->getParam('icon'),
 					} );
 					
 					main::idleStreams();
--- Slim/Web/XMLBrowser.pm
+++ Slim/Web/XMLBrowser.pm
@@ -620,6 +620,7 @@ sub handleFeed {
 				ct      => $streamItem->{'mime'},
 				secs    => $streamItem->{'duration'},
 				bitrate => $streamItem->{'bitrate'},
+				cover   => $streamItem->{'image'} || $streamItem->{'cover'},
 			} );
 		
 			$client->execute([ 'playlist', $action, $url ]);
@@ -658,6 +659,7 @@ sub handleFeed {
 				ct      => $item->{'mime'},
 				secs    => $item->{'duration'},
 				bitrate => $item->{'bitrate'},
+				cover   => $item->{'image'} || $item->{'cover'},
 			} );
 			
 			main::idleStreams();
@@ -784,6 +786,11 @@ sub handleFeed {
 					$_->{'showYear'}   = 1 if ($prefs->get('showYear')   && $_->{'year'});
 					$_->{'showArtist'} = 1 if ($prefs->get('showArtist') && $_->{'artist'});
 				}
+			}
+			
+			# keep track of station icons
+			if ( my $cover = ($_->{image} || $_->{cover}) ) {
+				$cache->set("remote_image_" . $_->{url}, $cover, 86400 * 30);
 			}
 		}
 
