07e94b94c7326c1fb3e4d40cb647ae71dc98eaa5
mherger <mherger@logitech.com>
2013-09-12 10:15:51 +0200

    Fix error handler: the first passed value is not the $http object, but the error message already.

--- Slim/Plugin/InternetRadio/Plugin.pm
+++ Slim/Plugin/InternetRadio/Plugin.pm
@@ -73,8 +73,7 @@ sub _gotRadio {
 
 my $retry = 5;
 sub _gotRadioError {
-	my $http  = shift;
-	my $error = $http->error;
+	my $error = shift;
 	
 	$log->error( "Unable to retrieve radio directory from SN: $error" );
 	
