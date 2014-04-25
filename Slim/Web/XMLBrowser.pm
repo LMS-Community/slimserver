0810f0915378c9a9a9b6339353d040d4d93206e6
Michael Herger <mherger@logitech.com>
2014-04-25 13:41:53 +0200

    Fix browsing New Music cache

--- Slim/Web/XMLBrowser.pm
+++ Slim/Web/XMLBrowser.pm
@@ -1234,7 +1234,7 @@ sub webLink {
 	my $renderCacheKey;
 	if ( $args->{path} =~ /\bbrowselibrary\b.*?\bmode=(?:artists|albums|genres|years)\b/ ) {
 		# cache key needs to make sure we respect the various prefs and cookies which control the display mode...
-		$renderCacheKey = join(':', 'blweb', Slim::Music::Import->lastScanTime, $index, $quantity, $params{mode}, map { $args->{$_} || $params{$_} || '' } qw(artwork player sess index start systemSkin skinOverride systemLanguage webroot thumbSize serverResizesArt orderBy) );
+		$renderCacheKey = join(':', 'blweb', Slim::Music::Import->lastScanTime, $index, $quantity, $params{mode}, $params{sort}, map { $args->{$_} || '' } qw(artwork player sess index start systemSkin skinOverride systemLanguage webroot thumbSize serverResizesArt orderBy) );
 
 		if ( my $cached = Slim::Utils::Cache->new->get($renderCacheKey) ) {
 			main::DEBUGLOG && $log->debug("Returning cached copy of rendered HTML page.");
