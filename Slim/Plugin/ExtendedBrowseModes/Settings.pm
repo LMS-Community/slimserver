bcf340739249f19ea98b0647dfa08f9d5df50354
Michael Herger <mherger@logitech.com>
2014-05-23 14:33:03 +0200

    Working settings page for the extended browse modes

--- Slim/Plugin/ExtendedBrowseModes/Settings.pm
+++ Slim/Plugin/ExtendedBrowseModes/Settings.pm
@@ -30,6 +30,81 @@ sub prefs {
 
 sub handler {
 	my ($class, $client, $params) = @_;
+	
+	# Restart if restart=1 param is set
+	if ( $params->{restart} ) {
+		$params = Slim::Web::Settings::Server::Plugins->restartServer($params, 1);
+	}
+
+	if ($params->{'saveSettings'}) {
+		my $menus = $prefs->get('menus');
+		
+		for (my $i = 1; defined $params->{"id$i"}; $i++) {
+			
+			if ( $params->{"delete$i"} ) {
+				$menus = [ grep { $_->{id} ne $params->{"id$i"} } @$menus ];
+				next;
+			}
+
+			my ($menu) = $params->{"id$i"} eq '_new_' ? {} : grep { $_->{id} eq $params->{"id$i"} } @$menus;
+
+			next unless $menu;
+			
+			if ($menu->{dontEdit}) {
+				$menu->{enabled} = $params->{"enabled$i"};
+				next;
+			}
+			else {
+				next unless $params->{"name$i"} && $params->{"feed$i"} && ($params->{"roleid$i"} || $params->{"genreid$i"});
+
+				if ( $params->{"id$i"} eq '_new_' ) {
+					
+					$params->{"enabled$i"} = '1';
+					
+					$menu = {
+						id => join('_', $params->{"feed$i"}, $params->{"roleid$i"} || '', $params->{"genreid$i"} || '', time),
+					};
+					
+					if ( $params->{"feed$i"} eq 'albums' ) {
+						$menu->{id}     = 'myMusicAlbums' . $menu->{id};
+						$menu->{weight} = 25;
+					}
+					else {
+						$menu->{id}     = 'myMusicArtists' . $menu->{id};
+						$menu->{weight} = 15;
+					}
+					
+					push @$menus, $menu;
+				}
+
+				# adjust icon and weight if the feed type has changed
+				$menu->{icon} = 'html/images/' . $params->{"feed$i"} . '.png';
+				$menu->{weight} = ($params->{"feed$i"} eq 'albums' ? 25 : 15) unless $menu->{feed} && $menu->{feed} eq $params->{"feed$i"};
+				
+				foreach (qw(enabled feed name)) {
+					$menu->{$_} = $params->{$_ . $i};
+				}
+				
+				if ($params->{"roleid$i"}) {
+					$menu->{params}->{role_id} = $params->{"roleid$i"}; 
+				}
+				else {
+					delete $menu->{params}->{role_id};
+				}
+				
+				if ($params->{"genreid$i"}) {
+					$menu->{params}->{genre_id} = $params->{"genreid$i"}; 
+				}
+				else {
+					delete $menu->{params}->{genre_id};
+				}
+			}
+		}
+		
+		$prefs->set('menus', $menus);
+		
+		$params = Slim::Web::Settings::Server::Plugins->getRestartMessage($params, Slim::Utils::Strings::string('CLEANUP_PLEASE_RESTART_SC'));
+	}
 
 	my $rs = Slim::Schema->search('Genre');
 	
@@ -47,7 +122,7 @@ sub handler {
 	
 	$params->{roles} = { map { $_ => Slim::Schema::Contributor->typeToRole($_) } Slim::Schema::Contributor->contributorRoles };
 	$params->{extended_menus} = [ sort { $b->{dontEdit} <=> $a->{dontEdit} } @{ Storable::dclone($prefs->get('menus')) } ];
-	push @{$params->{extended_menus}}, {};
+	push @{$params->{extended_menus}}, { id => '_new_' };
 	
 
 	$class->SUPER::handler($client, $params);
