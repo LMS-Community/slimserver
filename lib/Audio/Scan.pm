8bcd23b10407b9106e2e341238e3607382f3d892
Michael Herger <mherger@logitech.com>
2014-07-22 11:35:32 +0200

    Revert the order in which we try to load the Audio::Scan binary module versions. Some older Perl versions (5.8?) seem to have a problem trying more than once or twice. Those systems most likely will never see more recent Audio::Scan versions. Let's therefore try the oldest version first.

--- lib/Audio/Scan.pm
+++ lib/Audio/Scan.pm
@@ -7,7 +7,7 @@ our $VERSION;
 require XSLoader;
 
 BEGIN {
-	foreach ('0.95', '0.94', '0.93') {
+	foreach ('0.93', '0.95', '0.94') {
 		eval { XSLoader::load('Audio::Scan', $_); };
 		
 		if (!$@) {
