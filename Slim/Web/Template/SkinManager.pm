95de55a95d6fe8df374571ef2a713164f779ef72
Michael Herger <mherger@logitech.com>
2014-10-24 12:25:54 +0200

    Use random value as the contentitem identifier. There can be more than one loop per page.

--- Slim/Web/Template/SkinManager.pm
+++ Slim/Web/Template/SkinManager.pm
@@ -224,7 +224,7 @@ sub addSkinTemplate {
 			'utf8encode'        => \&Slim::Utils::Unicode::utf8encode,
 			'utf8on'            => \&Slim::Utils::Unicode::utf8on,
 			'utf8off'           => \&Slim::Utils::Unicode::utf8off,
-			'resizeimage'       => [ \&_resizeImage, 1 ],  
+			'resizeimage'       => [ \&_resizeImage, 1 ], 
 			'imageproxy'        => [ sub {
 				return _resizeImage($_[0], $_[1], $_[2], '-');
 			}, 1 ],
