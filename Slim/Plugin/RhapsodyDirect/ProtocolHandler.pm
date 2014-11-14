a39fb3fc93970684e52f7aa3581c36c7e5b21a78
Michael Herger <mherger@logitech.com>
2014-11-14 09:39:37 +0100

    When looking up metadata for Deezer/Rhapsody, don't forget to add the trackId which triggered the lookup.

--- Slim/Plugin/RhapsodyDirect/ProtocolHandler.pm
+++ Slim/Plugin/RhapsodyDirect/ProtocolHandler.pm
@@ -550,8 +550,11 @@ sub getMetadataFor {
 	my $meta      = $cache->get( 'rhapsody_meta_' . $trackId );
 	
 	if ( !$meta && !$client->master->pluginData('fetchingMeta') ) {
+
+		$client->master->pluginData( fetchingMeta => 1 );
+		
 		# Go fetch metadata for all tracks on the playlist without metadata
-		my @need;
+		my @need = ($trackId);
 		
 		for my $track ( @{ Slim::Player::Playlist::playList($client) } ) {
 			my $trackURL = blessed($track) ? $track->url : $track;
@@ -566,8 +569,6 @@ sub getMetadataFor {
 		if ( main::DEBUGLOG && $log->is_debug ) {
 			$log->debug( "Need to fetch metadata for: " . join( ', ', @need ) );
 		}
-		
-		$client->master->pluginData( fetchingMeta => 1 );
 		
 		my $metaUrl = Slim::Networking::SqueezeNetwork->url(
 			"/api/rhapsody/v1/playback/getBulkMetadata"
