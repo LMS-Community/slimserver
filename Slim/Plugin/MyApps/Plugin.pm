b6ab2610c33eff41e36c563dc3a1a9bacd874d9c
Michael Herger <mherger@logitech.com>
2015-02-27 09:47:13 +0100

    Don't try to fetch an apps list from an undefined URL

--- Slim/Plugin/MyApps/Plugin.pm
+++ Slim/Plugin/MyApps/Plugin.pm
@@ -79,16 +79,32 @@ sub feed {
 				$callback->($feed);
 			};
 				
-			Slim::Formats::XML->getFeedAsync(
-
-				$mergeCB, 
-
-				sub { $mergeCB->({ items => [] }) },	
-
-				{ client => $client, url => $feedUrl, timeout => 35 },
-			);
+			if (main::NOMYSB) {
+				$mergeCB->({ items => [] });
+			}
+			else {
+				Slim::Formats::XML->getFeedAsync(
+	
+					$mergeCB, 
+	
+					sub { $mergeCB->({ items => [] }) },	
+	
+					{ client => $client, url => $feedUrl, timeout => 35 },
+				);
+			}
 		}
 			
+	} elsif (main::NOMYSB) {
+		
+		return sub {
+			my ($client, $callback, $args) = @_;
+
+			$callback->({ items => [ {
+				name => $client->string('PLUGIN_MY_APPS_NO_APPS'),
+				type => 'text'
+			} ] });
+		}
+		
 	} else {
 
 		return $feedUrl;
