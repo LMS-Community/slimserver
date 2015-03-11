b14b51debc8e5e1de987b5406a42bb72facc671b
Michael Herger <mherger@logitech.com>
2015-03-11 07:41:19 -0700

    Simplify DST handling on Windows.
    Don't modify the changedOnlySQL query, but run a simple update on the timestamp column.

--- Slim/Utils/Scanner/Local.pm
+++ Slim/Utils/Scanner/Local.pm
@@ -240,8 +240,18 @@ sub rescan {
 			WHERE scanned_files.url LIKE '$basedir%'
 		};
 
-		if ( my $offset = Slim::Utils::OSDetect::getOS->getScanTimeOffset() ) {
-			$changedOnlySQL =~ s/(!= tracks.timestamp)/$1 + $offset/;
+		# bug 18078 - Windows doesn't handle DST changes in a file's timestamp correctly. We need to do this on our end.
+		if ( main::ISWINDOWS && !(main::SCANNER && $main::wipe) ) {
+			my $isDST = (localtime(time()))[8] ? 1 : 0;
+	
+			if ( $isDST != Slim::Music::Import->getLastScanTimeIsDST() ) {
+				my $offset = $isDST ? 3600 : -3600;
+				
+				$log->error("DST has changed since last scan - fix timestamps in tracks table");
+				
+				$dbh->do("UPDATE tracks SET timestamp = (timestamp + $offset)");
+				Slim::Music::Import->setLastScanTimeIsDST();
+			}
 		}
 		
 		# only remove missing tracks when looking for audio tracks
@@ -868,7 +878,7 @@ sub changed {
 	my $content_type = _content_type($url);
 	
 	if ( Slim::Music::Info::isSong($url, $content_type) ) {
-		$log->error("Handling changed audio track $url") unless main::SCANNER && $main::progress;
+		main::INFOLOG && $log->is_info && !(main::SCANNER && $main::progress) && $log->info("Handling changed audio track $url");
 		
 		my $work = sub {
 			# Fetch some original track, album, contributors, and genre information
