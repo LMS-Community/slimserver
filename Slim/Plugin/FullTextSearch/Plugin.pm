b3be43ecf2931b0e473d7e9724e49de2feb1ea19
Michael Herger <mherger@logitech.com>
2015-07-23 14:19:16 +0200

    Remove legacy comment

--- Slim/Plugin/FullTextSearch/Plugin.pm
+++ Slim/Plugin/FullTextSearch/Plugin.pm
@@ -106,7 +106,6 @@ sub initPlugin {
 	# no need to continue in scanner mode
 	return if main::SCANNER;
 
-	# XXXX - need some method to trigger re-build when user uses eg. BMF to add new music
 	Slim::Control::Request::subscribe( sub {
 		$prefs->remove('popularTerms');
 		_initPopularTerms();
