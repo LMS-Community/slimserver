646d0ce16988efd5684c9f2819609552a1f78988
Michael Herger <mherger@logitech.com>
2015-12-05 23:19:49 +0100

    Add "info total duration ?" query to get the total duration of the music library (in seconds).

--- Slim/Control/Queries.pm
+++ Slim/Control/Queries.pm
@@ -1463,7 +1463,7 @@ sub infoTotalQuery {
 	my $request = shift;
 
 	# check this is the correct query.
-	if ($request->isNotQuery([['info'], ['total'], ['genres', 'artists', 'albums', 'songs']])) {
+	if ($request->isNotQuery([['info'], ['total'], ['genres', 'artists', 'albums', 'songs', 'duration']])) {
 		$request->setStatusBadDispatch();
 		return;
 	}
@@ -1473,10 +1473,10 @@ sub infoTotalQuery {
 		return;
 	}
 	
-	my $totals = Slim::Schema->totals;
-	
 	# get our parameters
 	my $entity = $request->getRequest(2);
+	
+	my $totals = Slim::Schema->totals if $entity ne 'duration';
 
 	if ($entity eq 'albums') {
 		$request->addResult("_$entity", $totals->{album});
@@ -1489,6 +1489,9 @@ sub infoTotalQuery {
 	}
 	elsif ($entity eq 'songs') {
 		$request->addResult("_$entity", $totals->{track});
+	}
+	elsif ($entity eq 'duration') {
+		$request->addResult("_$entity", Slim::Schema->totalTime());
 	}
 	
 	$request->setStatusDone();
