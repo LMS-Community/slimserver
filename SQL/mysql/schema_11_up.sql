43da235b03b361d3706abca358e0f4c533babdb6
Bart Sjerps <development@outrun.nl>
2016-01-27 19:24:22 +0100

    Fix for utf8 and temp table

--- SQL/mysql/schema_11_up.sql
+++ SQL/mysql/schema_11_up.sql
@@ -4,5 +4,5 @@ CREATE TABLE scanned_files (
   url TEXT NOT NULL,
   timestamp int(10),
   filesize int(10)
-) ENGINE=InnoDB CHARACTER SET utf8 COLLATE utf8_unicode_ci;
+) ENGINE=InnoDB CHARACTER SET utf8;
 CREATE INDEX scannedUrlIndex ON scanned_files (url(255));
