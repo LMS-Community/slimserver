d9b348ab755f250f6881fa94067200d1e9d1c899
Michael Herger <mherger@logitech.com>
2016-05-13 16:57:08 +0200

    When downloading plugins, we don't want the user to restart before we're done downloading. But we don't want to silently return to the download page either, it's confusing. Therefore wait until the download has finished or we're timed out, before returning to the user.

--- Slim/Plugin/Extensions/Settings.pm
+++ Slim/Plugin/Extensions/Settings.pm
@@ -9,6 +9,8 @@ use Slim::Utils::OSDetect;
 
 use Digest::MD5;
 
+use constant MAX_DOWNLOAD_WAIT => 20;
+
 my $prefs = preferences('plugin.extensions');
 my $log   = logger('plugin.extensions');
 
@@ -129,8 +131,27 @@ sub _getReposCB {
 	}
 
 	if ( --$data->{'remaining'} <= 0 ) {
+		
+		my $pageInfo = $class->_addInfo($client, $params, $data);
+		
+		my $finalize;
+		my $timeout = MAX_DOWNLOAD_WAIT;
+		
+		$finalize = sub {
+			Slim::Utils::Timers::killTimers(undef, $finalize);
+			
+			# if a plugin is still being downloaded, wait a bit longer, or the user might restart the server before we're done
+			if ( $timeout-- > 0 && Slim::Utils::PluginDownloader->downloading ) {
+				Slim::Utils::Timers::setTimer(undef, time() + 1, $finalize);
+				
+				main::DEBUGLOG && $log->is_debug && $log->debug("PluginDownloader is still busy - waiting a little longer...");
+				return;
+			}
+			
+			$callback->($client, $params, $pageInfo, @$args);
+		};
 
-		$callback->($client, $params, $class->_addInfo($client, $params, $data), @$args);
+		$finalize->();
 	}
 }
 
@@ -273,7 +294,7 @@ sub _addInfo {
 	$params->{'rand'}     = $rand;
 
 	# don't offer the restart before the plugin download has succeeded.
-	my $needsRestart = Slim::Utils::PluginManager->needsRestart; # || Slim::Utils::PluginDownloader->downloading;
+	my $needsRestart = Slim::Utils::PluginManager->needsRestart || Slim::Utils::PluginDownloader->downloading;
 
 	$params->{'warning'} = $needsRestart ? Slim::Utils::Strings::string("PLUGIN_EXTENSIONS_RESTART_MSG") : '';
 
