a1acedfa438e4da22f91de2df9a314b949dab291
AlecMahone <39433106+AlecMahone@users.noreply.github.com>
2018-06-14 00:49:04 +0900

    Update Win32.pm

--- Slim/Utils/OS/Win32.pm
+++ Slim/Utils/OS/Win32.pm
@@ -305,7 +305,7 @@ sub getFileName {
 	}
 	
 	# display full name if we got a Windows 8.3 file name
-	if ($path =~ /~/) {
+	if ($path =~ /~\d$/) {
 
 		if (my $n = Win32::GetLongPathName($path)) {
 			$n = File::Basename::basename($n);
