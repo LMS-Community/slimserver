d73537a43878c125d9aa2bd871a677fba77c32c2
Peter Oliver <git@mavit.org.uk>
2019-06-25 18:01:18 +0100

    Verify HTTPS connections
    Setting VERIFY_NONE makes us vulnerable to man-in-the-middle attacks, and undermines the purpose of HTTPS.

--- Slim/Player/Protocols/HTTPS.pm
+++ Slim/Player/Protocols/HTTPS.pm
@@ -31,7 +31,6 @@ sub new {
 		PeerAddr => $server,
 		PeerPort => $port,
 		SSL_startHandshake => 1,
-		SSL_verify_mode => Net::SSLeay::VERIFY_NONE()		# SSL_VERIFY_NONE isn't recognized on some platforms?!?, and 0x00 isn't always "right"
 	) or do {
 
 		$log->error("Couldn't create socket binding to $main::localStreamAddr with timeout: $timeout - $!");
