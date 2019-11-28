cae441494d6e1ca741a3f5ab9e370e73e3bbc5fb
Michael Herger <mherger@logitech.com>
2019-11-28 17:43:46 +0100

    Don't lock up LMS while purging caches.
    It can lead to timeouts on startup interaction with mysb.com and otherwise delay the startup procedure. I've seen lockups of 15-20 seconds while purging a largish image proxy cache file.

--- Slim/Utils/DbCache.pm
+++ Slim/Utils/DbCache.pm
@@ -22,7 +22,7 @@ sub new {
 	if ( !defined $args->{root} ) {
 		require Slim::Utils::Prefs;
 		$args->{root} = Slim::Utils::Prefs::preferences('server')->get('cachedir');
-		
+
 		# Update root value if librarycachedir changes
 		Slim::Utils::Prefs::preferences('server')->setChange( sub {
 			$self->wipe;
@@ -30,9 +30,9 @@ sub new {
 			$self->_init_db;
 		}, 'cachedir' );
 	}
-	
+
 	$args->{default_expires_in} ||= DEFAULT_EXPIRES_TIME;
-	
+
 	return bless $args, $self;
 }
 
@@ -42,13 +42,13 @@ sub getRoot {
 
 sub setRoot {
 	my ( $self, $root ) = @_;
-	
+
 	$self->{root} = $root;
 }
 
 sub wipe {
 	my $self = shift;
-	
+
 	if ( my $dbh = $self->_init_db ) {
 		$dbh->do('DELETE FROM cache'); # truncate
 		$self->_close_db;
@@ -58,17 +58,17 @@ sub wipe {
 
 sub set {
 	my ( $self, $key, $data, $expiry ) = @_;
-	
+
 	$self->_init_db;
-	
+
 	$expiry = _canonicalize_expiration_time(defined $expiry ? $expiry : $self->{default_expires_in});
 
 	my $id = _key($key);
-	
+
 	if (ref $data) {
 		$data = freeze( $data );
 	}
-	
+
 	# Insert or replace the value
 	my $set = $self->{set_sth};
 	$set->bind_param( 1, $id );
@@ -79,7 +79,7 @@ sub set {
 
 sub get {
 	my ( $self, $key ) = @_;
-	
+
 	$self->_init_db;
 
 	my $id = _key($key);
@@ -88,16 +88,16 @@ sub get {
 	$get->execute($id);
 
 	my ($data, $expiry) = $get->fetchrow_array;
-	
+
 	if ($expiry && !$self->{noexpiry} && $expiry >= 0 && $expiry < time()) {
 		$data = undef;
 #		$self->{delete_sth}->execute($id);
 	}
-	
+
 	eval {
 		$data = thaw($data);
 	} if $data;
-	
+
 	return $data;
 }
 
@@ -105,22 +105,29 @@ sub remove {
 	my ( $self, $key ) = @_;
 
 	$self->_init_db;
-	
+
 	my $id = _key($key);
 	$self->{delete_sth}->execute($id);
 }
 
 sub purge {
 	my ( $self ) = @_;
-	
+
 	my $dbh = $self->_init_db;
-	
+
+	$dbh->sqlite_progress_handler(1000, sub {
+		main::idle();
+		return;
+	}) if !main::SCANNER;
+
 	$dbh->do('DELETE FROM cache WHERE t >= 0 AND t < ' . time());
+
+	$dbh->sqlite_progress_handler(0, undef) if !main::SCANNER;
 }
 
 sub _key {
 	my ( $key ) = @_;
-	
+
 	# Get a 60-bit unsigned int from MD5 (SQLite uses 64-bit signed ints for the key)
 	# Have to concat 2 values here so it works on a 32-bit machine
 	my $md5 = Digest::MD5::md5_hex($key);
@@ -129,7 +136,7 @@ sub _key {
 
 sub pragma {
 	my ( $self, $pragma ) = @_;
-	
+
 	if ( my $dbh = $self->_init_db ) {
 		$dbh->do("PRAGMA $pragma");
 
@@ -142,7 +149,7 @@ sub pragma {
 
 sub close {
 	my $self = shift;
-	
+
 	$self->_close_db;
 }
 
@@ -161,7 +168,7 @@ my %_Expiration_Units = ( map(($_,             1), qw(s second seconds sec)),
 # of seconds from the present.  E.g, "10 minutes" returns "600"
 sub _canonicalize_expiration_time {
 	my ( $expiry ) = @_;
-	
+
 	if ( lc( $expiry ) eq 'now' ) {
 		$expiry = 0;
 	}
@@ -183,15 +190,15 @@ sub _canonicalize_expiration_time {
 	if ( $expiry <= 2592000 && $expiry > -1 ) {
 		$expiry += time();
 	}
-	
+
 	return $expiry;
 }
 
 sub _get_dbfile {
 	my $self = shift;
-	
+
 	my $namespace = $self->{namespace};
-	
+
 	# namespace should not be longer than 8 characters on Windows, as it was causing DB corruption
 	if ( main::ISWINDOWS && length($namespace) > 8 ) {
 		$namespace = lc(substr($namespace, 0, 4)) . substr(Digest::MD5::md5_hex($namespace), 0, 4);
@@ -200,7 +207,7 @@ sub _get_dbfile {
 	elsif ( !main::ISWINDOWS ) {
 		$namespace =~ s/\//-/g;
 	}
-	
+
 	return catfile( $self->{root}, $namespace . '.db' );
 }
 
@@ -210,13 +217,13 @@ my $rebuilt;
 sub _init_db {
 	my $self  = shift;
 	my $retry = shift;
-	
+
 	return $self->{dbh} if $self->{dbh};
-	
+
 	my $dbfile = $self->_get_dbfile;
-	
+
 	my $dbh;
-	
+
 	eval {
 		$dbh = DBI->connect( "dbi:SQLite:dbname=$dbfile", '', '', {
 			AutoCommit => 1,
@@ -232,7 +239,7 @@ sub _init_db {
 			# only enable auto_vacuum when a file is newly created
 			#$dbh->do('VACUUM');
 		}
-		
+
 		$dbh->do('PRAGMA synchronous = OFF');
 		$dbh->do('PRAGMA journal_mode = WAL');
 		# scanner is heavy on writes, server on reads - tweak accordingly
@@ -252,7 +259,7 @@ sub _init_db {
 		}
 
 		$dbh->do('PRAGMA journal_size_limit = ' . ($dbjournalsize * 1024 * 1024)) if defined $dbjournalsize;
-	
+
 		# Increase cache size when using dbhighmem, and reduce it to 300K otherwise
 		if ( $dbhighmem ) {
 			$dbh->do('PRAGMA cache_size = 20000');
@@ -261,7 +268,7 @@ sub _init_db {
 		else {
 			$dbh->do('PRAGMA cache_size = 300');
 		}
-	
+
 		# Create the table, note that using an integer primary key
 		# is much faster than any other kind of key, such as a char
 		# because it doesn't have to create an index
@@ -273,21 +280,21 @@ sub _init_db {
 			$dbh->do('CREATE INDEX IF NOT EXISTS expiry ON cache (t)');
 		}
 	};
-	
+
 	if ( $@ ) {
 		if ( $retry ) {
 			# Give up after 2 tries
 			die "Unable to read/create $dbfile\n";
 		}
-		
+
 		warn "$@Delete the file $dbfile and start from scratch.\n";
-		
+
 		# Make sure cachedir exists
 		Slim::Utils::Prefs::makeCacheDir() unless main::RESIZER;
-		
+
 		# Something was wrong with the database, delete it and try again
 		unlink $dbfile;
-		
+
 		return $self->_init_db(1);
 	}
 
@@ -296,11 +303,11 @@ sub _init_db {
 		my ($msg, $handle, $value) = @_;
 
 		my $dbfile = $self->_get_dbfile;
-		
+
 		require Slim::Utils::Log;
 		Slim::Utils::Log::logBacktrace($msg);
 		Slim::Utils::Log::logError($dbfile);
-		
+
 		if ( $msg =~ /SQLite.*(?:database disk image is malformed|is not a database)/i ) {
 			# we've already tried to recover - give up
 			if ($rebuilt++) {
@@ -314,7 +321,7 @@ sub _init_db {
 			}
 		}
 	};
-	
+
 	# Prepare statements we need
 	if ($self->{noexpiry}) {
 		$self->{set_sth} = $dbh->prepare('INSERT OR REPLACE INTO cache (k, v) VALUES (?, ?)');
@@ -325,22 +332,22 @@ sub _init_db {
 		$self->{get_sth} = $dbh->prepare('SELECT v, t FROM cache WHERE k = ?');
 	}
 	$self->{delete_sth} = $dbh->prepare('DELETE FROM cache WHERE k = ?');
-	
+
 	$self->{dbh} = $dbh;
-	
+
 	return $dbh;
 }
 
 sub _close_db {
 	my $self = shift;
-	
+
 	if ( $self->{dbh} ) {
 		$self->{set_sth}->finish;
 		$self->{get_sth}->finish;
 		$self->{delete_sth}->finish;
-		
+
 		$self->{dbh}->disconnect;
-	
+
 		delete $self->{$_} for qw(set_sth get_sth dbh);
 	}
 }
