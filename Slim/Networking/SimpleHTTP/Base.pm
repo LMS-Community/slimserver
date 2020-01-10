bd5bb0f3a796a39a9acb3ba7fb3e719462ca8fa2
Michael Herger <mherger@logitech.com>
2020-01-10 07:18:32 +0100

    More copyright notices to update.
    And make sure File::Which is loaded when needed.

--- Slim/Networking/SimpleHTTP/Base.pm
+++ Slim/Networking/SimpleHTTP/Base.pm
@@ -1,5 +1,10 @@
 package Slim::Networking::SimpleHTTP::Base;
 
+# Logitech Media Server Copyright 2003-2020 Logitech.
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License,
+# version 2.
+
 use strict;
 
 use base qw(Slim::Utils::Accessor);
