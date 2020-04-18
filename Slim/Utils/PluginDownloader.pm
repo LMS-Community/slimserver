69b333b24778b6942e1ccdb4ff393134a7f66e59
Michael Herger <mherger@logitech.com>
2020-04-18 06:45:53 +0200

    When a plugin fails to install, get more information out of it
    * log expected and actual checksum
    * if in debug mode, copy downloaded file to cache folder for further investigation

--- Slim/Utils/PluginDownloader.pm
+++ Slim/Utils/PluginDownloader.pm
@@ -1,5 +1,10 @@
 package Slim::Utils::PluginDownloader;
 
+# Logitech Media Server Copyright 2001-2020 Logitech.
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License,
+# version 2.
+
 # Plugins are downloaded to <cachedir>/DownloadedPlugins and then extracted to <cachedir>/InstalledPlugins/Plugins/
 #
 # Plugins zip files should not include any additional path information - i.e. they include the install.xml file at the top level
@@ -86,7 +91,7 @@ sub extract {
 		if (-d $targetDir) {
 
 			main::INFOLOG && $log->info("removing existing $targetDir");
-		
+
 			rmtree $targetDir;
 		}
 
@@ -112,16 +117,16 @@ sub extract {
 
 			# ignore additional directory information in zip
 			for my $search ("Plugins/$plugin/", "$plugin/") {
-				
+
 				if ( $zip->membersMatching("^$search") ) {
-					
+
 					$source = $search;
 					last;
 				}
 			}
-			
+
 			if ( ($zipstatus = $zip->extractTree($source, "$targetDir/")) == Archive::Zip::AZ_OK() ) {
-					
+
 				main::INFOLOG && $log->info("extracted $plugin to $targetDir");
 
 			} else {
@@ -154,7 +159,7 @@ sub install {
 	unlink $file;
 
 	my $http = Slim::Networking::SimpleAsyncHTTP->new( \&_downloadDone, \&_downloadError, { saveAs => $file, args => $args } );
-	
+
 	main::INFOLOG && $log->info("install - downloading $name from $url");
 
 	$downloading++;
@@ -179,21 +184,34 @@ sub _downloadDone {
 	if (-r $file) {
 
 		my $sha1 = Digest::SHA1->new;
-		
-		open my $fh, '<', $file;
+
+		open my $fh, '<', $file or $log->error("Failed to open $file");
 
 		binmode $fh;
-		
-		$sha1->addfile($fh);
-		
-		close $fh;
-		
-		if ($digest ne $sha1->hexdigest) {
-			
-			$log->warn("digest does not match $file - $name will not be installed");
 
-			unlink $file;
-			
+		$sha1->addfile($fh);
+
+		close $fh;
+
+		my $gotDigest = $sha1->hexdigest;
+
+		if ($digest ne $gotDigest) {
+
+			$log->error("digest does not match $file - $name will not be installed: expected $digest, got " . $gotDigest);
+
+			if (main::DEBUGLOG && $log->is_debug && (my $cacheDir = preferences('server')->get('cachedir'))) {
+				require File::Basename;
+				require File::Copy;
+
+				my ($filename, undef, $ext) = File::Basename::fileparse($file, '.zip');
+				my $backup = catfile($cacheDir, $filename . '-' . $gotDigest . $ext);
+				$log->debug("Moving downloaded file for you to investigate:\n  from: $file\n  to:   $backup");
+				File::Copy::move($file, $backup);
+			}
+			else {
+				unlink $file;
+			}
+
 		} else {
 
 			main::INFOLOG && $log->info("digest matches - scheduling $name for install on restart");
@@ -280,7 +298,7 @@ sub _handleResponse {
 		Slim::Utils::PluginManager->message(
 			sprintf( "%s (%s)", Slim::Utils::Strings::string('PLUGINS_UPDATES_AVAILABLE'), $updates )
 		);
-		
+
 		# $updates is only set if we don't want to auto-update
 		return;
 	}
@@ -292,13 +310,13 @@ sub _handleResponse {
 			main::INFOLOG && $log->info("ignoring response - $plugin already pending action: " . $prefs->get($plugin));
 			next;
 		}
-		
+
 		my $entry = $actions->{$plugin};
 
 		if ($entry->{'action'} eq 'install' && $entry->{'url'} && $entry->{'sha'}) {
 
 			$class->install({ name => $plugin, url => $entry->{'url'}, sha => $entry->{'sha'} });
-							 
+
 		} elsif ($entry->{'action'} eq 'uninstall') {
 
 			$class->uninstall($plugin);
