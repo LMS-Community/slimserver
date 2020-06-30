a74908e69ecabc0fe62d6c9b3ac439fa2e7dc02e
Philippe G <philippe44@users.noreply.github.com>
2020-06-29 22:47:52 -0700

    Thank you Windows...
    typo!

--- Slim/Player/Protocols/HTTPS.pm
+++ Slim/Player/Protocols/HTTPS.pm
@@ -77,10 +77,18 @@ sub canDirectStreamSong {
 	return 0;
 }
 
+# see http://modernperlbooks.com/mt/2009/09/when-super-isnt.html
 sub _sysread {
-	return $_[0]->SUPER::sysread($_[1], $_[2], $_[3]); 
+	my $readLength = $_[0]->SUPER::sysread($_[1], $_[2], $_[3]); 
+	
+	if (main::ISWINDOWS && !$readLength) {
+		$! = EWOULDBLOCK;
+	}
+
+	return $readLength;
 }
 
+# we need to subclass sysread as HTTPS first inherits from IO::Socket::SSL  
 sub sysread {
 	my $readLength = Slim::Player::Protocols::HTTP::sysread(@_);
 
