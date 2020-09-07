6f349dbf945977ea06f9ca429b163dcc84d91e34
Michael Herger <mherger@logitech.com>
2020-09-07 16:23:33 +0200

    Add a simple smoketest to check whether a commit completely breaks LMS startup. More sophisticated tests to follow.

---
+++ t/00_smoketest.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+SERVER_LOG=$HOME/server.log
+TIMEOUT=30
+
+function wait_port() {
+	local wait_seconds="${1:-10}"; shift # 10 seconds as default timeout
+
+	until test $((wait_seconds--)) -eq 0 ; do
+		if nc -z localhost 9000 ; then
+			break
+		else
+			sleep 1
+		fi
+	done
+
+	((++wait_seconds))
+}
+
+function finish {
+	kill -9 $NODE_PID 2> /dev/null
+	cat $SERVER_LOG
+	rm $SERVER_LOG
+}
+trap finish EXIT
+
+./slimserver.pl --logfile=$SERVER_LOG &
+NODE_PID=$!
+
+wait_port $TIMEOUT || {
+	echo "Timing out trying to connect to LMS"
+	exit 1;
+}
+
+STATUS=$(curl -m1 -sX POST -d '{"id":0,"params":["",["serverstatus"]],"method":"slim.request"}' http://localhost:9000/jsonrpc.js)
+
+echo $STATUS | jq .
+
+RESULT=$(echo $STATUS | fgrep -q version)
+
+exit $RESULT
