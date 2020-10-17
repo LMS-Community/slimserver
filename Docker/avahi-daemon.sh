20b344b1201c236fb5be6b864d15f6a4ad4ab333
snoopy86 <8215353+snoopy86@users.noreply.github.com>
2020-10-18 00:11:16 +0200

    Clean up

--- Docker/avahi-daemon.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-exec /usr/sbin/avahi-daemon
--- Docker/install.sh
+++ Docker/install.sh
@@ -4,31 +4,20 @@
 ##        ENVIRONMENTAL CONFIG         ##
 #########################################
 
-# Configure user nobody
+# Configure user squeezeboxserver
 export DEBIAN_FRONTEND="noninteractive"
-usermod -u 99 nobody
-usermod -g 100 nobody
-usermod -d /home nobody
-chown -R nobody:users /home
-
-# Disable SSH, Syslog and Cron
-rm -rf /etc/service/sshd /etc/service/cron /etc/service/syslog-ng /etc/my_init.d/00_regen_ssh_host_keys.sh
+useradd squeezeboxserver
+usermod -u 99 squeezeboxserver
+usermod -g 100 squeezeboxserver
+usermod -d /home squeezeboxserver
+chown -R squeezeboxserver:users /home
 
 #########################################
 ##  FILES, SERVICES AND CONFIGURATION  ##
 #########################################
-# LMS
-mkdir -p /etc/service/logitechmediaserver
-cat <<'EOT' > /etc/service/logitechmediaserver/run
-#!/bin/bash
-chown -R nobody:users /config
-squeezeboxserver --user nobody  --prefsdir /config/prefs --logdir /config/logs --cachedir /config/cache
-EOT
-
-chmod -R +x /etc/service/ /etc/my_init.d/
 
 # Allow acces to /dev/snd
-usermod -a -G audio nobody
+usermod -a -G audio squeezeboxserver
 
 #########################################
 ##             INSTALLATION            ##
@@ -68,18 +57,6 @@ else
   fi
 fi
 
-
-
 curl -skL -o /tmp/lms.deb $URL
 dpkg -i /tmp/lms.deb
 rm /tmp/lms.deb
-
-
-/etc/init.d/avahi-daemon restart
-#########################################
-##                 CLEANUP             ##
-#########################################
-
-# Clean APT install files
-apt-get clean -y
-rm -rf /var/lib/apt/lists/* /var/cache/* /var/tmp/*
