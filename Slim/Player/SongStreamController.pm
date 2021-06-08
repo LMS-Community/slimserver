ea0b2105dea8499c7e3d0622ac947cee8b33894b
Philippe G <philippe44@users.noreply.github.com>
2021-06-08 01:27:41 -0700

    Keept SongSTreamController for now

--- Slim/Player/SongStreamController.pm
+++ Slim/Player/SongStreamController.pm
@@ -1,5 +1,6 @@
 package Slim::Player::SongStreamController;
 
+
 # Logitech Media Server Copyright 2001-2020 Logitech.
 # This program is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License,
@@ -8,30 +9,23 @@ package Slim::Player::SongStreamController;
 use bytes;
 use strict;
 
-use base qw(Slim::Utils::Accessor);
-
 use Slim::Utils::Log;
 
 my $log = logger('player.source');
 
 my $_liveCount = 0;
 
-{
-	__PACKAGE__->mk_accessor('ro', qw(song streamHandler protocolHandler));
-	__PACKAGE__->mk_accessor('rw', qw(playerProxyStreaming));
-}	
-
 sub new {
 	my ($class, $song, $streamHandler) = @_;
-	
-	my $self = $class->SUPER::new;
 
-	$self->init_accessor(
+	my $self = {
 		song => $song,
 		streamHandler => $streamHandler,
-		protocolHandler => Slim::Player::ProtocolHandlers->handlerForURL($song->streamUrl()),
-	);	
+		protocolHandler => my $handler = Slim::Player::ProtocolHandlers->handlerForURL($song->streamUrl()),
+	};
 
+	bless $self, $class;
+	
 	$_liveCount++;
 	if (main::DEBUGLOG && $log->is_debug) {
 		$log->debug("live=$_liveCount");	
@@ -51,32 +45,41 @@ sub DESTROY {
 	}
 }
 
+sub song {return shift->{'song'};}
+sub streamHandler {return shift->{'streamHandler'};}
+sub protocolHandler {return shift->{'protocolHandler'};}
+
+sub songProtocolHandler {return shift->song()->handler();}
+
 sub close {
 	my $self = shift;
 	
-	my $fd = $self->streamHandler;
+	my $fd = $self->{'streamHandler'};
 	
 	if (defined $fd) {
 		Slim::Networking::Select::removeError($fd);
 		Slim::Networking::Select::removeRead($fd);
 		$fd->close;
+		delete $self->{'streamHandler'};
 	}
 }
 
-sub songProtocolHandler {
-	return shift->song->handler();
-}
-
 sub isDirect {
-	return shift->song->directstream() || 0;
+	return shift->{'song'}->directstream() || 0;
 }
 
 sub streamUrl {
-	return shift->song->streamUrl();
+	return shift->{'song'}->streamUrl();
 }
 
 sub track {
-	return shift->song->currentTrack();
+	return shift->{'song'}->currentTrack();
+}
+
+sub playerProxyStreaming {
+	my $self = shift;
+	$self->{'playerProxyStreaming'} = shift if @_;
+	return $self->{'playerProxyStreaming'};
 }
 
 1;
