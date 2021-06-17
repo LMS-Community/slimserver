d453f8bc79690f2d8a4050c58b5f4a0f611dea34
Philippe G <philippe44@users.noreply.github.com>
2021-06-16 20:41:15 -0700

    making sure core url does not include startime

--- Slim/Plugin/Podcast/ProtocolHandler.pm
+++ Slim/Plugin/Podcast/ProtocolHandler.pm
@@ -23,32 +23,38 @@ Slim::Player::ProtocolHandlers->registerHandler('podcast', __PACKAGE__);
 # remove podcast:// protocol to scan real url
 sub scanUrl {
 	my ( $class, $url, $args ) = @_;
-	my $song = $args->{song};
-	my ($scanUrl, $startTime) = Slim::Plugin::Podcast::Plugin::unwrapUrl($url);	
 	
+	my $song = $args->{song};
+	my ($httpUrl, $startTime) = Slim::Plugin::Podcast::Plugin::unwrapUrl($url);	
 	my $cb = $args->{cb};
+
+	if ($startTime) {
+		# make a unique & clean url with no trailing start time
+		$url = Slim::Plugin::Podcast::Plugin::wrapUrl($httpUrl);
+		
+		# set seekdata for getNextTrack (once $song->track is updated)
+		$song->seekdata({ startTime => $startTime});	
+	}
 	
 	$args->{cb} = sub {
 		my $track = shift;
 
-		main::INFOLOG && $log->info("Scanned podcast $url from ($startTime) for title: ", $song->track->title);
+		main::INFOLOG && $log->info("Scanned podcast $url => ", $track->url, " from ($startTime) for title: ", $song->track->title);
+
+		# use the scanned track to get streamable url, ignore scanned title and coverart
 		$song->streamUrl($track->url);		
+		$track->title(Slim::Music::Info::getCurrentTitle($args->{client}, $url));
+		$track->cover(0);
 
-		# reset track's url first otherwise url-based methods will fail
+		# reset track's url - from now on all $url-based requests will refer to that track
 		$track->url($url);
-
-		# set seekdata so they can be used in proxied and direct
-		if ($startTime) {
-			my $seekdata = $song->getSeekData($startTime);
-			$song->seekdata($seekdata);
-		}
 
 		# must update playlist time for webUI to refresh - not sure why
 		$song->master->currentPlaylistUpdateTime( Time::HiRes::time() );	
 		$cb->($track, @_);
 	};
 	
-	$class->SUPER::scanUrl($scanUrl, $args);
+	$class->SUPER::scanUrl($httpUrl, $args);
 }
 
 sub shouldCacheImage { 1 }
@@ -66,7 +72,6 @@ sub onStop {
 	my $elapsed = $song->master->controller->playingSongElapsed;
 	my ($url) = Slim::Plugin::Podcast::Plugin::unwrapUrl($song->currentTrack->url);
 
-
 	if ($elapsed > 15 && (!$song->duration || $elapsed < $song->duration - 15)) {
 		$cache->set("podcast-$url", int ($elapsed), '30days');
 		main::INFOLOG && $log->info("Last position for $url is $elapsed");
@@ -78,13 +83,22 @@ sub onStop {
 sub onStream {
 	my ($self, $client, $song) = @_;
 
-	# ignore updated title that comes from parsing stream	
-	my $title = Slim::Music::Info::getCurrentTitle($client, $song->currentTrack->redir);	
-	Slim::Music::Info::setCurrentTitle($song->currentTrack->url, $title, $client);
-	$song->currentTrack->title($title);	
-	
 	Slim::Plugin::Podcast::Plugin->updateRecentlyPlayed($client, $song);
 }
+
+sub getNextTrack {
+	my ( $class, $song, $successCb, $errorCb ) = @_;
+	my $seekdata = $song->seekdata;
+
+	# set seekdata *after* $song->track has been updated by scanUrl 
+	# so that Slim::Music::Info::getBitrate works from unwrapped url
+	if (my $startTime = $seekdata->{startTime}) {
+		$song->seekdata($song->getSeekData($startTime));
+		main::INFOLOG && $log->info("starting from $startTime");
+	}
+	
+	$successCb->();
+}		
 
 
 1;
