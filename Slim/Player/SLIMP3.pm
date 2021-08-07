d55bf9eb93ee34d79201cddf429d473a2151059a
Philippe G <philippe44@users.noreply.github.com>
2021-08-06 17:43:59 -0700

    Left SLIMP3 with calling old protocolHandler method....

--- Slim/Player/SLIMP3.pm
+++ Slim/Player/SLIMP3.pm
@@ -196,7 +196,7 @@ sub play {
 	# is still processing the i2c commands we just sent.
 	select(undef,undef,undef,.05);
 
-	if ( $params->{'controller'}->protocolHandler()->isRemote() ) {	
+	if ( $params->{'controller'}->streamUrlHandler()->isRemote() ) {	
 		$client->buffering(bufferSize() / 2);
 	}
 
