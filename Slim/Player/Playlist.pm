LMS-Community/slimserver

commit 647d65e4ec090eec6fadc71311c14413b79af116

Don't try to access object property if the track object isn't a local track.

Slim/Player/Playlist.pm
@@ -763,6 +763,8 @@ sub reshuffle {
 		return;
 	}
 
+	return if $songcount <= 1;
+
 	my $realsong = ${$listRef}[Slim::Player::Source::playingSongIndex($client)];
 
 	if (!defined($realsong) || $dontpreservecurrsong) {
@@ -795,12 +797,15 @@ sub reshuffle {
 			main::DEBUGLOG && $log->is_debug && $log->debug("Using balanced shuffle");
 			my $newListRef = balancedShuffle([ map {
 				my $track = playList($client)->[$_];
-				my $artist = $track->artistName;
+				my $artist = $track->artistName if ref $track && $track->can('artistName');
 
-				my $handler = Slim::Player::ProtocolHandlers->handlerForURL($track->url) if !$artist;
+				if (!$artist) {
+					my $url = ref $track ? $track->url : $track;
+					my $handler = Slim::Player::ProtocolHandlers->handlerForURL($url);
 
-				if ( $handler && $handler->can('getMetadataFor') && (my $meta = $handler->getMetadataFor($client, $track->url)) ) {
-					$artist = $meta->{artist};
+					if ( $handler && $handler->can('getMetadataFor') && (my $meta = $handler->getMetadataFor($client, $url)) ) {
+						$artist = $meta->{artist};
+					}
 				}
 
 				[$_, $artist || ''];

Slim/Control/Commands.pm
@@ -1741,12 +1741,15 @@ sub playlistXtracksCommand {
 
 			my $sortList = Slim::Player::Playlist::balancedShuffle([ map {
 				my $track = $tracks[$_];
-				my $category = $track->$categoryMethod;
+				my $category = $track->$categoryMethod if ref $track && $track->can($categoryMethod);
 
-				my $handler = Slim::Player::ProtocolHandlers->handlerForURL($track->url) if !$category;
+				if (!$categoryMethod) {
+					my $url = ref $track ? $track->url : $track;
+					my $handler = Slim::Player::ProtocolHandlers->handlerForURL($url);
 
-				if ( $handler && $handler->can('getMetadataFor') && (my $meta = $handler->getMetadataFor($client, $track->url)) ) {
-					$category = $meta->{$categoryRemoteItem};
+					if ( $handler && $handler->can('getMetadataFor') && (my $meta = $handler->getMetadataFor($client, $url)) ) {
+						$category = $meta->{$categoryRemoteItem};
+					}
 				}
 
 				[$_, $category || ''];

commit 98a926c5a13c6d691f6214338f9a8807509617d1

Let reshuffle run also on single track playlist
If we don't let reshuffle run the client's shufflelist will be out of
sync with the playlist.

The problem can be seen if you do:
1. Enable shuffle
2. Play a single track (playlist contains a single track)
3. Select another track to "play next"

Expected: The playlist should contain two tracks and the first track
should still be playing while the second track is queued as next
track.

Actual: The first track is playing, but the playlist indicates that
it's the second track that's currently playing and the first track is
no longer in the playlist.

The problem seems to be that the client's shufflelist remains empty
after the first track is played. Then when the second track is added
it is the only track added to shufflelist and the first track is lost.

It looks like this problem was introduced in 647d65e4ec.

I'm now partially reverting 647d65e4ec. The change that I'm reverting,
was that an intentional change in 647d65e4ec? It doesn't look like the
change matches with the commit message.

Slim/Player/Playlist.pm
@@ -769,8 +769,6 @@ sub reshuffle {
 		return;
 	}
 
-	return if $songcount <= 1;
-
 	my $realsong = ${$listRef}[Slim::Player::Source::playingSongIndex($client)];
 
 	if (!defined($realsong) || $dontpreservecurrsong) {