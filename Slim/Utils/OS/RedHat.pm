0ed57d85ff75cf8edbd20b2e59c541f0b0bf4c16
Michael Herger <mherger@logitech.com>
2023-06-04 00:44:06 +0200

    Fix paths for gdresized in RedHat/Debian based systems

--- Slim/Utils/OS/RedHat.pm
+++ Slim/Utils/OS/RedHat.pm
@@ -2,7 +2,7 @@ package Slim::Utils::OS::RedHat;
 
 # Logitech Media Server Copyright 2001-2020 Logitech.
 # This program is free software; you can redistribute it and/or
-# modify it under the terms of the GNU General Public License, 
+# modify it under the terms of the GNU General Public License,
 # version 2.
 
 use strict;
@@ -33,7 +33,7 @@ sub dirsFor {
 	my ($class, $dir) = @_;
 
 	my @dirs = ();
-	
+
 	if ($dir =~ /^(?:oldprefs|updates)$/) {
 
 		push @dirs, $class->SUPER::dirsFor($dir);
@@ -43,11 +43,11 @@ sub dirsFor {
 		push @dirs, "/usr/share/squeezeboxserver/$dir";
 
 	} elsif ($dir eq 'Plugins') {
-			
+
 		push @dirs, $class->SUPER::dirsFor($dir);
 		push @dirs, "/usr/share/squeezeboxserver/Plugins";
 		push @dirs, "/usr/lib/perl5/vendor_perl/Slim/Plugin";
-		
+
 	} elsif ($dir =~ /^(?:strings|revision|repositories)$/) {
 
 		push @dirs, "/usr/share/squeezeboxserver";
@@ -95,17 +95,21 @@ sub scanner {
 	return '/usr/libexec/squeezeboxserver-scanner';
 }
 
+sub gdresized {
+	return '/usr/libexec/squeezeboxserver-resized';
+}
+
 sub canAutoUpdate { $_[0]->SUPER::runningFromSource ? 0 : 1 }
-sub installerExtension { 'rpm' }; 
+sub installerExtension { 'rpm' };
 sub installerOS { 'rpm' }
 
 sub getUpdateParams {
 	my ($class, $url) = @_;
-	
+
 	if ($url) {
 		Slim::Utils::OS::Linux::signalUpdateReady($url);
 	}
-	
+
 	return;
 }
 
