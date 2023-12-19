1602675baf9fd5d20727888d6375b629efed1dc0
darrell-k <97928054+darrell-k@users.noreply.github.com>
2023-12-19 19:29:25 +0000

    Fix album_contributor updates in new & changed scan
    This seems to work, but I wonder if we should put some more thought into how we handle multiple Artist tags - at the moment, tracks.primary_artist is arbitrarily set to the first element in the Artists array.
    
    Maybe if there is an ARTIST that is also an ALBUMARTIST (in the music file tags) it should take precedence? (Would also need to bear in mind that there could be multiple ALBUMARTIST tags as well).

--- Slim/Schema/Contributor.pm
+++ Slim/Schema/Contributor.pm
@@ -255,8 +255,9 @@ sub rescan {
 		SELECT COUNT(*) FROM contributor_track WHERE contributor = ?
 	} );
 
+	# There may have be more than one Artist tag in the music file, so track.primary_artist does not tell the whole story. So also check for ARTIST/ALBUMARTIST in contributor_track.
 	my $albumSth = $dbh->prepare_cached( qq{
-		SELECT COUNT(1) FROM tracks WHERE album = ? AND primary_artist = ?
+		SELECT COUNT(1) FROM tracks JOIN contributor_track ON tracks.id=contributor_track.track WHERE album=? AND ( tracks.primary_artist=? OR contributor_track.contributor=? AND contributor_track.role IN (1,5) )
 	} );
 
 	for my $id ( @$ids ) {
@@ -272,7 +273,7 @@ sub rescan {
 		}
 		# contributor->album relations aren't removed automatically when the last track with this primary_artist disappears
 		elsif ( $albumId ) {
-			$albumSth->execute($albumId, $id);
+			$albumSth->execute($albumId, $id, $id);
 			($count) = $albumSth->fetchrow_array;
 			$albumSth->finish;
 
