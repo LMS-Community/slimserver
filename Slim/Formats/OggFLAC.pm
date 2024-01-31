57398c0cba294e8eda3bfad36fae871e9617fa1d
philippe44 <philippe44@users.noreply.github.com>
2024-01-30 21:25:03 -0800

    add OggFlac support

--- Slim/Formats/OggFLAC.pm
+++ Slim/Formats/OggFLAC.pm
@@ -2,12 +2,58 @@ package Slim::Formats::OggFLAC;
 
 use base qw(Slim::Formats::FLAC);
 
-sub findFrameBoundaries { 0 }
+use Slim::Utils::Log;
+use Audio::Scan;
 
-sub scanBitrate{ 
-	return (-1, undef);
+my $log       = logger('scan.scanner');
+my $sourcelog = logger('player.source');
+
+=head2 getInitialAudioBlock( $fh, $offset, $time )
+
+Get the OggFlac header. For now, we use a fixed header
+that resets MD5 and frame number to 0 upon seeking. 
+NB: that function is only used when seeking, othersise
+the entire file us used untouched.
+
+=cut
+
+sub getInitialAudioBlock {
+	my ($class, $fh, $track, $time) = @_;
+
+	open my $localFh, '<&=', $fh;
+	my $info = Audio::Scan->find_frame_fh_return_info( ogf => $localFh, 0 );
+
+	main::INFOLOG && $sourcelog->is_info && $sourcelog->info('Reading initial audio block of ', length $info->{seek_header});
+
+	return $info->{seek_header};
 }
 
-sub canSeek { 0 }
+=head2 findFrameBoundaries( $fh, $offset, $time )
+
+Seeks to the Ogg block containing the sample at $time.
+
+=cut
+
+sub findFrameBoundaries {
+	my ( $class, $fh, $offset, $time ) = @_;
+
+	return (defined $fh && defined $time) ? 
+		   Audio::Scan->find_frame_fh( ogf => $fh, int($time * 1000) ) : 
+		   0;		
+}
+
+=head2 scanBitrate( $fh )
+
+Intended to scan the bitrate of a remote stream, although for FLAC this data
+is not accurate, but we can get the duration of the remote file from the header,
+so we use this to set the track duaration value.
+
+=cut
+
+sub scanBitrate { 
+	my ( $class, $fh, $url ) = @_;
+	return $class->SUPER($fh, $url, 'ogf');
+}
+
 
 1;
