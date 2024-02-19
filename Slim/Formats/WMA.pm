f9f9e1695c830ba2241bf857714083fc0391891b
Michael Herger <mherger@logitech.com>
2024-02-19 11:20:02 +0100

    Improve the release type detection for Ogg & WMA

--- Slim/Formats/WMA.pm
+++ Slim/Formats/WMA.pm
@@ -3,7 +3,7 @@ package Slim::Formats::WMA;
 
 # Logitech Media Server Copyright 2001-2020 Logitech.
 # This program is free software; you can redistribute it and/or
-# modify it under the terms of the GNU General Public License, 
+# modify it under the terms of the GNU General Public License,
 # version 2.
 
 use strict;
@@ -44,7 +44,7 @@ my %tagMapping = (
 	'MusicBrainz/Album Artist Id' => 'MUSICBRAINZ_ALBUMARTIST_ID',
 	'MusicBrainz/Album Id'        => 'MUSICBRAINZ_ALBUM_ID',
 	'MusicBrainz/Album Status'    => 'MUSICBRAINZ_ALBUM_STATUS',
-	'MusicBrainz/Album Type'      => 'MUSICBRAINZ_ALBUM_TYPE',
+	'MusicBrainz/Album Type'      => 'RELEASETYPE',
 	'MusicBrainz/Artist Id'       => 'MUSICBRAINZ_ARTIST_ID',
 	'MusicBrainz/Track Id'        => 'MUSICBRAINZ_ID',
 	'MusicBrainz/TRM Id'          => 'MUSICBRAINZ_TRM_ID',
@@ -53,12 +53,12 @@ my %tagMapping = (
 sub getTag {
 	my $class = shift;
 	my $file  = shift || return {};
-	
+
 	my $s = Audio::Scan->scan($file);
-	
+
 	my $info = $s->{info};
 	my $tags = $s->{tags};
-	
+
 	return unless $info->{song_length_ms};
 
 	# Map tags onto Logitech Media Server's preferred.
@@ -73,7 +73,7 @@ sub getTag {
 
 	# Add additional info
 	my $stream = $info->{streams}->[0];
-	
+
 	$tags->{SIZE}	      = $info->{file_size};
 	$tags->{SECS}	      = $info->{song_length_ms} / 1000;
 	$tags->{RATE}	      = $stream->{samplerate};
@@ -84,23 +84,23 @@ sub getTag {
 	$tags->{LOSSLESS}     = $info->{lossless};
 	$tags->{STEREO}       = $tags->{CHANNELS} == 2 ? 1 : 0;
 	$tags->{DLNA_PROFILE} = $info->{dlna_profile} || undef;
-	
+
 	if ( $tags->{IsVBR} ) {
 		$tags->{VBR_SCALE} = 1;
 	}
-	
+
 	if ( grep (/Professional/, map ($_->{'name'}, @{$info->{'codec_list'}})) ) {
 		$tags->{CONTENT_TYPE} = 'wmap';
 	} elsif ( $tags->{LOSSLESS} ) {
 		$tags->{CONTENT_TYPE} = 'wmal';
 	}
-	
+
 	# Flag if we have embedded cover art
 	if ( my $pic = $tags->{'WM/Picture'} ) {
 		if ( ref $pic eq 'ARRAY' ) {
 			# multiple images, use image with lowest image_type value
 			# In 'no artwork' mode, ARTWORK is the length
-			$tags->{COVER_LENGTH} = $ENV{AUDIO_SCAN_NO_ARTWORK} 
+			$tags->{COVER_LENGTH} = $ENV{AUDIO_SCAN_NO_ARTWORK}
 				? ( sort { $a->{image_type} <=> $b->{image_type} } @{$pic} )[0]->{image}
 				: length( ( sort { $a->{image_type} <=> $b->{image_type} } @{$pic} )[0]->{image} );
 		}
@@ -110,19 +110,19 @@ sub getTag {
 				: length( $pic->{image} );
 		}
 	}
-	
+
 	return $tags;
 }
 
 sub getCoverArt {
 	my $class = shift;
 	my $file  = shift || return undef;
-	
+
 	# Enable artwork in Audio::Scan
 	local $ENV{AUDIO_SCAN_NO_ARTWORK} = 0;
-	
+
 	my $s = Audio::Scan->scan_tags($file);
-	
+
 	if ( my $pic = $s->{tags}->{'WM/Picture'} ) {
 		if ( ref $pic eq 'ARRAY' ) {
 			# return image with lowest image_type value
@@ -132,26 +132,26 @@ sub getCoverArt {
 			return $pic->{image};
 		}
 	}
-	
+
 	return;
 }
 
 sub getInitialAudioBlock {
 	my ($class, $fh) = @_;
-	
+
 	open my $localFh, '<&=', $fh;
-	
+
 	seek $localFh, 0, 0;
-	
+
 	my $s = Audio::Scan->scan_fh( asf => $localFh );
-	
+
 	main::DEBUGLOG && $sourcelog->is_debug && $sourcelog->debug( 'Reading initial audio block: length ' . $s->{info}->{audio_offset} );
-	
+
 	seek $localFh, 0, 0;
 	read $localFh, my $buffer, $s->{info}->{audio_offset};
-	
+
 	close $localFh;
-	
+
 	return $buffer;
 }
 
@@ -161,7 +161,7 @@ sub findFrameBoundaries {
 	if (!defined $fh || !defined $time) {
 		return 0;
 	}
-	
+
 	return Audio::Scan->find_frame_fh( asf => $fh, int($time * 1000) );
 }
 
