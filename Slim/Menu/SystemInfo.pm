cc86f78f07322028410b0d1d35cba055c613ef81
Michael Herger <michael@herger.net>
2024-08-21 16:22:01 +0200

    Don't crash the Perl module reporting if we lack SSL support

--- Slim/Menu/SystemInfo.pm
+++ Slim/Menu/SystemInfo.pm
@@ -362,22 +362,30 @@ sub infoPerl {
 			type => 'text',
 			name => 'DBD::SQLite' . cstring($client, 'COLON') . " $DBD::SQLite::VERSION (sqlite " . Slim::Utils::Cache->new()->{_cache}->{dbh}->{sqlite_version} . ')'
 		},
+	];
 
-		{
+	if ( Slim::Networking::Async::HTTP->hasSSL() ) {
+		push @{$items}, {
 			type => 'text',
-			name => 'IO::Socket::SSL' . cstring($client, 'COLON') . ' ' . (Slim::Networking::Async::HTTP->hasSSL() ? $IO::Socket::SSL::VERSION : cstring($client, 'BLANK')),
+			name => 'IO::Socket::SSL' . cstring($client, 'COLON') . ' ' . $IO::Socket::SSL::VERSION,
 		},
 
 		{
 			type => 'text',
-			name => 'Mozilla::CA' . cstring($client, 'COLON') . ' ' . $Mozilla::CA::VERSION,
+			name => 'Mozilla::CA' . cstring($client, 'COLON') . ' ' . ($Mozilla::CA::VERSION || cstring($client, 'UNK')),
 		},
 
 		{
 			type => 'text',
 			name => sprintf("Net::SSLeay%s %s - %s", cstring($client, 'COLON'), $Net::SSLeay::VERSION, Net::SSLeay::SSLeay_version(Net::SSLeay::SSLEAY_VERSION())),
-		},
-	];
+		};
+	}
+	else {
+		push @{$items}, {
+			type => 'text',
+			name => 'IO::Socket::SSL' . cstring($client, 'COLON') . ' ' . cstring($client, 'BLANK'),
+		};
+	}
 
 	if ( Slim::Utils::OSDetect->getOS->sqlHelperClass() =~ /MySQL/i ) {
 		splice(@{$items}, 5, 0,	{
