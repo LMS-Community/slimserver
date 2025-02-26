0d04d3e02a60d825b2997a33a4c3fa894ef40b7e
darrell-k <darrell@darrell.org.uk>
2025-02-26 13:30:19 +0000

    tidy up
    Signed-off-by: darrell-k <darrell@darrell.org.uk>

--- Slim/Formats/Playlists/CUE.pm
+++ Slim/Formats/Playlists/CUE.pm
@@ -565,7 +565,19 @@ sub parse {
 		# Everything in a cue sheet should be marked as audio.
 		$track->{'AUDIO'} = 1;
 
-		Slim::Formats::sanitizeTagValues($track, $filename, 'cue', $embedded);
+		# external cue sheet hasn't been through tag mapping, so do it now.
+		if ( !$embedded && $track->{'FILENAME'} ) {
+			if ( my $type = Slim::Music::Info::typeFromPath(Slim::Utils::Misc::pathFromFileURL($track->{'FILENAME'})) ) {
+				if ( my $tagReaderClass = Slim::Formats->classForFormat($type) ) {
+					Slim::Formats->loadTagFormatForType($type);
+					if ( $tagReaderClass->can('doTagMapping') ) {
+						$tagReaderClass->doTagMapping($track);
+					}
+				}
+			}
+		}
+
+		Slim::Formats::sanitizeTagValues($track, $filename);
 	}
 
 	# Bug 8443, if no tracks contain a URI element, it's an invalid cue
