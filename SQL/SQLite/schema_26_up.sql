ec1586a70c046600658f2673e14a9e4277eb8d38
Michael Herger <michael@herger.net>
2025-06-09 17:38:33 +0200

    We must drop the index before we delete a column used by that index.
    Signed-off-by: Michael Herger <michael@herger.net>

--- SQL/SQLite/schema_26_up.sql
+++ SQL/SQLite/schema_26_up.sql
@@ -2,5 +2,5 @@ DROP TABLE IF EXISTS rescans;
 DROP TABLE IF EXISTS pluginversion;
 DROP TABLE IF EXISTS unreadable_tracks;
 
-ALTER TABLE tracks DROP hash;
 DROP INDEX IF EXISTS trackHashIndex;
+ALTER TABLE tracks DROP hash;
