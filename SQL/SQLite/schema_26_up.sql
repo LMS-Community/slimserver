1a232513438995ba47180e9a71a197c56253ba3a
Michael Herger <michael@herger.net>
2025-06-10 13:50:20 +0200

    Don't try to drop a table column, as this is only supported in more recent SQLite versions >= 3.35.0
    Signed-off-by: Michael Herger <michael@herger.net>

--- SQL/SQLite/schema_26_up.sql
+++ SQL/SQLite/schema_26_up.sql
@@ -3,4 +3,5 @@ DROP TABLE IF EXISTS pluginversion;
 DROP TABLE IF EXISTS unreadable_tracks;
 
 DROP INDEX IF EXISTS trackHashIndex;
-ALTER TABLE tracks DROP hash;
+-- DROP <column> is only supported in SQLite 3.35.0+
+-- ALTER TABLE tracks DROP hash;
