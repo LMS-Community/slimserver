9dfde14c176c764e0a7a2b3c42dda583c7585582
Michael Herger <michael@herger.net>
2025-10-08 22:27:35 +0200

    On macOS and Windows add the prefs folder to the search list for custom types/convert/strings files. Otherwise such user customized files would be overwritten on each LMS update.
    Signed-off-by: Michael Herger <michael@herger.net>

--- Slim/Utils/OS/OSX.pm
+++ Slim/Utils/OS/OSX.pm
@@ -162,6 +162,7 @@ sub dirsFor {
 	if ($dir =~ /^(?:strings|revision|convert|types|repositories)$/) {
 
 		push @dirs, $Bin;
+		push @dirs, $class->dirsFor('prefs');
 
 	} elsif ($dir =~ /^(?:Graphics|HTML|IR|Plugins)$/) {
 
