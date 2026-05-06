eb8fdc0cfec33563188f33d09300a2f9ecb3a75d
Michael Herger <michael@herger.net>
2026-05-06 06:11:19 +0200

    #1140  - Fix stat returning an empty size for most files. `canonpath` or something seems to be messing with the stats cache under certain circumstances.
    Signed-off-by: Michael Herger <michael@herger.net>

--- Slim/Utils/Scanner/Local/Async.pm
+++ Slim/Utils/Scanner/Local/Async.pm
@@ -150,10 +150,8 @@ sub find {
 
 		$count++;
 
-		# XXX Not sure why, but sometimes there is no cached stat data available?!
-		if ( !(stat _)[9] ) {
-			stat $file;
-		}
+		# canonpath or something seems to be messing with the stat cache, so we need to stat the file again to get the correct mtime and size
+		stat $file;
 
 		$sth->execute(
 			Slim::Utils::Misc::fileURLFromPath($file),
