4606027814b995ea7ee21d190273fe8c7e3a3618
Zapdatt <54618617+Zapdatt@users.noreply.github.com>
2026-07-08 02:54:31 +0200

    removed preference, now automatically only update when it has disappeared,
    Signed-off-by: Zapdatt <54618617+Zapdatt@users.noreply.github.com>

--- Slim/Music/Artwork.pm
+++ Slim/Music/Artwork.pm
@@ -334,9 +334,9 @@ sub updateStandaloneArtwork {
 
 				Slim::Utils::Scheduler::unpause() if !main::SCANNER;
 			}
-			# cover art has disappeared
-			# check user setting whether or not to do it.
-			elsif ( !$newCoverId && !Slim::Utils::Prefs::preferences('server')->get('disableArtworkCleanup')) {
+
+			# Only if cover art has disappeared; set reference default
+			elsif (!$newCoverId && $coverid && $albumid) {
 				$sth_update_albums->execute( undef, $albumid );
 				$sth_update_tracks->execute( 0, undef, $albumid );
 
