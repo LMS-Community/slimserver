2e51dd18c6eb562c7c2cda1de176b1b0d9a6cba5
darrell-k <darrell@darrell.org.uk>
2026-07-15 19:48:09 +0100

    take out some logging
    Signed-off-by: darrell-k <darrell@darrell.org.uk>

--- Slim/Control/Queries.pm
+++ Slim/Control/Queries.pm
@@ -814,7 +814,6 @@ sub albumsQuery {
 				join(',', @linkRoleIds));
 			} else {
 				my @linkRoles = Slim::Schema::Contributor->allAlbumLinkRoles();
-Slim::Utils::Log::logError("DK \@linkRoles=" . Data::Dump::dump(@linkRoles));
 				# when filtering by role and not by contributor, put that role at the head of the list if it wasn't in there yet
 				if ($roleID && !$contributorID) {
 					unshift @linkRoles, map { Slim::Schema::Contributor->roleToType($_) || $_ } split(/,/, $roleID);
@@ -841,7 +840,6 @@ Slim::Utils::Log::logError("DK \@linkRoles=" . Data::Dump::dump(@linkRoles));
 					%s
 					ORDER BY contributor_album.role, contributors.namesort
 				}, join(',', @linkRoleIds), $excludeDisplayArtists );
-#Slim::Utils::Log::logError("DK \$contributorSql=" . Data::Dump::dump($contributorSql));
 			}
 		}
 
@@ -967,9 +965,6 @@ Slim::Utils::Log::logError("DK \@linkRoles=" . Data::Dump::dump(@linkRoles));
 				}
 				@artists = Slim::Utils::Misc::uniq(@artists);
 				@artistIds = Slim::Utils::Misc::uniq(@artistIds);
-#Slim::Utils::Log::logError("DK =============================================================================================");
-#Slim::Utils::Log::logError("DK \@artists=" . Data::Dump::dump(@artists));
-#Slim::Utils::Log::logError("DK \@artistIds=" . Data::Dump::dump(@artistIds));
 
 				if ( $c->{'contributor_display.id'} ) {
 					my $displayArtistsSql = qq{
@@ -982,8 +977,6 @@ Slim::Utils::Log::logError("DK \@linkRoles=" . Data::Dump::dump(@linkRoles));
 					my $displayArtists = $dbh->selectall_arrayref($displayArtistsSth, { Slice => {} }, ( $c->{'albums.id'}, $c->{'contributor_display.id'} ) );
 					my $displayArtistsIds = join(',', map { $_->{id} } @$displayArtists);
 					my $displayArtistsNames = join(',', map { $_->{name} } @$displayArtists);
-#Slim::Utils::Log::logError("DK \$displayArtistsIds=" . Data::Dump::dump($displayArtistsIds));
-#Slim::Utils::Log::logError("DK \$displayArtistsNames=" . Data::Dump::dump($displayArtistsNames));
 					$request->addResultLoopIfValueDefined($loopname, $chunkCount, 'display_artist_artist_ids', $displayArtistsIds);
 					$request->addResultLoopIfValueDefined($loopname, $chunkCount, 'display_artist_artists', $displayArtistsNames);
 				}
