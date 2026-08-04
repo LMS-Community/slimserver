585551b21f03f7953fbb366e76b70e36fd27bbf8
Michael Herger <michael@herger.net>
2026-08-04 17:22:26 +0200

    Make scanner helper tables case sensitive
    Signed-off-by: Michael Herger <michael@herger.net>

--- SQL/SQLite/schema_scanner.sql
+++ SQL/SQLite/schema_scanner.sql
@@ -1,6 +1,6 @@
 DROP TABLE IF EXISTS scanned_files;
 CREATE TABLE scanned_files (
-  url text NOT NULL COLLATE NOCASE, -- URL must be case insensitive, or we might duplicate tracks if the filename changes case only (https://github.com/LMS-Community/slimserver/issues/705#issuecomment-1026229542)
+  url text NOT NULL,
   timestamp int(10),
   filesize int(10)
 );
@@ -8,7 +8,7 @@ CREATE INDEX scannedUrlIndex ON scanned_files (url);
 
 DROP TABLE IF EXISTS scanned_pics;
 CREATE TABLE scanned_pics (
-  url text NOT NULL COLLATE NOCASE, -- URL must be case insensitive, or we might duplicate tracks if the filename changes case only (https://github.com/LMS-Community/slimserver/issues/705#issuecomment-1026229542)
+  url text NOT NULL,
   timestamp int(10),
   filesize int(10)
 );
